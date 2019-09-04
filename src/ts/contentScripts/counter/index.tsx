f80e9e690e9826d53fc7e68463250ed1b08d8a93
Hank Stoever <hstove@gmail.com>
2019-09-04 10:55:08 -0700

    adds eslint, lint fixes

--- src/ts/contentScripts/counter/index.tsx
+++ src/ts/contentScripts/counter/index.tsx
@@ -1,21 +1,22 @@
-import * as React from 'react';
-import { Store } from 'webext-redux';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import CounterApp from './containers/CounterApp';
+import React from 'react'
+import { Store } from 'webext-redux'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import CounterApp from './containers/CounterApp'
 
-import { createDomAnchor } from '../../scripts/dom';
+import { createDomAnchor } from '../../scripts/dom'
 
-createDomAnchor('counter-root');
+createDomAnchor('counter-root')
 
 const store = new Store({
-	portName: 'ExPort' // Communication port between the background component and views such as browser tabs.
-});
+  portName: 'ExPort' // Communication port between the background component and views such as browser tabs.
+})
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 store.ready().then(() => {
-	ReactDOM.render(
-		<Provider store={store as any}>
-			<CounterApp />
-		</Provider>
-		, document.getElementById('counter-root'));
-});
+  ReactDOM.render(
+    <Provider store={store as any}>
+      <CounterApp />
+    </Provider>
+    , document.getElementById('counter-root'))
+})
