fa878b6ad699f98d8f233dceed3d110b919dbb48
Thomas Osmonson <thomas.osmonson@gmail.com>
2019-09-05 10:46:32 -0500

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,59 @@
 module.exports = {
-  'extends': [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react/recommended',
+  extends: [
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
-    'eslint:recommended',
+    'plugin:react/recommended',
+    // 'plugin:jsx-a11y/recommended'
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': '2018',
-    'sourceType': 'module',
-    'project': './tsconfig.json',
-    'tsconfigRootDir': __dirname,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: '2018',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
   },
-  'plugins': [
-    '@typescript-eslint',
-    'react'
-  ],
-  'env': {
-    'browser': true,
-    'node': true,
-    'es6': true
+  plugins: ['@typescript-eslint', 'react'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true
   },
-  'rules': {
-    'quotes': [2, 'single'],
+  rules: {
+    quotes: [2, 'single'],
     'eol-last': 2,
     'no-mixed-requires': 0,
     'no-var': 2,
     'no-param-reassign': 'error',
     'brace-style': 2,
     'no-empty': 'off',
-    '@typescript-eslint/indent': [2, 2, {
-      'FunctionDeclaration': {'parameters': 'first'},
-      'FunctionExpression': {'parameters': 'first'},
-      'ObjectExpression': 'first',
-      'ArrayExpression': 'first',
-      'ImportDeclaration': 'first',
-      'CallExpression': {'arguments': 'first'}
-    }],
-    '@typescript-eslint/member-delimiter-style': ['error', {
-      'multiline': {
-        'delimiter': 'none',
-        'requireLast': true
-      },
-      'singleline': {
-        'delimiter': 'semi',
-        'requireLast': true
-      },
-    }],
-    '@typescript-eslint/no-unused-vars': ['error', {'argsIgnorePattern': '^_'}],
+    '@typescript-eslint/indent': [
+      2,
+      2,
+      {
+        FunctionDeclaration: { parameters: 'first' },
+        FunctionExpression: { parameters: 'first' },
+        ObjectExpression: 'first',
+        ArrayExpression: 'first',
+        ImportDeclaration: 'first',
+        CallExpression: { arguments: 'first' }
+      }
+    ],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: true
+        }
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/array-type': 'error',
     '@typescript-eslint/semi': ['error', 'never'],
     '@typescript-eslint/no-misused-promises': 'error',
@@ -70,4 +73,4 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     'react/jsx-uses-vars': [2]
   }
-};
+}
