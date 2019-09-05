5704292e4a25e7a3f064007e16e08ca765eb048e
Thomas Osmonson <thomas.osmonson@gmail.com>
2019-09-05 10:39:46 -0500

    merge pr and modify prettier settings

--- src/ts/background/store/wallet/reducer.ts
+++ src/ts/background/store/wallet/reducer.ts
@@ -5,14 +5,17 @@ const initialState: WalletState = {
   seed: null
 }
 
-export const walletReducer: Reducer<WalletState, WalletActions> = (state = initialState, action: WalletActions): WalletState => {
-  switch(action.type) {
-  case STORE_SEED:
-    return {
-      ...state,
-      seed: action.payload
-    }
-  default:
-    return state
+export const walletReducer: Reducer<WalletState, WalletActions> = (
+  state = initialState,
+  action: WalletActions
+): WalletState => {
+  switch (action.type) {
+    case STORE_SEED:
+      return {
+        ...state,
+        seed: action.payload
+      }
+    default:
+      return state
   }
 }
