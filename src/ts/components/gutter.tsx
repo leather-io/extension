378b8c60fea1a7237159b72a43ca629604ff3073
Hank Stoever <hstove@gmail.com>
2019-11-01 18:29:59 -0700

    playing with TSX docs

--- src/ts/components/gutter.tsx
+++ src/ts/components/gutter.tsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { Box } from '@blockstack/ui'
 import { BoxProps } from '@blockstack/ui/dist/box'
 
 interface GutterProps extends BoxProps {
+  /** How many 'vertical spacing components' you want to create */
   multiplier: number
+  /** The vertical padding you'd like to use in each individual component.  */
   base?: number
 }
 
-const Gutter: React.FC<GutterProps> = ({ base = 6, multiplier, ...rest }: GutterProps) => {
-  const boxes = [];
+/**
+ * A component that can be used to create large vertical spacing.
+ * It's like using the `p` spacing prop, but creating a list of them.
+ * This is used because our spacing system only goes up to 6,
+ * and sometimes you want large bits of spacing, so you can do 6 * 3
+ */
+const Gutter: FC<GutterProps> = ({ base = 6, multiplier, ...rest }: GutterProps) => {
+  const boxes = []
   for (let index = 0; index < multiplier; index++) {
-    boxes.push(<Box py={base} {...rest} />);
+    boxes.push(<Box py={base} {...rest} />)
   }
-  return (
-    <>{boxes}</>
-  )
+  return <>{boxes}</>
 }
 
 export default Gutter
