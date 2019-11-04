e9764e196d5eaf313cd56757b702199c43b2e0f9
Hank Stoever <hstove@gmail.com>
2019-11-04 14:48:07 -0800

    remove deleted 'Test' component

--- src/ts/stories/0-Welcome.stories.js
+++ src/ts/stories/0-Welcome.stories.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { linkTo } from '@storybook/addon-links'
 import { Welcome } from '@storybook/react/demo'
-import Test from '@components/test'
 
 export default {
   title: 'Welcome'
 }
 
 export const toStorybook = () => <Welcome showApp={linkTo('Button')} />
-
-export const tester = () => <Test name="Hello" />
 
 toStorybook.story = {
   name: 'to Storybook'
