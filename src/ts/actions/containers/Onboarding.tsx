9428e0b15df70ba5f01d07c7f1c5ac4bacf04bda
Hank Stoever <hstove@gmail.com>
2019-12-09 16:47:51 -0800

    removes some commented stuff

--- src/ts/actions/containers/Onboarding.tsx
+++ src/ts/actions/containers/Onboarding.tsx
@@ -9,26 +9,6 @@ export const OnboardingApp: React.FC = () => {
       <React.Fragment>
         <CSSReset />
         <Onboarding />
-        {/* <Flex pt={6} px={2} wrap="wrap">
-          <Gutter base={6} multiplier={2} width="100%" />
-          <Box width="100%" textAlign="center">
-            <Text textStyle="display.large">
-              {!manifest ? 'Loading...' : `Sign in to ${manifest.name}`}
-            </Text>
-          </Box>
-          <Gutter multiplier={1} width="100%" />
-          <Box width="100%" textAlign="center" pt={6} px={4}>
-            <Button
-              isLoading={!manifest}
-              variant="solid"
-              mt={6}
-              size="lg"
-              onClick={signIn}
-            >
-              Continue
-            </Button>
-          </Box>
-        </Flex> */}
       </React.Fragment>
     </ThemeProvider>
   );
