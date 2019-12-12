26d1fb83affb510bb0081199f3ad7ca45aa69a1b
Hank Stoever <hstove@gmail.com>
2019-12-12 15:38:10 -0800

    show auth response on page in test app

--- tests/test-app/src/App.tsx
+++ tests/test-app/src/App.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { authenticate } from '@blockstack/connect'
+import { useState } from 'react';
 
 const App: React.FC = () => {
+  const [authResponse, setAuthResponse] = useState('');
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +13,9 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        {authResponse && (
+          <span id="auth-response">{authResponse}</span>
+        )}
         <a
           className="App-link"
           id="auth-action"
@@ -18,11 +23,12 @@ const App: React.FC = () => {
           onClick={() => authenticate({
             redirectTo: '/',
             manifestPath: '/manifest.json',
-            finished: () => {
-              console.log('done');
+            vaultUrl: 'http://localhost:8080',
+            finished: ({ authResponse }) => {
+              setAuthResponse(authResponse);
             },
             appDetails: {
-              name: 'Tester',
+              name: 'Tester-Fake-App',
               icon: `${window.location.origin}/logo512.png`
             },
           })}
