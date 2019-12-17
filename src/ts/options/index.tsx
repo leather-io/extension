e6221f25f4032c5ea96186ec52bebdd0b5bdc32a
Hank Stoever <hstove@gmail.com>
2019-12-16 18:46:24 -0800

    update extension redux to serialize wallet

--- src/ts/options/index.tsx
+++ src/ts/options/index.tsx
@@ -1,15 +1,15 @@
 import 'react-hot-loader';
 import * as React from 'react';
-import { Store, applyMiddleware } from 'webext-redux';
+import ExtStore from '@store/ext-store';
 import ReactDOM from 'react-dom';
 import { Store as ReduxStore } from 'redux';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, middlewareComponents } from '../background/store';
+import { persistor } from '../background/store';
 import OptionsApp from './containers/OptionsApp';
 import DevStore from '../dev/store';
 
-const buildApp = (store: ReduxStore | Store) => {
+const buildApp = (store: ReduxStore | typeof ExtStore) => {
   ReactDOM.render(
     <Provider store={store as any}>
       <PersistGate loading={null} persistor={persistor}>
@@ -23,10 +23,7 @@ const buildApp = (store: ReduxStore | Store) => {
 if (EXT_ENV === 'web') {
   buildApp(DevStore);
 } else {
-  const store = new Store({
-    portName: 'ExPort', // Communication port between the background component and views such as browser tabs.
-  });
-  applyMiddleware(store, ...middlewareComponents);
+  const store = ExtStore;
   // eslint-disable-next-line @typescript-eslint/no-floating-promises
   store.ready().then(() => buildApp(store));
 }
