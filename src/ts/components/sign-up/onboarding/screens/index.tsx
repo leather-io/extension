f65ff0e50da8c890a62506e88b41fc4445efef74
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-01-16 16:35:58 -0600

    move everything around

--- src/ts/components/sign-up/onboarding/screens/index.tsx
+++
@@ -1,8 +0,0 @@
-import { SignIn } from '@components/sign-up/onboarding/screens/sign-in';
-import { Create } from '@components/sign-up/onboarding/screens/create';
-import { SecretKey } from '@components/sign-up/onboarding/screens/secret-key';
-import { SaveKey } from '@components/sign-up/onboarding/screens/save-key';
-import { Connect } from '@components/sign-up/onboarding/screens/connect';
-import { Final } from '@components/sign-up/onboarding/screens/complete';
-
-export { Create, SecretKey, Connect, SaveKey, Final, SignIn };
