669ce8f3d6d53a682fa8e873e759a569ef259d85
timstackblock <49165468+timstackblock@users.noreply.github.com>
2020-03-05 12:27:35 -0500

    playwright

--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -149,7 +149,7 @@ module.exports = {
   // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
-  testRegex: '/tests/.*.test.tsx?$',
+  testRegex: '/tests/.*.tests.tsx?$',
 
   // This option allows the use of a custom results processor
   // testResultsProcessor: null,
