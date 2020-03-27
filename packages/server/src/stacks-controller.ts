e4e08ca359443ec2e17d6a4d0a53fefd798fe528
Hank Stoever <hstove@gmail.com>
2020-03-26 17:40:35 -0700

    feat: adds server with faucet, tests

---
+++ packages/server/src/stacks-controller.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from 'express';
+import { makeSTXTokenTransfer, TransactionVersion } from '@blockstack/stacks-transactions';
+import BigNum from 'bn.js';
+
+export const StacksController = express.Router();
+
+StacksController.post('/faucet', (req: Request, res: Response) => {
+  try {
+    const { address } = req.query;
+
+    const tx = makeSTXTokenTransfer(
+      address,
+      new BigNum(10e3),
+      new BigNum(10),
+      new BigNum(0),
+      process.env.FAUCET_PRIVATE_KEY,
+      TransactionVersion.Testnet,
+      'Faucet'
+    );
+
+    const hex = tx.serialize().toString('hex');
+
+    res.json({
+      success: true,
+      tx: hex,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false });
+  }
+});
