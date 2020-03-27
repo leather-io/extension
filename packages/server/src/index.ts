9d6daed8c0785c0dd37fefa1fb2c7a11125b89ae
Hank Stoever <hstove@gmail.com>
2020-03-27 13:11:23 -0700

    feat: run server with yarn dev

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,7 +1,12 @@
 import DotEnv from 'dotenv';
+import { resolve } from 'path';
 import { app } from './app';
 
-DotEnv.config();
+if (process.env.NODE_ENV === 'development') {
+  DotEnv.config({ path: resolve(process.cwd(), '.env.sample') });
+  DotEnv.config({ path: resolve(process.cwd(), '.env') });
+}
+
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 5555;
 
 app.listen(port, err => {
