aabb036f7b2a8043f431870824324155376ca0bb
Hank Stoever <hstove@gmail.com>
2020-04-10 11:53:24 -0700

    feat: rpc methods for contract interface, call-read-only

--- packages/rpc-client/src/index.ts
+++ packages/rpc-client/src/index.ts
@@ -1,4 +1,5 @@
 import BN from 'bn.js';
+import { serializeCV, ClarityValue } from '@blockstack/stacks-transactions';
 
 const baseURL = 'http://127.0.0.1:9000';
 const sidecarURL = 'http://localhost:3999';
@@ -38,4 +39,72 @@ export const broadcastTX = async (hex: Buffer) => {
     // body: hex.toString('hex'),
   });
   return response;
+};
+
+interface FetchContractInterface {
+  contractAddress: string;
+  contractName: string;
+}
+
+interface BufferArg {
+  buffer: {
+    length: number;
+  };
+}
+
+export interface ContractInterfaceFunctionArg {
+  name: string;
+  type: string | BufferArg;
+}
+
+export interface ContractInterfaceFunction {
+  name: string;
+  access: 'public' | 'private' | 'read_only';
+  args: ContractInterfaceFunctionArg[];
+}
+
+export interface ContractInterface {
+  functions: ContractInterfaceFunction[];
+}
+
+export const fetchContractInterface = async ({
+  contractAddress,
+  contractName,
+}: FetchContractInterface) => {
+  const url = `${baseURL}/v2/contracts/interface/${contractAddress}/${contractName}`;
+  const response = await fetch(url);
+  const contractInterface: ContractInterface = await response.json();
+  return contractInterface;
+};
+
+interface CallReadOnly extends FetchContractInterface {
+  args: ClarityValue[];
+  functionName: string;
+}
+
+export const callReadOnly = async ({
+  contractName,
+  contractAddress,
+  functionName,
+  args,
+}: CallReadOnly) => {
+  const url = `${baseURL}/v2/contracts/call-read/${contractAddress}/${contractName}/${functionName}`;
+  const argsStrings = args.map(arg => {
+    return `0x${serializeCV(arg).toString('hex')}`;
+  });
+  const body = {
+    sender: 'SP31DA6FTSJX2WGTZ69SFY11BH51NZMB0ZW97B5P0',
+    arguments: argsStrings,
+  };
+  console.log(body);
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+  return data;
 };
