f6301737ad1d341a4ec8452bcd6500a6c68d4559
Hank Stoever <hstove@gmail.com>
2020-04-24 14:32:47 -0700

    feat: improved faucet UX

--- packages/test-app/components/faucet.tsx
+++ packages/test-app/components/faucet.tsx
@@ -3,13 +3,17 @@ import { Flex, Box, Button, Input, Text } from '@blockstack/ui';
 import { getRPCClient } from '@common/utils';
 
 interface FaucetResponse {
-  tx?: string;
+  txId?: string;
   success: boolean;
 }
 
 export const Faucet: React.FC = () => {
   const [address, setAddress] = useState('');
   const [tx, setTX] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const client = getRPCClient();
 
   const handleInput = (evt: React.FormEvent<HTMLInputElement>) => {
     setAddress(evt.currentTarget.value || '');
@@ -23,20 +27,35 @@ export const Faucet: React.FC = () => {
     return 'https://crashy-stacky.zone117x.com';
   };
 
+  const waitForBalance = async (currentBalance: number, attempts: number) => {
+    const { balance } = await client.fetchAccount(address);
+    if (attempts > 11) {
+      setError('No results after 2 minutes. Something may have gone wrong.');
+      setLoading(false);
+    }
+    if (balance.toNumber() > currentBalance) {
+      setLoading(false);
+      return;
+    }
+    setTimeout(async () => {
+      await waitForBalance(currentBalance, attempts + 1);
+    }, 10000);
+  };
+
   const onSubmit = async () => {
+    setLoading(true);
     console.log(address);
     const url = `${getServerURL()}/sidecar/v1/debug/faucet?address=${address}`;
     const res = await fetch(url, {
       method: 'POST',
     });
     const data: FaucetResponse = await res.json();
-    if (data.tx) {
-      setTX(data.tx);
-      const buf = Buffer.from(data.tx, 'hex');
-      const client = getRPCClient();
-      await client.broadcastTX(buf);
-    }
     console.log(data);
+    if (data.txId) {
+      setTX(data.txId);
+    }
+    const { balance } = await client.fetchAccount(address);
+    await waitForBalance(balance.toNumber(), 0);
   };
 
   return (
@@ -48,8 +67,13 @@ export const Faucet: React.FC = () => {
         Receive some free testnet STX for testing out the network. STX are required to execute smart contract functions.
       </Text>
       {tx && (
-        <Text display="inline-block" my={3} fontSize={1} style={{ wordBreak: 'break-all' }}>
-          {tx}
+        <Text display="inline-block" my={1} fontSize={1} style={{ wordBreak: 'break-all' }}>
+          TXID: {tx}
+        </Text>
+      )}
+      {error && (
+        <Text display="inline-block" my={1} fontSize={1} color="red">
+          {error}
         </Text>
       )}
       <Flex wrap="wrap">
@@ -64,7 +88,9 @@ export const Faucet: React.FC = () => {
           />
         </Box>
         <Box width="100%" mt={3} onClick={onSubmit}>
-          <Button>Receive Testnet STX</Button>
+          <Button isLoading={loading} loadingText=" Waiting For TX to Confirm">
+            Receive Testnet STX
+          </Button>
         </Box>
       </Flex>
     </Box>
