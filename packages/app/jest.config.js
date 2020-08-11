3b9748e8645560bf3646554038a8211b56ac0a64
Friedger Müffke <mail@friedger.de>
2020-08-11 13:08:26 +0200

    fix(app): transform js files

--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -166,7 +166,10 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  transform: { '^.+\\.tsx?$': 'ts-jest' },
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
