29e0c30464079251be1bd891fc29a6de4a8a6f1e
Hank Stoever <hstove@gmail.com>
2020-08-31 21:15:14 -0700

    build: fix CI by building connect-ui

--- packages/connect-ui/src/index.ts
+++ packages/connect-ui/src/index.ts
@@ -1,18 +1,1 @@
-// import { AuthOptions, authenticate } from '@blockstack/connect-core';
 export { Components, JSX } from './components';
-
-// export const showBlockstackConnect = (authOptions: AuthOptions) => {
-//   const element = document.createElement('connect-modal');
-//   element.authOptions = authOptions;
-//   document.body.appendChild(element);
-//   element.addEventListener('signUp', () => {
-//     authenticate({
-//       ...authOptions,
-//       sendToSignIn: false,
-//     });
-//     console.log('signin up');
-//   });
-//   element.addEventListener('signIn', () => {
-//     console.log('signin in');
-//   });
-// };
