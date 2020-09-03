8d9f1a4f0530c71c68fef286f62bc33aec51a5df
Hank Stoever <hstove@gmail.com>
2020-09-03 14:19:59 -0700

    feat: remove all connect stuff from connect-react

--- packages/test-app/components/auth.tsx
+++ packages/test-app/components/auth.tsx
@@ -1,36 +1,19 @@
 import React from 'react';
 import { Button, Text, Box, space, ButtonGroup } from '@blockstack/ui';
 import { useConnect } from '@blockstack/connect-react';
-import { showBlockstackConnect, authenticate } from '@blockstack/connect';
 
 export const Auth: React.FC = () => {
-  // const { doOpenAuth } = useConnect();
-  const { authOptions } = useConnect();
-  console.log(authOptions);
+  const { doOpenAuth } = useConnect();
   return (
     <Box>
       <Text display="block" textStyle="body.large">
         Sign in with your Blockstack account to try the demo
       </Text>
       <ButtonGroup spacing={space('base')} mt={space('base-loose')}>
-        <Button
-          size="lg"
-          onClick={() =>
-            authenticate({
-              ...authOptions,
-              sendToSignIn: true,
-            })
-          }
-          data-test="sign-in"
-        >
+        <Button size="lg" onClick={() => doOpenAuth(true)} data-test="sign-in">
           Sign in
         </Button>
-        <Button
-          size="lg"
-          mode="tertiary"
-          onClick={() => showBlockstackConnect(authOptions)}
-          data-test="sign-up"
-        >
+        <Button size="lg" mode="tertiary" onClick={() => doOpenAuth()} data-test="sign-up">
           Sign up
         </Button>
       </ButtonGroup>
