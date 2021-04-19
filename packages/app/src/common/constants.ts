ed96f37bf3a87d52fed176dad18190d84bec45cb
Hank Stoever <hstove@gmail.com>
2021-04-19 11:15:41 -0700

    fix: testnet registrar on localhost - needed for tests

--- packages/app/src/common/constants.ts
+++ packages/app/src/common/constants.ts
@@ -2,12 +2,11 @@ import { Subdomains } from '@stacks/keychain';
 
 export const gaiaUrl = 'https://hub.blockstack.org';
 
-export const Subdomain: Subdomains = Subdomains.STACKS;
+export let Subdomain: Subdomains = Subdomains.STACKS;
 
-// Uncomment if you want to use the testnet registrar during dev
-// if (document?.location.origin.includes('localhost')) {
-//   Subdomain = Subdomains.TEST_V2;
-// }
+if (document?.location.origin.includes('localhost')) {
+  Subdomain = Subdomains.TEST_V2;
+}
 
 export const transition = 'all .2s cubic-bezier(.215,.61,.355,1)';
 
