d545a1d630e1d4ca7e4957336077766179a3796e
kyranjamie <kyran.burraston@gmail.com>
2021-06-16 11:33:05 +0200

    feat: add `<TxStatusLabel />` component

---
+++ src/pages/transaction-details/components/tx-status-label.tsx
@@ -0,0 +1,52 @@
+import React, { memo } from 'react';
+import { Flex, Text, FlexProps, FailedIcon, Box } from '@stacks/ui';
+import { IconClock } from '@tabler/icons';
+
+type TxStatus = 'failed' | 'pending' | 'confirmed';
+interface TxStatusLabelProps extends FlexProps {
+  status: TxStatus;
+}
+function TxStatusLabelBase({ status, ...props }: TxStatusLabelProps) {
+  return (
+    <Flex
+      flexDirection="row"
+      display="inline-flex"
+      border={`1px solid ${getStatusBorderColor(status)}`}
+      color={getStatusColor(status)}
+      borderRadius="24px"
+      height="loose"
+      justifyContent="center"
+      alignItems="center"
+      fontWeight="500"
+      px="tight"
+      {...props}
+    >
+      {status !== 'confirmed' && (
+        <Box mr="extra-tight">
+          {status === 'failed' && <FailedIcon size="13px" />}
+          {status === 'pending' && <IconClock size="14px" />}
+        </Box>
+      )}
+      <Text textStyle="caption">{getStatusText(status)}</Text>
+    </Flex>
+  );
+}
+
+//
+// Many of the colors here aren't in the color map
+function getStatusColor(status: TxStatus) {
+  const colorMap = { failed: '#C83532', pending: '#74777D', confirmed: '#008051' };
+  return colorMap[status];
+}
+
+function getStatusBorderColor(status: TxStatus) {
+  const colorMap = { failed: '#F7CDCA', pending: '#DCDDE2', confirmed: '#D9EDD4' };
+  return colorMap[status];
+}
+
+function getStatusText(status: TxStatus) {
+  const colorMap = { failed: 'Failed', pending: 'Pending', confirmed: 'Confirmed' };
+  return colorMap[status];
+}
+
+export const TxStatusLabel = memo(TxStatusLabelBase);
--- src/pages/transaction-details/transaction-details.tsx
+++ src/pages/transaction-details/transaction-details.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { Box, Stack } from '@stacks/ui';
 
 import { PopupContainer } from '@components/popup/container';
-import { useParseTxid } from './hooks/use-is-valid-txid';
 import { Header } from '@components/header';
+
+import { useParseTxid } from './hooks/use-is-valid-txid';
+import { TxStatusLabel } from './components/tx-status-label';
 
 export function TransactionDetails() {
   const { txid, isValidTxid } = useParseTxid();
   if (!isValidTxid) return <>{txid} is not a valid txid</>;
   return (
     <PopupContainer header={<Header />}>
-      <>{txid} is valid</>;
+      <Box maxWidth="392px">
+        <>{txid} is valid</>
+        <Stack isInline mt="base">
+          <TxStatusLabel status="confirmed" />
+          <TxStatusLabel status="pending" />
+          <TxStatusLabel status="failed" />
+        </Stack>
+      </Box>
     </PopupContainer>
   );
 }
