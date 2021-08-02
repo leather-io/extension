1a181a2c9dfc1ae72d9f6d51558de47d4cafca69
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-08-02 12:50:57 -0500

    fix: add network to api clients

--- src/store/common/api-clients.ts
+++ src/store/common/api-clients.ts
@@ -11,12 +11,13 @@ import {
 import { fetcher } from '@common/api/wrapped-fetch';
 import { MICROBLOCKS_ENABLED } from '@common/constants';
 import type { Middleware, RequestContext } from '@stacks/blockchain-api-client';
+import { atomFamily } from 'jotai/utils';
 
 // Used to display microblock, this passes unanchored=true to the search param
 const unanchoredMiddleware: Middleware = {
   pre: (context: RequestContext) => {
     const url = new URL(context.url);
-    url.searchParams.set('unanchored', 'true');
+    if (!url.toString().includes('/v2')) url.searchParams.set('unanchored', 'true');
     return Promise.resolve({
       init: context.init,
       url: url.toString(),
@@ -24,38 +25,44 @@ const unanchoredMiddleware: Middleware = {
   },
 };
 
-export const apiClientConfiguration = atom<Configuration>(get => {
-  const network = get(currentNetworkState);
-  const middleware: Middleware[] = [];
-  if (MICROBLOCKS_ENABLED) middleware.push(unanchoredMiddleware);
-  return new Configuration({
-    basePath: network.url,
-    fetchApi: fetcher,
-    middleware,
-  });
-});
+export const apiClientConfiguration = atomFamily<string, Configuration>(basePath =>
+  atom<Configuration>(_get => {
+    const middleware: Middleware[] = [];
+    if (MICROBLOCKS_ENABLED) middleware.push(unanchoredMiddleware);
+    return new Configuration({
+      basePath,
+      fetchApi: fetcher,
+      middleware,
+    });
+  })
+);
 
 export const smartContractClientState = atom<SmartContractsApi>(get => {
-  const config = get(apiClientConfiguration);
+  const network = get(currentNetworkState);
+  const config = get(apiClientConfiguration(network.url));
   return new SmartContractsApi(config);
 });
 
 export const accountsApiClientState = atom<AccountsApi>(get => {
-  const config = get(apiClientConfiguration);
+  const network = get(currentNetworkState);
+  const config = get(apiClientConfiguration(network.url));
   return new AccountsApi(config);
 });
 
 export const infoApiClientState = atom<InfoApi>(get => {
-  const config = get(apiClientConfiguration);
+  const network = get(currentNetworkState);
+  const config = get(apiClientConfiguration(network.url));
   return new InfoApi(config);
 });
 
 export const blocksApiClientState = atom<BlocksApi>(get => {
-  const config = get(apiClientConfiguration);
+  const network = get(currentNetworkState);
+  const config = get(apiClientConfiguration(network.url));
   return new BlocksApi(config);
 });
 
 export const feesApiClientState = atom<FeesApi>(get => {
-  const config = get(apiClientConfiguration);
+  const network = get(currentNetworkState);
+  const config = get(apiClientConfiguration(network.url));
   return new FeesApi(config);
 });
