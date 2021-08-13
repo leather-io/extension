9642d1e9e16c12afa2695228f3760783ce03aee0
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-08-13 07:54:23 -0500

    feat: persistence

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import { persistAndRenderApp } from '@common/persistence';
 
 function renderApp() {
   ReactDOM.render(<App />, document.getElementById('actions-root'));
 }
 
-renderApp();
+void persistAndRenderApp(renderApp);
