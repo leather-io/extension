655df5c2e1f1f95b7cbf6eed7b1c219de419d71b
kyranjamie <kyran.burraston@gmail.com>
2021-08-16 15:49:58 +0200

    feat: memo warnings, closes #1311

--- src/pages/receive-tokens/receive-tokens.tsx
+++ src/pages/receive-tokens/receive-tokens.tsx
@@ -1,5 +1,5 @@
-import React, { memo } from 'react';
-import { Box, Button, color, useClipboard, Flex, FlexProps, Stack } from '@stacks/ui';
+import React from 'react';
+import { Box, Button, useClipboard, Stack } from '@stacks/ui';
 import { PopupContainer } from '@components/popup/container';
 import { useDoChangeScreen } from '@common/hooks/use-do-change-screen';
 import { ScreenPaths } from '@common/types';
@@ -7,43 +7,10 @@ import { useWallet } from '@common/hooks/use-wallet';
 import { Toast } from '@components/toast';
 import { getAccountDisplayName } from '@stacks/wallet-sdk';
 import { Caption, Title } from '@components/typography';
-import { createQR } from '@vkontakte/vk-qr';
 import { truncateMiddle } from '@stacks/ui-utils';
 import { Tooltip } from '@components/tooltip';
-import { Header } from '@components/header';
-
-const QRcode: React.FC<{ principal: string } & FlexProps> = memo(({ principal, ...rest }) => {
-  const qrSvg = React.useMemo(
-    () =>
-      createQR(principal, {
-        ecc: 0,
-        qrSize: 180,
-        backgroundColor: color('text-body'),
-        foregroundColor: color('invert'),
-      }),
-    [principal]
-  );
-
-  const qr = <Box dangerouslySetInnerHTML={{ __html: qrSvg }} />; // Bad?
-
-  return (
-    <Flex
-      alignItems="center"
-      justifyContent="center"
-      p="loose"
-      borderRadius="18px"
-      boxShadow="mid"
-      border="1px solid"
-      borderColor={color('border')}
-      position="relative"
-      mx="auto"
-      {...rest}
-    >
-      {qr}
-      <Box position="absolute">{qr}</Box>
-    </Flex>
-  );
-});
+import { QrCode } from './components/address-qr-code';
+import { ReceiveTokensHeader } from './components/recieve-tokens-header';
 
 export const PopupReceive: React.FC = () => {
   const { currentAccount, currentAccountStxAddress } = useWallet();
@@ -52,11 +19,11 @@ export const PopupReceive: React.FC = () => {
   const { onCopy, hasCopied } = useClipboard(address);
   return (
     <PopupContainer
-      header={<Header title="Receive" onClose={() => doChangeScreen(ScreenPaths.POPUP_HOME)} />}
+      header={<ReceiveTokensHeader onClose={() => doChangeScreen(ScreenPaths.POPUP_HOME)} />}
     >
       <Toast show={hasCopied} />
       <Box mt="extra-loose" textAlign="center" mx="auto">
-        <QRcode principal={address} />
+        <QrCode principal={address} />
       </Box>
       <Stack spacing="base-loose" width="100%" mt="extra-loose" textAlign="center">
         {currentAccount && (
