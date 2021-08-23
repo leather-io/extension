2543d97fb5845fd2ee0fca00843c1637a3fcf4a6
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-08-23 11:56:39 -0500

    fix: fetch only current name account

--- src/features/current-user/current-user-name.tsx
+++ src/features/current-user/current-user-name.tsx
@@ -1,12 +1,12 @@
 import React, { memo } from 'react';
 import { BoxProps } from '@stacks/ui';
 import { useCurrentAccount } from '@common/hooks/account/use-current-account';
-import { useAccountNames } from '@common/hooks/account/use-account-names';
 import { getAccountDisplayName } from '@stacks/wallet-sdk';
 import { truncateString } from '@common/utils';
 import { Tooltip } from '@components/tooltip';
 import { Title } from '@components/typography';
 import { memoWithAs } from '@stacks/ui-core';
+import { useCurrentAccountDisplayName } from '@common/hooks/account/use-account-names';
 
 const UsernameTitle = (props: BoxProps) => (
   <Title
@@ -21,11 +21,9 @@ const UsernameTitle = (props: BoxProps) => (
 
 const UsernameSuspense = memo((props: BoxProps) => {
   const currentAccount = useCurrentAccount();
-  const names = useAccountNames();
+  const name = useCurrentAccountDisplayName();
   if (!currentAccount || typeof currentAccount.index === 'undefined') return null;
   const nameCharLimit = 18;
-  const name =
-    names?.[currentAccount.index]?.names?.[0] || getAccountDisplayName(currentAccount as any);
   const isLong = name.length > nameCharLimit;
   const displayName = truncateString(name, nameCharLimit);
 
