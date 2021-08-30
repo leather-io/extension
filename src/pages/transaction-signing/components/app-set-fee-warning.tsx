969de09ec62206f1fa481f4b738f4759c82d8f59
kyranjamie <kyran.burraston@gmail.com>
2021-08-30 18:21:46 +0100

    feat: add a fee warning when app has defined one

---
+++ src/pages/transaction-signing/components/app-set-fee-warning.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react';
+import { Flex, Box, color, Text } from '@stacks/ui';
+import { FiInfo } from 'react-icons/fi';
+import { microStxToStx } from '@stacks/ui-utils';
+
+interface AppSetFeeWarningProps {
+  uStxFee: number;
+}
+export const AppSetFeeWarning: FC<AppSetFeeWarningProps> = ({ uStxFee }) => {
+  return (
+    <Box background={color('bg-alt')} py="base" px="base-loose" borderRadius="10px">
+      <Flex>
+        <Box mr="base-tight" mt="2px">
+          <FiInfo color={color('accent')} />
+        </Box>
+        <Box>
+          <Text textStyle="body.small.medium">App has set fee of {microStxToStx(uStxFee)} STX</Text>
+          <Text
+            textStyle="body.small"
+            color={color('text-caption')}
+            lineHeight="22px"
+            mt="extra-tight"
+          >
+            Stacks app developers can set a fee to be used when broadcasting this transaction. This
+            is sometimes needed when calling a particularly expensive contract.
+          </Text>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
