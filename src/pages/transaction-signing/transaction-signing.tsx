fdcf4d7e421ae7fee9ad453fb6daad228289c0f4
kyranjamie <kyran.burraston@gmail.com>
2021-08-30 18:21:46 +0100

    feat: add a fee warning when app has defined one

--- src/pages/transaction-signing/transaction-signing.tsx
+++ src/pages/transaction-signing/transaction-signing.tsx
@@ -1,30 +1,31 @@
 import React, { memo, useEffect } from 'react';
 import { Stack } from '@stacks/ui';
 
+import {
+  useTransactionRequest,
+  useUpdateTransactionBroadcastError,
+} from '@store/transactions/requests.hooks';
 import { PopupHeader } from '@pages/transaction-signing/components/popup-header';
 import { PopupContainer } from '@components/popup/container';
 import { TransactionsActions } from '@pages/transaction-signing/components/actions';
-import { TransactionError } from './components/transaction-error';
 import { TransactionPageTop } from '@pages/transaction-signing/components/transaction-page-top';
 import { ContractCallDetails } from '@pages/transaction-signing/components/contract-call-details';
 import { ContractDeployDetails } from '@pages/transaction-signing/components/contract-deploy-details';
 import { PostConditions } from '@pages/transaction-signing/components/post-conditions/list';
 import { StxTransferDetails } from '@pages/transaction-signing/components/stx-transfer-details';
 import { PostConditionModeWarning } from '@pages/transaction-signing/components/post-condition-mode-warning';
-import {
-  useTransactionRequest,
-  useUpdateTransactionBroadcastError,
-} from '@store/transactions/requests.hooks';
+import { TransactionError } from './components/transaction-error';
+import { AppSetFeeWarning } from './components/app-set-fee-warning';
 
 export const TransactionPage = memo(() => {
   const transactionRequest = useTransactionRequest();
   const setBroadcastError = useUpdateTransactionBroadcastError();
+  // TODO: pull from data from connect
+  const appHasSetFee = true;
   if (!transactionRequest) return null;
 
   useEffect(() => {
-    return () => {
-      setBroadcastError(null);
-    };
+    return () => setBroadcastError(null);
   }, [setBroadcastError]);
 
   return (
@@ -37,6 +38,7 @@ export const TransactionPage = memo(() => {
         {transactionRequest.txType === 'contract_call' && <ContractCallDetails />}
         {transactionRequest.txType === 'token_transfer' && <StxTransferDetails />}
         {transactionRequest.txType === 'smart_contract' && <ContractDeployDetails />}
+        {appHasSetFee && <AppSetFeeWarning uStxFee={1999} />}
         <TransactionsActions />
       </Stack>
     </PopupContainer>
