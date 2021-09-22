ce957d028f3f9f09e92c8a72e0025053b58b027d
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-09-22 08:38:08 -0500

    refactor: move balances-activity to feature

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { Box, Stack } from '@stacks/ui';
 import { PopupContainer } from '@components/popup/container';
 import { Header } from '@components/header';
-import { BalancesAndActivity } from '@components/popup/balances-and-activity';
+import { BalancesAndActivity } from '@features/balances-and-activity';
 import { UserAccount } from '@pages/home/components/user-area';
 import { HomeActions } from '@pages/home/components/actions';
 import { HiroMessages } from '@features/hiro-messages/hiro-messages';
