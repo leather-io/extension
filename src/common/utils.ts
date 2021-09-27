db019b242e11ab6017c9e3edba2b2fa0fb772d64
Thomas Osmonson <aulneau@users.noreply.github.com>
2021-09-27 13:02:37 -0500

    fix: default empty string

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -152,7 +152,7 @@ export function validateAndCleanRecoveryInput(value: string) {
 
 export const hasLineReturn = (input: string) => input.includes('\n');
 
-export function makeTxExplorerLink(txid: string, chain: 'mainnet' | 'testnet', suffix?: string) {
+export function makeTxExplorerLink(txid: string, chain: 'mainnet' | 'testnet', suffix = '') {
   return `https://explorer.stacks.co/txid/${txid}?chain=${chain}${suffix}`;
 }
 
