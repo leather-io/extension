8c0f5c40841958ae7455593b8ea9f61a922aab19
fbwoolf <fwoolf@blockstack.com>
2021-10-08 16:03:43 -0500

    fix: webpack dev server

--- webpack/dev-server.js
+++ webpack/dev-server.js
@@ -14,16 +14,17 @@ const config = require('./webpack.config.dev');
 
 // This is important, allows for fast refresh to work
 // we don't want to inject our fast refresh helpers into everything
-const excludeEntriesToHotReload = ['content-script.js'];
+const excludeEntriesToHotReload = ['content-script'];
 
 const NODE_ENV = process.env.NODE_ENV;
 
+const HOST = 'localhost'
 const PORT = process.env.PORT || '8080';
 
 Object.keys(config.entry).forEach(entryName => {
   if (excludeEntriesToHotReload.indexOf(entryName) === -1 && config.entry) {
     config.entry[entryName] = [
-      'webpack-dev-server/client?http://localhost:' + PORT,
+      `webpack-dev-server/client?hot=true&live-reload=true&hostname=${HOST}&port=${PORT}`,
       'webpack/hot/dev-server',
     ].concat(config.entry[entryName]);
   }
@@ -36,24 +37,32 @@ config.plugins = [
 
 const compiler = webpack(config);
 
-const server = new WebpackDevServer(compiler, {
+const server = new WebpackDevServer({
   https: false,
-  transportMode: 'ws',
-  hot: true,
-  injectClient: false,
-  writeToDisk: true,
+  webSocketServer: 'ws',
+  // We disable hot bc we do a manual setup for specific entries
+  hot: false,
+  // We disable client bc we do a manual setup for specific entries
+  client: false,
   port: process.env.PORT,
-  contentBase: path.join(__dirname, '../build'),
-  publicPath: `http://localhost:${process.env.PORT}`,
+  static: {
+    directory: path.join(__dirname, '../build'),
+  },
   headers: {
     'Access-Control-Allow-Origin': '*',
   },
-  disableHostCheck: true,
-  stats: 'errors-only',
-});
+  allowedHosts: "all",
+  devMiddleware: {
+    publicPath: `http://localhost:${process.env.PORT}`,
+    stats: 'errors-only',
+    writeToDisk: true,
+  },
+}, compiler);
 
 if (NODE_ENV === 'development' && module.hot) {
   module.hot.accept();
 }
 
-server.listen(process.env.PORT);
+server.startCallback(() => {
+  console.log("Starting server on http://localhost:8080");
+});
