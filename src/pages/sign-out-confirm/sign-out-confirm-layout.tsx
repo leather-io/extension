0c423476b32cf16689b951728950a245821d4e6d
andresgalante <agalante@gmail.com>
2021-10-12 09:19:05 -0300

    chore: improves the wording on the sign out message

--- src/pages/sign-out-confirm/sign-out-confirm-layout.tsx
+++ src/pages/sign-out-confirm/sign-out-confirm-layout.tsx
@@ -25,7 +25,7 @@ export const SignOutConfirmLayout: FC<SignOutConfirmLayoutProps> = props => {
       <Box mx="loose" mb="extra-loose">
         <form onChange={form.handleChange} onSubmit={form.handleSubmit}>
           <Body>
-            When you sign out, you’ll need your 24-word Secret Key to sign back in. Only sign out if
+            When you sign out, you’ll need your Secret Key to sign back in. Only sign out if
             you’ve backed up your Secret Key.
           </Body>
 
@@ -37,7 +37,7 @@ export const SignOutConfirmLayout: FC<SignOutConfirmLayoutProps> = props => {
                 data-testid={SettingsSelectors.SignOutConfirmHasBackupCheckbox}
               />
             </Box>
-            <Caption userSelect="none">I've backed up my 24-word Secret Key</Caption>
+            <Caption userSelect="none">I've backed up my Secret Key</Caption>
           </Flex>
           <Flex mt="loose">
             <Button
