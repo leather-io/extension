7870818451a443e3ef95cec826db656cc67de331
kyranjamie <kyran.burraston@gmail.com>
2021-10-19 11:32:13 +0200

    chore(bns): add small BNS tab with link to test app, closes #1838

---
+++ test-app/src/components/bns.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box, Button, Text } from '@stacks/ui';
+
+export const Bns = () => {
+  return (
+    <Box py={6}>
+      <Text as="h2" textStyle="display.small">
+        BNS
+      </Text>
+      <Text textStyle="body.large" display="block" my={'loose'}>
+        Use the testnet version of btc.us to QA BNS names
+      </Text>
+      <Button onClick={() => window.open('https://btc-website.tintashlabs.com/')}>
+        Open testnet btc.us
+      </Button>
+    </Box>
+  );
+};
