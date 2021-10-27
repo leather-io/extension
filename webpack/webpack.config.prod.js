eb298d20d969bc39e481001ca47fb3b3dfe558e4
kyranjamie <kyran.burraston@gmail.com>
2021-10-27 12:12:07 +0200

    fix(sentry): use sentry sourcemap plugin

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const config = require('./webpack.config.base');
 const { ESBuildMinifyPlugin } = require('esbuild-loader');
-
+const SentryCliPlugin = require('@sentry/webpack-plugin');
 const shouldMinify = JSON.parse(process.env.MINIFY_PRODUCTION_BUILD || false);
+const package = require('../package.json');
 
 config.mode = 'production';
 
@@ -30,6 +31,22 @@ config.optimization = {
     : {}),
 };
 
-config.devtool = 'inline-source-map';
+config.devtool = 'source-map';
+
+if (
+  process.env.CI &&
+  (process.env.WALLET_ENVIRONMENT === 'production' ||
+    process.env.WALLET_ENVIRONMENT === 'development')
+) {
+  console.log('Using Sentry Sourcemap plugin');
+  config.plugins = [
+    ...config.plugins,
+    new SentryCliPlugin({
+      include: 'dist',
+      ignore: ['node_modules'],
+      release: package.version,
+    }),
+  ];
+}
 
 module.exports = config;
