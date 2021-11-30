5f93f94fcf30f302523257bb77b08b0418ff8178
kyranjamie <kyran.burraston@gmail.com>
2021-11-30 20:00:03 +0100

    test(tx-signing): fix dead test app

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -3,8 +3,8 @@ import { setup } from 'jest-dev-server';
 // ts-unused-exports:disable-next-line
 export default async function globalSetup() {
   await setup({
-    command: 'yarn test:serve',
-    launchTimeout: 10000,
-    port: 3001,
+    command: 'yarn dev:test-app',
+    launchTimeout: 15000,
+    port: 3000,
   });
 }
