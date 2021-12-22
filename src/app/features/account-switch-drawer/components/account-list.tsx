8464b7497f48552128584aaf4791478ab17257a7
kyranjamie <kyran.burraston@gmail.com>
2021-12-22 12:44:05 +0000

    fix: render whole account list when fewer than 10 accounts

--- src/app/features/account-switch-drawer/components/account-list.tsx
+++ src/app/features/account-switch-drawer/components/account-list.tsx
@@ -4,6 +4,8 @@ import { Virtuoso } from 'react-virtuoso';
 import { AccountWithAddress } from '@app/store/accounts/account.models';
 import { AccountListItem } from './account-list-item';
 
+const smallNumberOfAccountsToRenderWholeList = 10;
+
 interface AccountListProps {
   handleClose: () => void;
   accounts: AccountWithAddress[];
@@ -11,6 +13,16 @@ interface AccountListProps {
 }
 export const AccountList = memo(
   ({ accounts, currentAccountIndex, handleClose }: AccountListProps) => {
+    if (accounts.length <= smallNumberOfAccountsToRenderWholeList) {
+      return (
+        <>
+          {accounts.map(account => (
+            <AccountListItem handleClose={handleClose} account={account} key={account.address} />
+          ))}
+        </>
+      );
+    }
+
     return (
       <Virtuoso
         initialTopMostItemIndex={currentAccountIndex}
