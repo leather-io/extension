9e183c18194547b702407c5066dab02f09d5a3bd
fbwoolf <fwoolf@blockstack.com>
2021-12-22 17:19:02 -0600

    refactor: global styles

--- src/app/pages/onboarding/welcome/welcome.layout.tsx
+++ src/app/pages/onboarding/welcome/welcome.layout.tsx
@@ -1,12 +1,19 @@
+import { css, cx } from '@emotion/css';
 import { Button, Stack } from '@stacks/ui';
 
 import { Text, Title } from '@app/components/typography';
 import { Link } from '@app/components/link';
-import ExploreStacksLarge from '@assets/images/explore-stacks-lg.svg';
-import ExploreStacksSmall from '@assets/images/explore-stacks-sm.svg';
+import WelcomeStacksFull from '@assets/images/onboarding/welcome-full.svg';
+import WelcomeStacksPopup from '@assets/images/onboarding/welcome-popup.svg';
 import { InitialPageSelectors } from '@tests/integration/initial-page.selectors';
 import { getViewMode } from '@app/common/utils';
 
+import {
+  fullPageContentImage,
+  fullPageContentText,
+  fullPageTitle,
+  popupTitle,
+} from './welcome.styles';
 interface WelcomeLayoutProps {
   isGeneratingWallet: boolean;
   onStartOnboarding(): void;
@@ -15,16 +22,24 @@ interface WelcomeLayoutProps {
 export function WelcomeLayout(props: WelcomeLayoutProps): JSX.Element {
   const { isGeneratingWallet, onStartOnboarding, onRestoreWallet } = props;
   const mode = getViewMode();
+  const isFullPage = mode === 'full';
+  const isPopup = mode === 'popup';
 
   return (
-    <Stack className="welcome-page" isInline={mode === 'full'} width="100%">
-      <Stack className="content-image" flexGrow={1}>
-        <img src={ExploreStacksLarge} className="image-large" />
-        <img src={ExploreStacksSmall} className="image-small" />
+    <Stack isInline={isFullPage} width="100%">
+      <Stack className={cx({ [fullPageContentImage]: isFullPage })} flexGrow={1}>
+        {isFullPage ? <img src={WelcomeStacksFull} /> : <img src={WelcomeStacksPopup} />}
       </Stack>
-      <Stack className="content-text" flexGrow={1} justifyContent="center">
+      <Stack
+        className={cx({ [fullPageContentText]: isFullPage })}
+        flexGrow={1}
+        justifyContent="center"
+      >
         <Stack width="100%" textAlign="left" alignItems="start">
-          <Title className="title" fontWeight={500}>
+          <Title
+            className={cx({ [fullPageTitle]: isFullPage }, { [popupTitle]: isPopup })}
+            fontWeight={500}
+          >
             Explore the world of Stacks
           </Title>
           <Text className="text">
