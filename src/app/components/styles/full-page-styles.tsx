a90b322f0be4e3e44c6de9dec175447df22fa425
fbwoolf <fwoolf@blockstack.com>
2021-12-21 19:21:35 -0600

    feat: add welcome page

---
+++ src/app/components/styles/full-page-styles.tsx
@@ -0,0 +1,47 @@
+import { css } from '@emotion/react';
+
+export const fullPageStyles = css`
+  .mode__full-page {
+    &,
+    body {
+      height: 100%;
+      max-height: unset;
+      width: 100%;
+    }
+    main.main-content {
+      flex-direction: row;
+      flex-grow: 1;
+      justify-content: center;
+      margin: 0 auto;
+    }
+    .temp {
+      max-width: 440px;
+    }
+    .welcome-page {
+      .content-image {
+        order: 2;
+        height: calc(100vh - 102px);
+        .image-small {
+          display: none;
+        }
+      }
+      .content-text {
+        order: 1;
+        margin-left: 168px;
+        .title {
+          max-width: 500px;
+          font-size: 64px;
+          margin-bottom: 24px;
+        }
+        .text {
+          max-width: 440px;
+          font-size: 16px;
+          margin-bottom: 24px;
+        }
+      }
+    }
+    .onboarding-text {
+      text-align: center;
+    }
+  }
+`;
