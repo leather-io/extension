cdb3fbd37f39db336dbd09da20e84adc34d22fab
fbwoolf <fwoolf@blockstack.com>
2021-12-23 15:05:35 -0600

    feat: add back up secret key page

--- src/app/pages/save-secret-key/save-secret-key.tsx
+++ src/app/pages/save-secret-key/save-secret-key.tsx
@@ -13,26 +13,18 @@ import { SecretKeyMessage } from './components/secret-key-message';
 import { SecretKeyCard } from './components/secret-key-card';
 
 export const SaveSecretKey = memo(() => {
-  const { hasSetPassword, secretKey } = useWallet();
+  const { secretKey } = useWallet();
   const analytics = useAnalytics();
   const navigate = useNavigate();
 
-  useRouteHeader(
-    <Header
-      onClose={
-        hasSetPassword ? () => navigate(RouteUrls.Home) : () => navigate(RouteUrls.Onboarding)
-      }
-      hideActions={!hasSetPassword}
-      title={hasSetPassword ? 'Your Secret Key' : 'Save your Secret Key'}
-    />
-  );
+  useRouteHeader(<Header onClose={() => navigate(RouteUrls.Home)} title={'Your Secret Key'} />);
 
   useEffect(() => {
     if (!secretKey) navigate(RouteUrls.Onboarding);
   }, [navigate, secretKey]);
 
   useEffect(() => {
-    void analytics.page('view', '/save-your-secret-key');
+    void analytics.page('view', '/save-secret-key');
   }, [analytics]);
 
   return (
