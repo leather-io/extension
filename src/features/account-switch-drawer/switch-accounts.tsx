50e2b9849be798bdae0af53ca3b91d1cc24902c2
Fara Woolf <fwoolf@blockstack.com>
2022-01-11 14:45:40 -0600

    Revert "fix: null rendering when accounts are undefined, closes #2000"
    This reverts commit 5d95af09622a7ebe7470a29a1a1384aea4511b31.

--- src/features/account-switch-drawer/switch-accounts.tsx
+++ src/features/account-switch-drawer/switch-accounts.tsx
@@ -1,4 +1,5 @@
 import React, { memo } from 'react';
+import { Box, Button } from '@stacks/ui';
 
 import { useUpdateAccountDrawerStep } from '@store/ui/ui.hooks';
 import { AccountStep } from '@store/ui/ui.models';
@@ -6,8 +7,6 @@ import { useAccounts } from '@store/accounts/account.hooks';
 import { useAnalytics } from '@common/hooks/analytics/use-analytics';
 
 import { AccountList } from './components/account-list';
-import { AccountListUnavailable } from './components/account-list-unavailable';
-import { CreateAccountAction } from './components/create-account-action';
 
 interface SwitchAccountProps {
   close(): void;
@@ -16,21 +15,17 @@ export const SwitchAccounts = memo(({ close }: SwitchAccountProps) => {
   const setAccountDrawerStep = useUpdateAccountDrawerStep();
   const accounts = useAccounts();
   const analytics = useAnalytics();
-
   const setCreateAccountStep = () => {
     void analytics.track('choose_to_create_account');
     setAccountDrawerStep(AccountStep.Create);
   };
 
-  if (!accounts) {
-    void analytics.track('account_list_unavailable_warning_displayed');
-    return <AccountListUnavailable />;
-  }
-
   return (
     <>
-      <AccountList accounts={accounts} handleClose={close} />
-      <CreateAccountAction onCreateAccount={() => setCreateAccountStep()} />
+      {accounts ? <AccountList accounts={accounts} handleClose={close} /> : null}
+      <Box pt="base" pb="loose" px="loose">
+        <Button onClick={setCreateAccountStep}>Create an account</Button>
+      </Box>
     </>
   );
 });
