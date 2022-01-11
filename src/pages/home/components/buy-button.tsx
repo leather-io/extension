0cea331c0b7931dc3b466eb3a1fee971d9f24893
Fara Woolf <fwoolf@blockstack.com>
2022-01-11 14:45:40 -0600

    Revert "fix: show fiat onramp buy button only on mainnet"
    This reverts commit c02f1382186af0e7ae2da9b5d40216c73d9d14fa.

--- src/pages/home/components/buy-button.tsx
+++ src/pages/home/components/buy-button.tsx
@@ -1,17 +1,14 @@
 import React, { memo, Suspense } from 'react';
-import { ChainID } from '@stacks/transactions';
+
 import { useHasFiatProviders } from '@query/hiro-config/hiro-config.query';
+
 import { BuyTxButton } from './tx-button';
-import { useCurrentNetworkState } from '@store/network/networks.hooks';
 
 const BuyButtonFallback = memo(() => <BuyTxButton isDisabled />);
 
 export const BuyButton = () => {
   const hasFiatProviders = useHasFiatProviders();
-  const currentNetwork = useCurrentNetworkState();
   if (!hasFiatProviders) return null;
-  if (currentNetwork.chainId !== ChainID.Mainnet) return null;
-
   return (
     <Suspense fallback={<BuyButtonFallback />}>
       <BuyTxButton />
