72bddc8408492b3797caf2497a4f4eea63f46d7e
fbwoolf <fwoolf@blockstack.com>
2022-01-22 16:34:43 -0600

    fix: sponsored tx fee

---
+++ src/app/components/sponsored-label.tsx
@@ -0,0 +1,27 @@
+import { FiAlertCircle } from 'react-icons/fi';
+import { Box, color, Stack, Text } from '@stacks/ui';
+
+export function SponsoredLabel(): JSX.Element {
+  return (
+    <Stack width="100%">
+      <Stack
+        alignItems="center"
+        bg={color('bg-4')}
+        borderRadius="10px"
+        height="48px"
+        isInline
+        pl="base"
+      >
+        <Box
+          _hover={{ cursor: 'pointer' }}
+          as={FiAlertCircle}
+          color={color('accent')}
+          size="16px"
+        />
+        <Text color={color('text-title')} fontSize="12px" fontWeight="500">
+          This transaction is sponsored, so no fee is charged
+        </Text>
+      </Stack>
+    </Stack>
+  );
+}
