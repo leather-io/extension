53c81164cf957f262cc96d59bcf2b3c534cac68e
fbwoolf <fwoolf@blockstack.com>
2022-01-31 17:10:53 -0600

    fix: missing account balance

--- src/app/pages/sign-transaction/components/balance.tsx
+++ src/app/pages/sign-transaction/components/balance.tsx
@@ -1,16 +1,24 @@
+import { useMemo } from 'react';
+
 import { stacksValue } from '@app/common/stacks-utils';
 import { Caption } from '@app/components/typography';
-import { useCurrentAccountInfo } from '@app/store/accounts/account.hooks';
+import { useAddressAvailableStxBalance } from '@app/query/balance/balance.hooks';
 
-export function Balance(): JSX.Element | null {
-  const info = useCurrentAccountInfo();
+interface BalanceProps {
+  address: string;
+}
+export function Balance(props: BalanceProps): JSX.Element | null {
+  const { address } = props;
+  const availableStxBalance = useAddressAvailableStxBalance(address);
 
-  return info?.balance ? (
-    <Caption>
-      {stacksValue({
-        value: info.balance.toString(),
+  const balance = useMemo(
+    () =>
+      stacksValue({
+        value: availableStxBalance || 0,
         withTicker: true,
-      })}
-    </Caption>
-  ) : null;
+      }),
+    [availableStxBalance]
+  );
+
+  return <Caption>{balance}</Caption>;
 }
