8b8d48439371c5099e250b8986a5376667b21d3b
kyranjamie <kyran.burraston@gmail.com>
2022-02-04 10:01:41 +0100

    refactor: update secret key to support new design

--- src/app/pages/onboarding/back-up-secret-key/back-up-secret-key.tsx
+++ src/app/pages/onboarding/back-up-secret-key/back-up-secret-key.tsx
@@ -4,14 +4,26 @@ import { useNavigate } from 'react-router-dom';
 import { RouteUrls } from '@shared/route-urls';
 import { BackUpSecretKeyLayout } from './back-up-secret-key.layout';
 import { useGeneratedSecretKey } from '@app/store/keys/key.selectors';
+import { useRouteHeader } from '@app/common/hooks/use-route-header';
+import { Header } from '@app/components/header';
+import { SecretKeyDisplayer } from '@app/features/secret-key-displayer/secret-key-displayer';
 
 export const BackUpSecretKeyPage = memo(() => {
   const secretKey = useGeneratedSecretKey();
   const navigate = useNavigate();
 
+  useRouteHeader(<Header hideActions />);
+
   useEffect(() => {
     if (!secretKey) navigate(RouteUrls.Onboarding);
   }, [navigate, secretKey]);
 
-  return <BackUpSecretKeyLayout onBackedUpSecretKey={() => navigate(RouteUrls.SetPassword)} />;
+  if (!secretKey) return null;
+
+  return (
+    <BackUpSecretKeyLayout
+      secretKeyDisplay={<SecretKeyDisplayer secretKey={secretKey} />}
+      onBackedUpSecretKey={() => navigate(RouteUrls.SetPassword)}
+    />
+  );
 });
