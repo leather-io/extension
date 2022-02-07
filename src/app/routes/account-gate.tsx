4d8587fdd56ce52dbaba7e43b471eeb7100a0256
kyranjamie <kyran.burraston@gmail.com>
2022-02-07 13:08:37 +0100

    refactor: only run devtools in dev

--- src/app/routes/account-gate.tsx
+++ src/app/routes/account-gate.tsx
@@ -5,6 +5,21 @@ import { RouteUrls } from '@shared/route-urls';
 import { useCurrentKeyDetails, useGeneratedSecretKey } from '@app/store/keys/key.selectors';
 import { useDefaultWalletSecretKey } from '@app/store/in-memory-key/in-memory-key.selectors';
 
+export function shouldNavigateBackToBackupSecretKeyPage(
+  currentKeyDetails: any,
+  yetToBeEncryptedKey?: string | null
+) {
+  return !currentKeyDetails && yetToBeEncryptedKey;
+}
+
+export function shouldNavigateToOnboardingStartPage(currentKeyDetails?: any) {
+  return !currentKeyDetails;
+}
+
+export function shouldNavigateToUnlockWalletPage(currentInMemorySecretKey?: string) {
+  return !currentInMemorySecretKey;
+}
+
 interface AccountGateProps {
   children?: ReactNode;
 }
@@ -13,13 +28,14 @@ export const AccountGate = ({ children }: AccountGateProps) => {
   const yetToBeEncryptedKey = useGeneratedSecretKey();
   const currentInMemorySecretKey = useDefaultWalletSecretKey();
 
-  const generatedKeyButNotYetEncrypted = !currentKeyDetails && yetToBeEncryptedKey;
-  const newlyCreatedWallet = !currentKeyDetails;
-  const hasGeneratedWalletButItsLocked = !currentInMemorySecretKey;
+  if (shouldNavigateBackToBackupSecretKeyPage(currentKeyDetails, yetToBeEncryptedKey))
+    return <Navigate to={RouteUrls.BackUpSecretKey} />;
 
-  if (generatedKeyButNotYetEncrypted) return <Navigate to={RouteUrls.BackUpSecretKey} />;
-  if (newlyCreatedWallet) return <Navigate to={RouteUrls.Onboarding} />;
-  if (hasGeneratedWalletButItsLocked) return <Navigate to={RouteUrls.Unlock} />;
+  if (shouldNavigateToOnboardingStartPage(currentKeyDetails))
+    return <Navigate to={RouteUrls.Onboarding} />;
+
+  if (shouldNavigateToUnlockWalletPage(currentInMemorySecretKey))
+    return <Navigate to={RouteUrls.Unlock} />;
 
   return <>{children}</>;
 };
