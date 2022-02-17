c27ff94e826cd267c910618f5e6d010ce701ec2b
kyranjamie <kyran.burraston@gmail.com>
2022-02-17 13:31:44 +0100

    refactor: settings dropdown, ledger support

---
+++ src/app/features/settings-dropdown/components/settings-menu-item.tsx
@@ -0,0 +1,23 @@
+import { memo } from 'react';
+import { BoxProps, Text, color } from '@stacks/ui';
+
+export const SettingsMenuItem = memo((props: BoxProps) => {
+  const { onClick, children, ...rest } = props;
+  return (
+    <Text
+      width="100%"
+      px="base"
+      py="base"
+      cursor="pointer"
+      color={color('text-title')}
+      _hover={{ backgroundColor: color('bg-4') }}
+      onClick={e => {
+        onClick?.(e);
+      }}
+      fontSize={1}
+      {...rest}
+    >
+      {children}
+    </Text>
+  );
+});
