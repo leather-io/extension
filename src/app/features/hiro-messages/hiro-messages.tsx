4f98430d8ca5c546363f8350c83be493423bd1ee
kyranjamie <kyran.burraston@gmail.com>
2022-02-23 14:15:21 +0100

    feat: add testnet survey msg, closes #2253

--- src/app/features/hiro-messages/hiro-messages.tsx
+++ src/app/features/hiro-messages/hiro-messages.tsx
@@ -1,16 +1,25 @@
 import { Box, Flex, FlexProps } from '@stacks/ui';
 
-import { useRemoteHiroConfig } from '@app/query/hiro-config/hiro-config.query';
+import { useRemoteHiroMessages } from '@app/query/hiro-config/hiro-config.query';
 import { HiroMessageItem } from './components/hiro-message-item';
+import { useCurrentNetwork } from '@app/common/hooks/use-current-network';
 
 export const HiroMessages = (props: FlexProps) => {
-  const config = useRemoteHiroConfig();
-  if (!config?.messages?.global?.[0]) return null;
+  const messages = useRemoteHiroMessages();
+  const network = useCurrentNetwork();
+
+  if (!messages || messages?.length === 0) return null;
+
+  const firstMessage = messages[0];
+
+  if (firstMessage.chainTarget !== 'all' && firstMessage.chainTarget !== network.mode) {
+    return null;
+  }
 
   return (
     <Box pt="tight">
       <Flex background="#F7F8FD" borderRadius="8px" p="base" {...props}>
-        <HiroMessageItem {...config.messages.global[0]} />
+        <HiroMessageItem {...firstMessage} />
       </Flex>
     </Box>
   );
