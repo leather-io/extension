Ceramicwhite/stacks-wallet-web

feat: add new onboarding home page

tests/integration/onboarding/onboarding.spec.ts
@@ -22,8 +22,13 @@ describe(`Onboarding integration tests`, () => {
     } catch (error) {}
   });
 
-  it('should be able to sign up from welcome page', async () => {
+  it('should be able to allow analytics', async () => {
     await wallet.clickAllowAnalytics();
+    await wallet.waitForWelcomPage();
+  });
+
+  it('should be able to sign up from welcome page', async () => {
+    await wallet.clickDenyAnalytics();
     await wallet.clickSignUp();
     await wallet.backUpKeyAndSetPassword();
     await wallet.waitForHomePage();
@@ -34,7 +39,7 @@ describe(`Onboarding integration tests`, () => {
   });
 
   it('should be able to login from welcome page then logout', async () => {
-    await wallet.clickAllowAnalytics();
+    await wallet.clickDenyAnalytics();
     await wallet.clickSignIn();
     await wallet.loginWithPreviousSecretKey(SECRET_KEY);
     await wallet.waitForHomePage();
@@ -50,7 +55,7 @@ describe(`Onboarding integration tests`, () => {
   });
 
   it('should route to unlock page if the wallet is locked', async () => {
-    await wallet.clickAllowAnalytics();
+    await wallet.clickDenyAnalytics();
     await wallet.clickSignUp();
     await wallet.backUpKeyAndSetPassword();
     await wallet.waitForHomePage();
@@ -77,5 +82,55 @@ describe(`Onboarding integration tests`, () => {
     await wallet.waitForHomePage();
     const homePageVisible = await wallet.page.isVisible(wallet.$homePageBalancesList);
     expect(homePageVisible).toBeTruthy();
+  });
+
+  it('should show onboarding steps for a new wallet with only one account', async () => {
+    await wallet.clickDenyAnalytics();
+    await wallet.clickSignUp();
+    await wallet.backUpKeyAndSetPassword();
+    await wallet.waitForHomePage();
+    await wallet.waitForHideOnboardingsStepsButton();
+  });
+
+  it('should hide onboarding steps when signing in with an existing secret key', async () => {
+    await wallet.clickDenyAnalytics();
+    await wallet.clickSignIn();
+    await wallet.loginWithPreviousSecretKey(SECRET_KEY);
+    await wallet.waitForHomePage();
+    const onboardingStepsVisible = await wallet.page.isVisible(wallet.$onboardingStepsList);
+    expect(onboardingStepsVisible).toBeFalsy();
+  });
+
+  it('should hide onboarding steps if user clicks the button to hide them', async () => {
+    await wallet.clickDenyAnalytics();
+    await wallet.clickSignUp();
+    await wallet.backUpKeyAndSetPassword();
+    await wallet.waitForHomePage();
+    await wallet.waitForHideOnboardingsStepsButton();
+    await wallet.page.click(wallet.$hideStepsBtn);
+    const onboardingStepsVisible = await wallet.page.isVisible(wallet.$onboardingStepsList);
+    expect(onboardingStepsVisible).toBeFalsy();
+  });
+
+  it('should be able to start and complete an onboarding step in the list', async () => {
+    await wallet.clickDenyAnalytics();
+    await wallet.clickSignUp();
+    await wallet.backUpKeyAndSetPassword();
+    await wallet.waitForHomePage();
+    await wallet.waitForOnboardingStepsList();
+    const stepsToStartBtns = await wallet.page.$$(wallet.$onboardingStepStartBtn);
+    await stepsToStartBtns[2].click();
+    const stepsDone = await wallet.page.$$(wallet.$onboardingStepDoneBadge);
+    expect(stepsDone.length).toEqual(2);
+  });
+
+  it('should be able to use the link to add funds if balances list has no assets', async () => {
+    await wallet.clickDenyAnalytics();
+    await wallet.clickSignUp();
+    await wallet.backUpKeyAndSetPassword();
+    await wallet.waitForHomePage();
+    await wallet.waitForOnboardingStepsList();
+    const noAssetsFundAccountLink = await wallet.page.$(wallet.$noAssetsFundAccountLink);
+    await noAssetsFundAccountLink?.click();
   });
 });

tests/page-objects/wallet.page.ts
@@ -1,7 +1,7 @@
 import { Page } from 'playwright-core';
 
 import { RouteUrls } from '@shared/route-urls';
-import { OnboardingSelectors } from '@tests/integration/onboarding.selectors';
+import { OnboardingSelectors } from '@tests/integration/onboarding/onboarding.selectors';
 import { HomePageSelectors } from '@tests/page-objects/home-page.selectors';
 import { SettingsSelectors } from '@tests/integration/settings.selectors';
 import { BuyTokensSelectors } from '@tests/page-objects/buy-tokens-selectors';
@@ -50,6 +50,11 @@ export class WalletPage {
   $enterPasswordInput = createTestSelector(SettingsSelectors.EnterPasswordInput);
   $unlockWalletBtn = createTestSelector(SettingsSelectors.UnlockWalletBtn);
   $magicRecoveryMessage = createTestSelector(WalletPageSelectors.MagicRecoveryMessage);
+  $hideStepsBtn = createTestSelector(OnboardingSelectors.HideStepsBtn);
+  $onboardingStepsList = createTestSelector(OnboardingSelectors.StepsList);
+  $onboardingStepStartBtn = createTestSelector(OnboardingSelectors.StepItemStart);
+  $onboardingStepDoneBadge = createTestSelector(OnboardingSelectors.StepItemDone);
+  $noAssetsFundAccountLink = createTestSelector(OnboardingSelectors.NoAssetsFundAccountLink);
 
   page: Page;
 
@@ -79,6 +84,10 @@ export class WalletPage {
 
   async clickDenyAnalytics() {
     await this.page.click(this.$analyticsDenyButton);
+  }
+
+  async waitForWelcomPage() {
+    await this.page.waitForSelector(OnboardingSelectors.SignUpBtn);
   }
 
   async clickSignUp() {
@@ -127,6 +136,14 @@ export class WalletPage {
 
   async waitForStatusMessage() {
     await this.page.waitForSelector(this.$statusMessage, { timeout: 20000 });
+  }
+
+  async waitForHideOnboardingsStepsButton() {
+    await this.page.waitForSelector(this.$hideStepsBtn, { timeout: 30000 });
+  }
+
+  async waitForOnboardingStepsList() {
+    await this.page.waitForSelector(this.$onboardingStepsList, { timeout: 30000 });
   }
 
   async loginWithPreviousSecretKey(secretKey: string) {
@@ -206,14 +223,14 @@ export class WalletPage {
 
   /** Sign up with a randomly generated seed phrase */
   async signUp() {
-    await this.clickAllowAnalytics();
+    await this.clickDenyAnalytics();
     await this.clickSignUp();
     await this.backUpKeyAndSetPassword();
     await this.waitForHomePage();
   }
 
   async signIn(secretKey: string) {
-    await this.clickAllowAnalytics();
+    await this.clickDenyAnalytics();
     await this.clickSignIn();
     let startTime = new Date();
     await this.enterSecretKey(secretKey);

leather-io/extension

tests/integration/onboarding/onboarding.spec.ts
@@ -22,8 +22,13 @@ describe(`Onboarding integration tests`, () => {
     } catch (error) {}
   });
 
-  it('should be able to sign up from welcome page', async () => {
+  it('should be able to allow analytics', async () => {
     await wallet.clickAllowAnalytics();
+    await wallet.waitForWelcomePage();
+  });
+
+  it('should be able to sign up from welcome page', async () => {
+    await wallet.clickDenyAnalytics();
     await wallet.clickSignUp();
     await wallet.backUpKeyAndSetPassword();
     await wallet.waitForHomePage();
@@ -34,7 +39,7 @@ describe(`Onboarding integration tests`, () => {
   });
 
   it('should be able to login from welcome page then logout', async () => {
-    await wallet.clickAllowAnalytics();
+    await wallet.clickDenyAnalytics();
     await wallet.clickSignIn();
     await wallet.loginWithPreviousSecretKey(SECRET_KEY);
     await wallet.waitForHomePage();
@@ -46,11 +51,11 @@ describe(`Onboarding integration tests`, () => {
     await wallet.page.click('text=Sign Out');
     await wallet.page.click(wallet.$signOutConfirmHasBackupCheckbox);
     await wallet.page.click(wallet.$signOutDeleteWalletBtn);
-    await wallet.waitForLoginPage();
+    await wallet.waitForWelcomePage();
   });
 
   it('should route to unlock page if the wallet is locked', async () => {
-    await wallet.clickAllowAnalytics();
+    await wallet.clickDenyAnalytics();
     await wallet.clickSignUp();
     await wallet.backUpKeyAndSetPassword();
     await wallet.waitForHomePage();
@@ -77,5 +82,53 @@ describe(`Onboarding integration tests`, () => {
     await wallet.waitForHomePage();
     const homePageVisible = await wallet.page.isVisible(wallet.$homePageBalancesList);
     expect(homePageVisible).toBeTruthy();
+  });
+
+  it('should show onboarding steps for a new wallet with only one account', async () => {
+    await wallet.clickDenyAnalytics();
+    await wallet.clickSignUp();
+    await wallet.backUpKeyAndSetPassword();
+    await wallet.waitForHomePage();
+    await wallet.waitForHideOnboardingsStepsButton();
+  });
+
+  it('should hide onboarding steps when signing in with an existing secret key', async () => {
+    await wallet.clickDenyAnalytics();
+    await wallet.clickSignIn();
+    await wallet.loginWithPreviousSecretKey(SECRET_KEY);
+    await wallet.waitForHomePage();
+    const onboardingStepsVisible = await wallet.page.isVisible(wallet.$onboardingStepsList);
+    expect(onboardingStepsVisible).toBeFalsy();
+  });
+
+  it('should hide onboarding steps if user clicks the button to hide them', async () => {
+    await wallet.clickDenyAnalytics();
+    await wallet.clickSignUp();
+    await wallet.backUpKeyAndSetPassword();
+    await wallet.waitForHomePage();
+    await wallet.waitForHideOnboardingsStepsButton();
+    await wallet.clickHideSteps();
+  });
+
+  it('should be able to start and complete an onboarding step in the list', async () => {
+    await wallet.clickDenyAnalytics();
+    await wallet.clickSignUp();
+    await wallet.backUpKeyAndSetPassword();
+    await wallet.waitForHomePage();
+    await wallet.waitForOnboardingStepsList();
+    const stepsToStartBtns = await wallet.page.$$(wallet.$onboardingStepStartBtn);
+    await stepsToStartBtns[2].click();
+    const stepsDone = await wallet.page.$$(wallet.$onboardingStepDoneBadge);
+    expect(stepsDone.length).toEqual(2);
+  });
+
+  it('should be able to use the link to add funds if balances list has no assets', async () => {
+    await wallet.clickDenyAnalytics();
+    await wallet.clickSignUp();
+    await wallet.backUpKeyAndSetPassword();
+    await wallet.waitForHomePage();
+    await wallet.waitForOnboardingStepsList();
+    const noAssetsFundAccountLink = await wallet.page.$(wallet.$noAssetsFundAccountLink);
+    await noAssetsFundAccountLink?.click();
   });
 });

tests/page-objects/wallet.page.ts
@@ -1,7 +1,7 @@
 import { Page } from 'playwright-core';
 
 import { RouteUrls } from '@shared/route-urls';
-import { OnboardingSelectors } from '@tests/integration/onboarding.selectors';
+import { OnboardingSelectors } from '@tests/integration/onboarding/onboarding.selectors';
 import { HomePageSelectors } from '@tests/page-objects/home-page.selectors';
 import { SettingsSelectors } from '@tests/integration/settings.selectors';
 import { BuyTokensSelectors } from '@tests/page-objects/buy-tokens-selectors';
@@ -50,6 +50,11 @@ export class WalletPage {
   $enterPasswordInput = createTestSelector(SettingsSelectors.EnterPasswordInput);
   $unlockWalletBtn = createTestSelector(SettingsSelectors.UnlockWalletBtn);
   $magicRecoveryMessage = createTestSelector(WalletPageSelectors.MagicRecoveryMessage);
+  $hideStepsBtn = createTestSelector(OnboardingSelectors.HideStepsBtn);
+  $onboardingStepsList = createTestSelector(OnboardingSelectors.StepsList);
+  $onboardingStepStartBtn = createTestSelector(OnboardingSelectors.StepItemStart);
+  $onboardingStepDoneBadge = createTestSelector(OnboardingSelectors.StepItemDone);
+  $noAssetsFundAccountLink = createTestSelector(OnboardingSelectors.NoAssetsFundAccountLink);
 
   page: Page;
 
@@ -97,6 +102,10 @@ export class WalletPage {
     await this.page.click(this.$contractCallButton);
   }
 
+  async clickHideSteps() {
+    await this.page.click(this.$hideStepsBtn);
+  }
+
   async waitForHomePage() {
     await this.page.waitForSelector(this.$homePageBalancesList, { timeout: 30000 });
   }
@@ -121,12 +130,20 @@ export class WalletPage {
     await this.page.waitForSelector(this.$hiroWalletLogo, { timeout: 3000 });
   }
 
-  async waitForLoginPage() {
+  async waitForWelcomePage() {
     await this.page.waitForSelector(this.$signInButton, { timeout: 3000 });
   }
 
   async waitForStatusMessage() {
     await this.page.waitForSelector(this.$statusMessage, { timeout: 20000 });
+  }
+
+  async waitForHideOnboardingsStepsButton() {
+    await this.page.waitForSelector(this.$hideStepsBtn, { timeout: 30000 });
+  }
+
+  async waitForOnboardingStepsList() {
+    await this.page.waitForSelector(this.$onboardingStepsList, { timeout: 30000 });
   }
 
   async loginWithPreviousSecretKey(secretKey: string) {
@@ -206,14 +223,14 @@ export class WalletPage {
 
   /** Sign up with a randomly generated seed phrase */
   async signUp() {
-    await this.clickAllowAnalytics();
+    await this.clickDenyAnalytics();
     await this.clickSignUp();
     await this.backUpKeyAndSetPassword();
     await this.waitForHomePage();
   }
 
   async signIn(secretKey: string) {
-    await this.clickAllowAnalytics();
+    await this.clickDenyAnalytics();
     await this.clickSignIn();
     let startTime = new Date();
     await this.enterSecretKey(secretKey);