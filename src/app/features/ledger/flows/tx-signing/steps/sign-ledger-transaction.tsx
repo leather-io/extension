3160b139266d9fda6e10206fa3f5de2999af704d
fbwoolf <fwoolf@blockstack.com>
2022-04-13 17:02:57 -0500

    fix: transaction details microstacks

--- src/app/features/ledger/flows/tx-signing/steps/sign-ledger-transaction.tsx
+++ src/app/features/ledger/flows/tx-signing/steps/sign-ledger-transaction.tsx
@@ -1,6 +1,7 @@
 import { useContext, useMemo } from 'react';
 import { cvToString, PayloadType } from '@stacks/transactions';
 
+import { microStxToStx } from '@app/common/stacks-utils';
 import { ledgerTxSigningContext } from '@app/features/ledger/ledger-tx-signing.context';
 import { useHasApprovedTransaction } from '@app/features/ledger/hooks/use-has-approved-transaction';
 import { SignLedgerTransactionLayout } from '@app/features/ledger/steps/sign-ledger-transaction.layout';
@@ -11,18 +12,28 @@ function formatSipTenTransferArgument(index: number) {
   return `Argument ${index} — ${sipTenTransferArguments[index]}`;
 }
 
+function formatTooltipLabel(amount: bigint) {
+  const stxFromMicroStx = microStxToStx(Number(amount));
+  return `The amount is displayed in microstacks (µSTX) and is equal to ${stxFromMicroStx} STX`;
+}
+
 export function SignLedgerTransaction() {
   const { transaction } = useContext(ledgerTxSigningContext);
   const hasApprovedTransaction = useHasApprovedTransaction();
 
-  const transactionDetails: [string, string][] = useMemo(() => {
+  const transactionDetails: [string, string, string?][] = useMemo(() => {
     if (!transaction) return [];
 
-    if (transaction.payload.payloadType === PayloadType.TokenTransfer)
+    if (transaction.payload.payloadType === PayloadType.TokenTransfer) {
       return [
         ['To', cvToString(transaction.payload.recipient)],
-        ['Amount', String(transaction.payload.amount)],
+        [
+          'Amount (µSTX)',
+          String(transaction.payload.amount),
+          formatTooltipLabel(transaction.payload.amount),
+        ],
       ];
+    }
 
     if (transaction.payload.payloadType === PayloadType.ContractCall)
       return transaction.payload.functionArgs
