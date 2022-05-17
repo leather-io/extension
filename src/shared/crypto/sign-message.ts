05125784cfb4a3cf41e50eb4e30bb46bf7cff2c5
kyranjamie <kyran.burraston@gmail.com>
2022-05-17 14:44:19 +0200

    fix(message-signing): signature defaulting to rsv order, closes #2419

--- src/shared/crypto/sign-message.ts
+++ src/shared/crypto/sign-message.ts
@@ -7,10 +7,16 @@ import {
   StacksPrivateKey,
 } from '@stacks/transactions';
 
+function convertSignatureVrsToRsvFormat(sig: string) {
+  return Buffer.from(sig.slice(2) + sig.slice(0, 2), 'hex').toString('hex');
+}
+
 export function signMessage(message: string, privateKey: StacksPrivateKey): SignatureData {
   const hash = hashMessage(message);
+  const signatureVrs = signWithKey(privateKey, hash.toString('hex')).data;
+  const signatureRsv = convertSignatureVrsToRsvFormat(signatureVrs);
   return {
-    signature: signWithKey(privateKey, hash.toString('hex')).data,
+    signature: signatureRsv,
     publicKey: publicKeyToString(getPublicKey(privateKey)),
   };
 }
