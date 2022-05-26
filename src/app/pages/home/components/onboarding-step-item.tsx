00d22f5baae0e115fd8f5df0a7ed608a1c630959
fbwoolf <fwoolf@blockstack.com>
2022-05-26 13:47:34 -0500

    refactor: dim completed onboarding steps

--- src/app/pages/home/components/onboarding-step-item.tsx
+++ src/app/pages/home/components/onboarding-step-item.tsx
@@ -1,12 +1,14 @@
-import { FiArrowRight, FiCheck } from 'react-icons/fi';
-import { Box, Circle, color, Flex, Stack, useMediaQuery } from '@stacks/ui';
+import { FiArrowRight } from 'react-icons/fi';
+import { Box, color, Flex, Stack, useMediaQuery } from '@stacks/ui';
 
 import { DESKTOP_VIEWPORT_MIN_WIDTH } from '@app/components/global-styles/full-page-styles';
 import { Tooltip } from '@app/components/tooltip';
-import { Body, Caption, Text, Title } from '@app/components/typography';
+import { Body, Text, Title } from '@app/components/typography';
 import { Link } from '@app/components/link';
 import { OnboardingSelectors } from '@tests/integration/onboarding/onboarding.selectors';
 import { RouteType } from '@shared/models/onboarding-types';
+
+import { StepDoneBadge } from './step-done-badge';
 
 const externalLinkInfo =
   'This link will take you to an external third-party website that is not affiliated with Hiro Systems PBC.';
@@ -16,24 +18,31 @@ interface StepIllustrationProps {
 }
 const StepIllustration = ({ image }: StepIllustrationProps) => <img src={image} />;
 
-const PopupDoneIcon = () => (
-  <Circle border="1px solid" borderColor={color('border')} color={color('bg')} size="40px">
-    <FiCheck color={color('accent')} />
-  </Circle>
-);
-
 interface OnboardingStepItemProps {
   action: string;
   body: string;
   imageFull: string;
+  imageFullDone: string;
   imagePopup: string;
+  imagePopupDone: string;
   isDone: boolean;
   onClick(): void;
   routeType: number;
   title: string;
 }
 export function OnboardingStepItem(props: OnboardingStepItemProps) {
-  const { action, body, imageFull, imagePopup, isDone, onClick, routeType, title } = props;
+  const {
+    action,
+    body,
+    imageFull,
+    imageFullDone,
+    imagePopup,
+    imagePopupDone,
+    isDone,
+    onClick,
+    routeType,
+    title,
+  } = props;
 
   const [desktopViewport] = useMediaQuery(`(min-width: ${DESKTOP_VIEWPORT_MIN_WIDTH})`);
 
@@ -52,11 +61,9 @@ export function OnboardingStepItem(props: OnboardingStepItemProps) {
     >
       <Box height={['46px', '100px']} width={['55px', '132px']}>
         {desktopViewport ? (
-          <StepIllustration image={imageFull} />
-        ) : !isDone ? (
-          <StepIllustration image={imagePopup} />
+          <StepIllustration image={isDone ? imageFullDone : imageFull} />
         ) : (
-          <PopupDoneIcon />
+          <StepIllustration image={isDone ? imagePopupDone : imagePopup} />
         )}
       </Box>
       <Tooltip
@@ -67,7 +74,7 @@ export function OnboardingStepItem(props: OnboardingStepItemProps) {
       >
         <Flex alignItems={['center', 'unset']} flexDirection={['unset', 'column']}>
           <Title
-            color={!desktopViewport && isDone ? color('text-caption') : color('text-title')}
+            color={isDone ? color('text-caption') : color('text-title')}
             fontSize={[1, 2]}
             lineHeight="24px"
             mr="extra-tight"
@@ -77,7 +84,6 @@ export function OnboardingStepItem(props: OnboardingStepItemProps) {
           {!desktopViewport && routeType === RouteType.External ? (
             <Box as={FiArrowRight} transform={'rotate(-45deg)'} />
           ) : null}
-
           <Body display={['none', 'block']} mt="tight">
             {body}
           </Body>
@@ -85,28 +91,7 @@ export function OnboardingStepItem(props: OnboardingStepItemProps) {
       </Tooltip>
       {desktopViewport ? (
         isDone ? (
-          <Box
-            data-testid={OnboardingSelectors.StepItemDone}
-            border="2px solid"
-            borderColor={color('bg-4')}
-            borderRadius="25px"
-            color={color('text-caption')}
-            height="24px"
-            width="62px"
-          >
-            <Stack
-              alignItems="center"
-              height="100%"
-              isInline
-              justifyContent="center"
-              spacing="extra-tight"
-            >
-              <FiCheck />
-              <Caption fontWeight={400} variant="c2">
-                Done
-              </Caption>
-            </Stack>
-          </Box>
+          <StepDoneBadge />
         ) : (
           <Link
             data-testid={OnboardingSelectors.StepItemStart}
@@ -126,10 +111,9 @@ export function OnboardingStepItem(props: OnboardingStepItemProps) {
                   <Text color={color('accent')} fontWeight={500} mr="extra-tight">
                     {action}
                   </Text>
-                  <Box
-                    as={FiArrowRight}
-                    transform={routeType === RouteType.External ? 'rotate(-45deg)' : 'unset'}
-                  />
+                  {routeType === RouteType.External ? (
+                    <Box as={FiArrowRight} transform="rotate(-45deg)" />
+                  ) : null}
                 </Flex>
               </Tooltip>
             </Flex>
--- src/app/pages/home/components/onboarding-steps-list.tsx
+++ src/app/pages/home/components/onboarding-steps-list.tsx
@@ -11,63 +11,12 @@ import { Text, Title } from '@app/components/typography';
 import { useAppDispatch } from '@app/store';
 import { onboardingActions } from '@app/store/onboarding/onboarding.actions';
 import { useStepsStatus } from '@app/store/onboarding/onboarding.selectors';
-import AddFundsFull from '@assets/images/onboarding/steps/add-funds-light.png';
-import BackUpSecretKeyFull from '@assets/images/onboarding/steps/backup-key-light.png';
-import ExploreAppsFull from '@assets/images/onboarding/steps/explore-apps-light.png';
-import BuyNftFull from '@assets/images/onboarding/steps/buy-nft-light.png';
-import AddFundsPopup from '@assets/images/onboarding/steps/add-funds-light-sm.png';
-import BackUpSecretKeyPopup from '@assets/images/onboarding/steps/backup-key-light-sm.png';
-import ExploreAppsPopup from '@assets/images/onboarding/steps/explore-apps-light-sm.png';
-import BuyNftPopup from '@assets/images/onboarding/steps/buy-nft-light-sm.png';
 import { OnboardingSteps, OnboardingStepStatus, RouteType } from '@shared/models/onboarding-types';
-import { RouteUrls } from '@shared/route-urls';
 import { OnboardingSelectors } from '@tests/integration/onboarding/onboarding.selectors';
 
 import { OnboardingStepItem } from './onboarding-step-item';
 import { useOnboardingSteps } from '../hooks/use-onboarding-steps';
-
-const onboardingSteps = [
-  {
-    action: 'View secret key',
-    body: "Don't lose access to your account and crypto",
-    event: 'back_up_secret_key',
-    imageFull: BackUpSecretKeyFull,
-    imagePopup: BackUpSecretKeyPopup,
-    route: RouteUrls.ViewSecretKey,
-    routeType: RouteType.Internal,
-    title: OnboardingSteps.BackUpSecretKey,
-  },
-  {
-    action: 'View options',
-    body: 'Get some STX so you can start using apps',
-    event: 'add_funds',
-    imageFull: AddFundsFull,
-    imagePopup: AddFundsPopup,
-    route: RouteUrls.Fund,
-    routeType: RouteType.Internal,
-    title: OnboardingSteps.AddFunds,
-  },
-  {
-    action: 'Find apps',
-    body: 'Try Stacks apps for finance, NFTs, blogging and more',
-    event: 'explore_apps',
-    imageFull: ExploreAppsFull,
-    imagePopup: ExploreAppsPopup,
-    route: 'https://www.stacks.co/explore/discover-apps#apps',
-    routeType: RouteType.External,
-    title: OnboardingSteps.ExploreApps,
-  },
-  {
-    action: 'Find NFT',
-    body: 'Collect and trade NFTs secured by Bitcoin',
-    event: 'buy_nft',
-    imageFull: BuyNftFull,
-    imagePopup: BuyNftPopup,
-    route: 'https://www.hiro.so/wallet-faq/nfts',
-    routeType: RouteType.External,
-    title: OnboardingSteps.BuyNft,
-  },
-];
+import { onboardingSteps } from './onboarding-steps';
 
 export function OnboardingStepsList() {
   const analytics = useAnalytics();
@@ -144,7 +93,9 @@ export function OnboardingStepsList() {
               action={step.action}
               body={step.body}
               imageFull={step.imageFull}
+              imageFullDone={step.imageFullDone}
               imagePopup={step.imagePopup}
+              imagePopupDone={step.imagePopupDone}
               isDone={onboardingStepsStatus[step.title] === OnboardingStepStatus.Done}
               key={step.title}
               onClick={() => handleStepStart(step)}
