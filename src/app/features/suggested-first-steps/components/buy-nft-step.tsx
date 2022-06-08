a9b8e8fb2b1cc308a86558cebc0237074160d798
fbwoolf <fwoolf@blockstack.com>
2022-06-08 17:03:09 -0500

    fix: suggested steps only updating with refresh

--- src/app/features/suggested-first-steps/components/buy-nft-step.tsx
+++ src/app/features/suggested-first-steps/components/buy-nft-step.tsx
@@ -1,27 +1,16 @@
-import { useCallback } from 'react';
-
-import { useAnalytics } from '@app/common/hooks/analytics/use-analytics';
-import { openInNewTab } from '@app/common/utils/open-in-new-tab';
-import { useSuggestedFirstStepsStatus } from '@app/store/onboarding/onboarding.selectors';
 import BuyNftFull from '@assets/images/onboarding/steps/buy-nft-light.png';
 import BuyNftFullDone from '@assets/images/onboarding/steps/buy-nft-light-done.png';
 import BuyNftPopup from '@assets/images/onboarding/steps/buy-nft-light-sm.png';
 import BuyNftPopupDone from '@assets/images/onboarding/steps/buy-nft-light-done-sm.png';
-import { SuggestedFirstSteps, SuggestedFirstStepStatus } from '@shared/models/onboarding-types';
+import { SuggestedFirstSteps } from '@shared/models/onboarding-types';
 
 import { SuggestedFirstStep } from './suggested-first-step';
 
-const buyNftExternalRoute = 'https://www.hiro.so/wallet-faq/nfts';
-
-export function BuyNftStep() {
-  const analytics = useAnalytics();
-  const stepsStatus = useSuggestedFirstStepsStatus();
-
-  const onSelectStep = useCallback(() => {
-    void analytics.track('select_next_step', { step: 'buy_nft' });
-    openInNewTab(buyNftExternalRoute);
-  }, [analytics]);
-
+interface BuyNftStepProps {
+  isComplete: boolean;
+  onSelectStep(): void;
+}
+export function BuyNftStep({ isComplete, onSelectStep }: BuyNftStepProps) {
   return (
     <SuggestedFirstStep
       action="Find NFT"
@@ -30,7 +19,7 @@ export function BuyNftStep() {
       imageFullDone={BuyNftFullDone}
       imagePopup={BuyNftPopup}
       imagePopupDone={BuyNftPopupDone}
-      isComplete={stepsStatus[SuggestedFirstSteps.BuyNft] === SuggestedFirstStepStatus.Complete}
+      isComplete={isComplete}
       isExternalRoute
       key={SuggestedFirstSteps.BuyNft}
       onClick={onSelectStep}
