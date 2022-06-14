6e3f2c29980f06f3b1ae96fe145efb2976cf192c
fbwoolf <fwoolf@blockstack.com>
2022-06-14 12:21:16 -0500

    fix: sign message with line breaks

--- test-app/src/components/signature.tsx
+++ test-app/src/components/signature.tsx
@@ -32,6 +32,8 @@ export const Signature = () => {
   const signatureMessage = 'Hello world!';
   const longSignatureMessage =
     'Nullam eu ante vel est convallis dignissim.  Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio.  Nunc porta vulputate tellus.  Nunc rutrum turpis sed pede.  Sed bibendum.  Aliquam posuere.  Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.  Pellentesque condimentum, magna ut suscipit hendrerit, ipsum augue ornare nulla, non luctus diam neque sit amet urna.  Curabitur vulputate vestibulum lorem.  Fusce sagittis, libero non molestie mollis, magna orci ultrices dolor, at vulputate neque nulla lacinia eros.  Sed id ligula quis est convallis tempor.  Curabitur lacinia pulvinar nibh.  Nam a sapien.';
+  const signatureMessageWithLineBreaks =
+    'This is line one.\nThis is line two.\nThis is line three.\nThis is line four.\nThis is line five.';
   const ADDRESS = 'SP2JXKMSH007NPYAQHKJPQMAQYAD90NQGTVJVQ02B';
 
   const structuredData = tupleCV({
@@ -130,6 +132,10 @@ export const Signature = () => {
       <br />
       <Button mt={3} onClick={() => signMessage(longSignatureMessage)}>
         Signature (long message)
+      </Button>
+      <br />
+      <Button mt={3} onClick={() => signMessage(signatureMessageWithLineBreaks)}>
+        Signature (with line breaks)
       </Button>
       <br />
       <Button mt={3} onClick={() => signStructure(structuredData)}>
