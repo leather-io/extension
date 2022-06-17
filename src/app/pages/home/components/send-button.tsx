talentdev0807/stacks-wallet-web

fix: direct landing on send form page, closes #2470

src/app/pages/home/components/send-button.tsx
@@ -6,16 +6,16 @@ import { useNavigate } from 'react-router-dom';
 import { useTransferableAssets } from '@app/store/assets/asset.hooks';
 import { RouteUrls } from '@shared/route-urls';
 import { PrimaryButton } from '@app/components/primary-button';
-import { WalletPageSelectors } from '@tests/page-objects/wallet.selectors';
 
 import { HomeActionButton } from './tx-button';
 import { useWalletType } from '@app/common/use-wallet-type';
 import { openIndexPageInNewTab } from '@app/common/utils/open-in-new-tab';
 import { whenPageMode } from '@app/common/utils';
+import { HomePageSelectors } from '@tests/page-objects/home.selectors';
 
 const SendTxButton = (props: ButtonProps) => (
   <HomeActionButton
-    data-testid={WalletPageSelectors.BtnSendTokens}
+    data-testid={HomePageSelectors.BtnFundAccount}
     icon={FiArrowUp}
     label="Send"
     buttonComponent={PrimaryButton}

src/app/pages/home/home.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { Flex, Stack } from '@stacks/ui';
 
@@ -12,31 +12,21 @@ import { ActivityList } from '@app/features/activity-list/account-activity';
 import { BalancesList } from '@app/features/balances-list/balances-list';
 import { SuggestedFirstSteps } from '@app/features/suggested-first-steps/suggested-first-steps';
 import { useSuggestedFirstSteps } from '@app/features/suggested-first-steps/hooks/use-suggested-first-steps';
-import { CurrentAccount } from '@app/pages/home/components/account-area';
 
 import { HomeActions } from '@app/pages/home/components/home-actions';
-<<<<<<< HEAD
-import { useAccounts } from '@app/store/accounts/account.hooks';
-=======
-import { useCurrentAccount } from '@app/store/accounts/account.hooks';
->>>>>>> b1a39aca4 (refactor: home buttons, open in new pages, fixes #2453)
+
 import { RouteUrls } from '@shared/route-urls';
 import {
   useCurrentAccount,
   useCurrentAccountAvailableStxBalance,
 } from '@app/store/accounts/account.hooks';
-import { HomePageSelectors } from '@tests/page-objects/home.selectors';
 
-import { AccountInfoFetcher, BalanceFetcher } from './components/fetchers';
 import { HomeTabs } from './components/home-tabs';
 
 import { FullPageLoadingSpinner } from '@app/components/loading-spinner';
-import { OnboardingStepsList } from './components/onboarding-steps-list';
-import { useOnboardingSteps } from './hooks/use-onboarding-steps';
-import { useTrackFirstDeposit } from '@app/common/hooks/analytics/transactions-analytics.hooks';
-import { FullPageLoadingSpinner } from '@app/components/loading-spinner';
-
 import { CurrentAccount } from './components/account-area';
+import { useSkipFundAccount } from '@app/store/onboarding/onboarding.selectors';
+import { HomePageSelectors } from '@tests/page-objects/home.selectors';
 
 export function Home() {
   const { decodedAuthRequest } = useOnboardingState();
@@ -67,10 +57,6 @@ export function Home() {
 
   return (
     <>
-      <Suspense fallback={null}>
-        {account?.address && <BalanceFetcher address={account.address} />}
-        {account?.address && <AccountInfoFetcher address={account.address} />}
-      </Suspense>
       <Stack alignItems="center" width="100%" spacing="extra-tight">
         {showSuggestedFirstSteps && <SuggestedFirstSteps />}
         <Stack

leather-io/extension

src/app/pages/home/components/send-button.tsx
@@ -6,16 +6,16 @@ import { useNavigate } from 'react-router-dom';
 import { useTransferableAssets } from '@app/store/assets/asset.hooks';
 import { RouteUrls } from '@shared/route-urls';
 import { PrimaryButton } from '@app/components/primary-button';
-import { WalletPageSelectors } from '@tests/page-objects/wallet.selectors';
 
 import { HomeActionButton } from './tx-button';
 import { useWalletType } from '@app/common/use-wallet-type';
 import { openIndexPageInNewTab } from '@app/common/utils/open-in-new-tab';
 import { whenPageMode } from '@app/common/utils';
+import { HomePageSelectors } from '@tests/page-objects/home.selectors';
 
 const SendTxButton = (props: ButtonProps) => (
   <HomeActionButton
-    data-testid={WalletPageSelectors.BtnSendTokens}
+    data-testid={HomePageSelectors.BtnSendTokens}
     icon={FiArrowUp}
     label="Send"
     buttonComponent={PrimaryButton}

src/app/pages/home/home.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { Flex, Stack } from '@stacks/ui';
 
@@ -11,30 +11,16 @@ import { HiroMessages } from '@app/features/hiro-messages/hiro-messages';
 import { ActivityList } from '@app/features/activity-list/account-activity';
 import { BalancesList } from '@app/features/balances-list/balances-list';
 import { SuggestedFirstSteps } from '@app/features/suggested-first-steps/suggested-first-steps';
-import { CurrentAccount } from '@app/pages/home/components/account-area';
 
 import { HomeActions } from '@app/pages/home/components/home-actions';
-<<<<<<< HEAD
-import { useAccounts } from '@app/store/accounts/account.hooks';
-=======
-import { useCurrentAccount } from '@app/store/accounts/account.hooks';
->>>>>>> b1a39aca4 (refactor: home buttons, open in new pages, fixes #2453)
-import { RouteUrls } from '@shared/route-urls';
-import {
-  useCurrentAccount,
-  useCurrentAccountAvailableStxBalance,
-} from '@app/store/accounts/account.hooks';
-import { HomePageSelectors } from '@tests/page-objects/home.selectors';
 
-import { AccountInfoFetcher, BalanceFetcher } from './components/fetchers';
+import { RouteUrls } from '@shared/route-urls';
+import { useCurrentAccount } from '@app/store/accounts/account.hooks';
+
 import { HomeTabs } from './components/home-tabs';
 
 import { FullPageLoadingSpinner } from '@app/components/loading-spinner';
-import { OnboardingStepsList } from './components/onboarding-steps-list';
-import { useOnboardingSteps } from './hooks/use-onboarding-steps';
-import { useTrackFirstDeposit } from '@app/common/hooks/analytics/transactions-analytics.hooks';
-import { FullPageLoadingSpinner } from '@app/components/loading-spinner';
-
+import { HomePageSelectors } from '@tests/page-objects/home.selectors';
 import { CurrentAccount } from './components/account-area';
 
 export function Home() {
@@ -59,10 +45,6 @@ export function Home() {
 
   return (
     <>
-      <Suspense fallback={null}>
-        {account?.address && <BalanceFetcher address={account.address} />}
-        {account?.address && <AccountInfoFetcher address={account.address} />}
-      </Suspense>
       <Stack alignItems="center" width="100%" spacing="extra-tight">
         <SuggestedFirstSteps />
         <Stack