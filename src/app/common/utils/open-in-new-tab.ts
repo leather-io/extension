luciorubeens/stacks-wallet-web

refactor: ledger modal navigation

src/app/common/utils/open-in-new-tab.ts
@@ -7,6 +7,9 @@ export function openInNewTab(url: string) {
   if (newWindow) newWindow.opener = null;
 }
 
-export function openIndexPageInNewTab(path: RouteUrls | string) {
-  return chrome.tabs.create({ url: chrome.runtime.getURL('index.html#' + path) });
+export function openIndexPageInNewTab(path: RouteUrls | string, urlParams?: URLSearchParams) {
+  const paramsString = urlParams ? `?${urlParams.toString()}` : '';
+  return chrome.tabs.create({
+    url: chrome.runtime.getURL(`index.html#${path}` + paramsString),
+  });
 }

src/app/features/increase-fee-drawer/increase-fee-drawer.tsx
@@ -3,56 +3,59 @@ import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { Flex, Spinner, Stack } from '@stacks/ui';
 
 import { LoadingKeys, useLoading } from '@app/common/hooks/use-loading';
-import { ControlledDrawer } from '@app/components/drawer/controlled-drawer';
+import { BaseDrawer } from '@app/components/drawer/base-drawer';
 import { Caption } from '@app/components/typography';
 import { useRawTxIdState } from '@app/store/transactions/raw.hooks';
 import { RouteUrls } from '@shared/route-urls';
 
 import { IncreaseFeeForm } from './components/increase-fee-form';
 
-export function IncreaseFeeDrawer(): JSX.Element {
+export function IncreaseFeeDrawer() {
   const [rawTxId, setRawTxId] = useRawTxIdState();
   const { isLoading, setIsIdle } = useLoading(LoadingKeys.INCREASE_FEE_DRAWER);
   const navigate = useNavigate();
   const location = useLocation();
+  const urlParams = new URLSearchParams(location.search);
+  const txIdFromParams = urlParams.get('txId');
 
   useEffect(() => {
+    if (!rawTxId && txIdFromParams) {
+      setRawTxId(txIdFromParams);
+    }
     if (isLoading && !rawTxId) {
       setIsIdle();
     }
-  }, [isLoading, rawTxId, setIsIdle]);
+  }, [isLoading, rawTxId, setIsIdle, setRawTxId, txIdFromParams]);
 
   const onClose = () => {
     setRawTxId(null);
     navigate(RouteUrls.Home);
   };
 
-  return (
+  return rawTxId ? (
     <>
-      <ControlledDrawer
+      <BaseDrawer
         isShowing={location.pathname === RouteUrls.IncreaseFee}
         onClose={onClose}
         title="Increase transaction fee"
       >
         <Stack px="loose" spacing="loose" pb="extra-loose">
-          {rawTxId ? (
-            <Suspense
-              fallback={
-                <Flex alignItems="center" justifyContent="center" p="extra-loose">
-                  <Spinner />
-                </Flex>
-              }
-            >
-              <Caption>
-                If your transaction has been pending for a long time, its fee might not be high
-                enough to be included in a block. Increase the fee and try again.
-              </Caption>
-              <IncreaseFeeForm />
-            </Suspense>
-          ) : null}
+          <Suspense
+            fallback={
+              <Flex alignItems="center" justifyContent="center" p="extra-loose">
+                <Spinner />
+              </Flex>
+            }
+          >
+            <Caption>
+              If your transaction has been pending for a long time, its fee might not be high enough
+              to be included in a block. Increase the fee and try again.
+            </Caption>
+            <IncreaseFeeForm />
+          </Suspense>
         </Stack>
-      </ControlledDrawer>
+      </BaseDrawer>
       <Outlet />
     </>
-  );
+  ) : null;
 }

leather-io/extension

src/app/common/utils/open-in-new-tab.ts
@@ -7,6 +7,9 @@ export function openInNewTab(url: string) {
   if (newWindow) newWindow.opener = null;
 }
 
-export function openIndexPageInNewTab(path: RouteUrls | string) {
-  return chrome.tabs.create({ url: chrome.runtime.getURL('index.html#' + path) });
+export function openIndexPageInNewTab(path: RouteUrls | string, searchParams?: string) {
+  const paramsString = searchParams ? searchParams : '';
+  return chrome.tabs.create({
+    url: chrome.runtime.getURL(`index.html#${path}` + paramsString),
+  });
 }

src/app/features/increase-fee-drawer/increase-fee-drawer.tsx
@@ -1,58 +1,61 @@
 import { Suspense, useEffect } from 'react';
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { Flex, Spinner, Stack } from '@stacks/ui';
 
 import { LoadingKeys, useLoading } from '@app/common/hooks/use-loading';
-import { ControlledDrawer } from '@app/components/drawer/controlled-drawer';
+import { BaseDrawer } from '@app/components/drawer/base-drawer';
 import { Caption } from '@app/components/typography';
 import { useRawTxIdState } from '@app/store/transactions/raw.hooks';
 import { RouteUrls } from '@shared/route-urls';
 
 import { IncreaseFeeForm } from './components/increase-fee-form';
 
-export function IncreaseFeeDrawer(): JSX.Element {
+export function IncreaseFeeDrawer() {
   const [rawTxId, setRawTxId] = useRawTxIdState();
   const { isLoading, setIsIdle } = useLoading(LoadingKeys.INCREASE_FEE_DRAWER);
   const navigate = useNavigate();
   const location = useLocation();
+  const [searchParams] = useSearchParams();
+  const txIdFromParams = searchParams.get('txId');
 
   useEffect(() => {
+    if (!rawTxId && txIdFromParams) {
+      setRawTxId(txIdFromParams);
+    }
     if (isLoading && !rawTxId) {
       setIsIdle();
     }
-  }, [isLoading, rawTxId, setIsIdle]);
+  }, [isLoading, rawTxId, setIsIdle, setRawTxId, txIdFromParams]);
 
   const onClose = () => {
     setRawTxId(null);
     navigate(RouteUrls.Home);
   };
 
-  return (
+  return rawTxId ? (
     <>
-      <ControlledDrawer
+      <BaseDrawer
         isShowing={location.pathname === RouteUrls.IncreaseFee}
         onClose={onClose}
         title="Increase transaction fee"
       >
         <Stack px="loose" spacing="loose" pb="extra-loose">
-          {rawTxId ? (
-            <Suspense
-              fallback={
-                <Flex alignItems="center" justifyContent="center" p="extra-loose">
-                  <Spinner />
-                </Flex>
-              }
-            >
-              <Caption>
-                If your transaction has been pending for a long time, its fee might not be high
-                enough to be included in a block. Increase the fee and try again.
-              </Caption>
-              <IncreaseFeeForm />
-            </Suspense>
-          ) : null}
+          <Suspense
+            fallback={
+              <Flex alignItems="center" justifyContent="center" p="extra-loose">
+                <Spinner />
+              </Flex>
+            }
+          >
+            <Caption>
+              If your transaction has been pending for a long time, its fee might not be high enough
+              to be included in a block. Increase the fee and try again.
+            </Caption>
+            <IncreaseFeeForm />
+          </Suspense>
         </Stack>
-      </ControlledDrawer>
+      </BaseDrawer>
       <Outlet />
     </>
-  );
+  ) : null;
 }