56cd5aff6264886c87e80f18765efe22bdff2a84
kyranjamie <kyran.burraston@gmail.com>
2022-08-12 09:58:38 +0200

    fix: redirect back after successful unlock

--- src/app/pages/unlock.tsx
+++ src/app/pages/unlock.tsx
@@ -1,4 +1,3 @@
-import { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Stack } from '@stacks/ui';
 
@@ -7,25 +6,19 @@ import { useDrawers } from '@app/common/hooks/use-drawers';
 import { Header } from '@app/components/header';
 import { CenteredPageContainer } from '@app/components/centered-page-container';
 import { SignOutConfirmDrawer } from '@app/pages/sign-out-confirm/sign-out-confirm';
-import { useOnboardingState } from '@app/common/hooks/auth/use-onboarding-state';
 import { CENTERED_FULL_PAGE_MAX_WIDTH } from '@app/components/global-styles/full-page-styles';
-import { RouteUrls } from '@shared/route-urls';
 import { RequestPassword } from '@app/components/request-password';
 
-export function Unlock(): JSX.Element {
-  const { decodedAuthRequest } = useOnboardingState();
+export function Unlock() {
   const { showSignOut } = useDrawers();
   const navigate = useNavigate();
 
   useRouteHeader(<Header />);
 
-  const handleSuccess = useCallback(async () => {
-    if (decodedAuthRequest) {
-      navigate(RouteUrls.ChooseAccount);
-    } else {
-      navigate(RouteUrls.Home);
-    }
-  }, [decodedAuthRequest, navigate]);
+  // Users land on unlock page as they've been directed here from `<AccountGate/>`.
+  // On successful unlock, we can navigate back to the previous page, now
+  // with account details.
+  const handleSuccess = () => navigate(-1);
 
   return (
     <CenteredPageContainer>
