eaea6f8a30a08ef554dfad3f4bd0bad351edfd57
kyranjamie <kyran.burraston@gmail.com>
2022-09-16 15:26:30 +0200

    fix: slow firefox sign in times, persistent worker

--- src/shared/crypto/generate-encryption-key.ts
+++ src/shared/crypto/generate-encryption-key.ts
@@ -1,19 +1,14 @@
 import { createWorker, WorkerScript } from '../workers';
 
+const worker = createWorker(WorkerScript.DecryptionWorker);
 interface GenerateEncryptionKeyArgs {
   password: string;
   salt: string;
 }
 export async function generateEncryptionKey(args: GenerateEncryptionKeyArgs): Promise<string> {
-  const worker = createWorker(WorkerScript.DecryptionWorker);
-
-  return new Promise((resolve, reject) => {
-    worker.addEventListener('message', (e: MessageEvent<string>) => {
-      worker.terminate();
-      resolve(e.data);
-    });
-    worker.addEventListener('error', e => reject(e));
-    worker.addEventListener('messageerror', e => reject(e));
+  return new Promise(resolve => {
+    const handler = (e: MessageEvent<string>) => resolve(e.data);
+    worker.addEventListener('message', handler);
     worker.postMessage(args);
   });
 }
