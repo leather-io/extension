d1be7bebaff98569c34406a753d011b9643e4208
kyranjamie <kyran.burraston@gmail.com>
2022-10-03 18:19:51 +0200

    fix(ledger): mismatching public key

---
+++ src/app/features/ledger/hooks/use-verify-matching-public-key.ts
@@ -0,0 +1,23 @@
+import { useCallback } from 'react';
+import StacksApp from '@zondax/ledger-stacks';
+
+import { useCurrentAccount } from '@app/store/accounts/account.hooks';
+import { requestPublicKeyForStxAccount } from '../ledger-utils';
+import { useLedgerNavigate } from './use-ledger-navigate';
+
+export function useVerifyMatchingLedgerPublicKey() {
+  const account = useCurrentAccount();
+  const ledgerNavigate = useLedgerNavigate();
+
+  return useCallback(
+    async (stacksApp: StacksApp) => {
+      if (!account) return;
+      const { publicKey } = await requestPublicKeyForStxAccount(stacksApp)(account.index);
+      if (publicKey.toString('hex') !== account.stxPublicKey) {
+        ledgerNavigate.toPublicKeyMismatchStep();
+        throw new Error('Mismatching public keys');
+      }
+    },
+    [account, ledgerNavigate]
+  );
+}
