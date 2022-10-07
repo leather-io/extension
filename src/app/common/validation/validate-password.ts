ac702b512c42b11a8178eb687844d72058d615e3
kyranjamie <kyran.burraston@gmail.com>
2022-10-07 15:50:00 +0200

    fix: password box lag issue, closes #2697

--- src/app/common/validation/validate-password.ts
+++ src/app/common/validation/validate-password.ts
@@ -27,6 +27,7 @@ export interface ValidatedPassword extends ZXCVBNResult {
   meetsLengthRequirement: boolean;
   meetsScoreRequirement: boolean;
   meetsAllStrengthRequirements: boolean;
+  password: string;
 }
 
 export function validatePassword(input: string): ValidatedPassword {
@@ -41,6 +42,7 @@ export function validatePassword(input: string): ValidatedPassword {
     meetsScoreRequirement,
     meetsLengthRequirement,
     meetsAllStrengthRequirements,
+    password,
   });
 }
 
