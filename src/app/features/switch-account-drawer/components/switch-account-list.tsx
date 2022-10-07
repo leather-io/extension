083b62c253f383b99eb4dfa6117eb1d278467851
kyranjamie <kyran.burraston@gmail.com>
2022-10-07 16:27:06 +0200

    refactor: reuse account item layout component

---
+++ src/app/features/switch-account-drawer/components/switch-account-list.tsx
@@ -0,0 +1,44 @@
+import { memo } from 'react';
+import { Virtuoso } from 'react-virtuoso';
+
+import { AccountWithAddress } from '@app/store/accounts/account.models';
+import { SwitchAccountListItem } from './switch-account-list-item';
+import { Box } from '@stacks/ui';
+
+const smallNumberOfAccountsToRenderWholeList = 10;
+
+interface SwitchAccountListProps {
+  handleClose: () => void;
+  accounts: AccountWithAddress[];
+  currentAccountIndex: number;
+}
+export const SwitchAccountList = memo(
+  ({ accounts, currentAccountIndex, handleClose }: SwitchAccountListProps) => {
+    if (accounts.length <= smallNumberOfAccountsToRenderWholeList) {
+      return (
+        <>
+          {accounts.map(account => (
+            <SwitchAccountListItem
+              handleClose={handleClose}
+              account={account}
+              key={account.address}
+            />
+          ))}
+        </>
+      );
+    }
+
+    return (
+      <Virtuoso
+        initialTopMostItemIndex={currentAccountIndex}
+        style={{ paddingTop: '24px', height: '70vh' }}
+        totalCount={accounts.length}
+        itemContent={index => (
+          <Box mx="extra-loose">
+            <SwitchAccountListItem handleClose={handleClose} account={accounts[index]} />
+          </Box>
+        )}
+      />
+    );
+  }
+);
