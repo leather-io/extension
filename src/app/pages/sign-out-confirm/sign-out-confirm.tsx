907821178871d5d3fbd3f143f3919e67b9c66ef3
Eduard Bardají Puig <edu@trustmachines.co>
2022-10-31 16:59:18 +0000

    fix: remove unnecessary call to setShowSignOut

--- src/app/pages/sign-out-confirm/sign-out-confirm.tsx
+++ src/app/pages/sign-out-confirm/sign-out-confirm.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from 'react-router-dom';
 
-import { useDrawers } from '@app/common/hooks/use-drawers';
 import { useWallet } from '@app/common/hooks/use-wallet';
 import { RouteUrls } from '@shared/route-urls';
 
@@ -9,17 +8,14 @@ import { SignOutConfirmLayout } from './sign-out-confirm-layout';
 export const SignOutConfirmDrawer = () => {
   const { signOut } = useWallet();
   const navigate = useNavigate();
-  const { setShowSignOut } = useDrawers();
 
   return (
     <SignOutConfirmLayout
       onUserDeleteWallet={() => {
-        setShowSignOut(false);
         navigate(RouteUrls.Onboarding);
         void signOut();
       }}
       onUserSafelyReturnToHomepage={() => {
-        setShowSignOut(false);
         navigate(RouteUrls.Home);
       }}
     />
