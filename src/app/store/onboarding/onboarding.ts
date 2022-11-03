8f7d61d01e2799987fa50437dcd02d2d14e9f5ef
Eduard Bardají Puig <edu@trustmachines.co>
2022-11-03 14:17:55 +0000

    feat: capture analytics on consent screen only by default, closes #2784

--- src/app/store/onboarding/onboarding.ts
+++ src/app/store/onboarding/onboarding.ts
@@ -1,12 +1,4 @@
 import { atom } from 'jotai';
-import { atomWithStorage } from 'jotai/utils';
-
-import { userHasAllowedDiagnosticsKey } from '@shared/utils/storage';
 
 export const seedInputErrorState = atom<string | undefined>(undefined);
 export const secretKeyState = atom(null);
-
-export const hasAllowedDiagnosticsState = atomWithStorage<boolean | undefined>(
-  userHasAllowedDiagnosticsKey,
-  undefined
-);
