c22f7173fe333c59182172482cfa3fa79681afde
fbwoolf <fwoolf@blockstack.com>
2022-11-10 11:03:54 -0600

    feat: send form amount field

---
+++ src/app/pages/send-crypto-asset/send-crypto-asset-form/components/send-all-button.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@stacks/ui';
+
+export function SendAllButton() {
+  return (
+    <Button
+      borderRadius="12px"
+      fontSize={0}
+      height="32px"
+      onClick={() => {}}
+      mode="tertiary"
+      px="tight"
+      type="button"
+      width="70px"
+    >
+      Send all
+    </Button>
+  );
+}
