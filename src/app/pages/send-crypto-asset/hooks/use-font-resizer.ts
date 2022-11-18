e6b67d88a4c4c10d2d2c8150414462dd50938938
fbwoolf <fwoolf@blockstack.com>
2022-11-18 14:01:32 -0600

    fix: amount input in extension

--- src/app/pages/send-crypto-asset/hooks/use-font-resizer.ts
+++ src/app/pages/send-crypto-asset/hooks/use-font-resizer.ts
@@ -1,38 +1,56 @@
 import { useCallback, useState } from 'react';
 
 import { useOnMount } from '@app/common/hooks/use-on-mount';
+import { createDelay } from '@app/common/utils';
 
 const maxFontSize = 48;
 const minFontSize = 14;
 
-export const amountInputId = 'crypto-send-form-amount';
+export const amountInputId = 'amount-input';
+export const assetSymbolId = 'asset-symbol';
 export const maxInputContainerWidth = 344;
+
+const waitForSymbolToLoad = createDelay(500);
 
 export function useFontResizer() {
   const [inputFontSize, setInputFontSize] = useState(maxFontSize + 'px');
+  const [symbolTextWidth, setSymbolTextWidth] = useState(100);
 
-  const onChange = useCallback(event => {
-    const el = event.target;
+  const onChange = useCallback(
+    event => {
+      const el = event.target;
+      const symbol = document.getElementById(assetSymbolId);
+      if (!symbol) return;
 
-    let fontSize = maxFontSize;
-    el.style.fontSize = fontSize + 'px';
-    setInputFontSize(fontSize + 'px');
-    while (fontSize > minFontSize && el.scrollWidth > el.clientWidth) {
-      fontSize--;
+      let fontSize = maxFontSize;
       el.style.fontSize = fontSize + 'px';
       setInputFontSize(fontSize + 'px');
-    }
-  }, []);
+
+      while (fontSize > minFontSize && el.scrollWidth > symbolTextWidth) {
+        fontSize--;
+        el.style.fontSize = fontSize + 'px';
+        setInputFontSize(fontSize + 'px');
+      }
+    },
+    [symbolTextWidth]
+  );
 
   useOnMount(() => {
     const input = document.getElementById(amountInputId);
-    if (!input) return;
+    const symbol = document.getElementById(assetSymbolId);
+    if (!input || !symbol) return;
+
     input.addEventListener('input', onChange);
-    input.style.fontSize = maxFontSize + 'px';
+
+    const getSymbolTextWidth = async () => {
+      await waitForSymbolToLoad();
+      setSymbolTextWidth(symbol.offsetWidth + 4);
+    };
+    void getSymbolTextWidth();
     return () => {
-      document.removeEventListener('input', onChange);
+      input.removeEventListener('input', onChange);
     };
   });
 
-  return { inputFontSize };
+  return { inputFontSize, symbolTextWidth };
 }
