0ed015bee8d1b39c3564136d9c9488468ad55585
kyranjamie <kyran.burraston@gmail.com>
2022-11-28 13:43:54 +0100

    chore: add window.open lint rule

--- src/app/common/utils/open-in-new-tab.ts
+++ src/app/common/utils/open-in-new-tab.ts
@@ -3,6 +3,8 @@ import { isValidUrl } from '@shared/utils/validate-url';
 
 export function openInNewTab(url: string) {
   if (!isValidUrl(url)) return;
+  // Only place window.open may be called
+  // eslint-disable-next-line no-restricted-properties
   const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
   if (newWindow) newWindow.opener = null;
 }
