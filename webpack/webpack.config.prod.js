febde6a3a39d92b93776745fdc9cea7b637622fb
kyranjamie <kyran.burraston@gmail.com>
2022-12-22 14:32:05 +0000

    fix: track sentry releases, source maps, closes #2915, #2914

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,8 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const config = require('./webpack.config.base');
+const package = require('../package.json');
 const { ESBuildMinifyPlugin } = require('esbuild-loader');
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
 const shouldMinify = JSON.parse(process.env.MINIFY_PRODUCTION_BUILD || false);
+const sentryAuthToken = process.env.SENTRY_AUTH_TOKEN;
 
 config.mode = 'production';
 
@@ -31,6 +34,27 @@ config.optimization = {
       }
     : {}),
 };
+
+config.plugins = [
+  ...config.plugins,
+  ...(sentryAuthToken
+    ? [
+        new SentryWebpackPlugin({
+          org: 'trust-machines',
+          project: 'hiro-wallet',
+
+          // Specify the directory containing build artifacts
+          include: './dist',
+
+          // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+          // and needs the `project:releases` and `org:read` scopes
+          authToken: sentryAuthToken,
+
+          release: package.version,
+        }),
+      ]
+    : []),
+];
 
 config.devtool = 'source-map';
 
