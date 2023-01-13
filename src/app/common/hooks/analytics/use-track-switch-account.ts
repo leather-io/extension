a768c0f5c433e17f0ee86fe4292734cdedc41568
kyranjamie <kyran.burraston@gmail.com>
2023-01-13 11:11:14 +0100

    chore: alt implementation, query cache

--- src/app/common/hooks/analytics/use-track-switch-account.ts
+++ src/app/common/hooks/analytics/use-track-switch-account.ts
@@ -1,21 +1,27 @@
 import { useCallback } from 'react';
 
+import { queryClient } from '@app/common/persistence';
 import { parseBalanceResponse } from '@app/query/stacks/balance/balance.hooks';
-import { useStacksClientAnchored } from '@app/store/common/api-clients.hooks';
 
 import { useAnalytics } from './use-analytics';
 
 export function useTrackSwitchAccount() {
   const analytics = useAnalytics();
-  const client = useStacksClientAnchored();
 
   return useCallback(
     async (address: string, index: number) => {
-      const balancesResp = await client.accountsApi.getAccountBalance({ principal: address });
-      const balances = parseBalanceResponse(balancesResp as any);
-      const hasStxBalance = !!balances?.stx.availableStx.amount.isGreaterThan(0);
-      void analytics.track('switch_account', { index, hasStxBalance });
+      const accountBalanceCache = queryClient.getQueryData([
+        'get-address-anchored-stx-balance',
+        address,
+      ]);
+      if (!accountBalanceCache) return;
+      try {
+        const balances = parseBalanceResponse(accountBalanceCache as any);
+        const hasStxBalance = !!balances?.stx.availableStx.amount.isGreaterThan(0);
+        void analytics.track('switch_account', { index, hasStxBalance });
+      } finally {
+      }
     },
-    [analytics, client.accountsApi]
+    [analytics]
   );
 }
