669f8a41e95b08b075df6c1897163488342f50cb
kyranjamie <kyran.burraston@gmail.com>
2023-01-23 11:17:33 +0100

    chore: fix failed merge

--- src/app/pages/send/send-crypto-asset-form/send-crypto-asset-form.tsx
+++ src/app/pages/send/send-crypto-asset-form/send-crypto-asset-form.tsx
@@ -6,22 +6,9 @@ import { isString } from '@shared/utils';
 import { useRouteHeader } from '@app/common/hooks/use-route-header';
 import { Header } from '@app/components/header';
 
-<<<<<<< HEAD
 import { SendCryptoAssetFormLayout } from './_components/send-crypto-asset-form.layout';
-import { BtcCryptoCurrencySendForm } from './btc/btc-crypto-currency-send-form';
 import { StacksSip10FungibleTokenSendForm } from './stacks-sip10/stacks-sip10-fungible-token-send-form';
 import { StxCryptoCurrencySendForm } from './stx/stx-crypto-currency-send-form';
-||||||| d62440114
-import { SendCryptoAssetLayout } from './components/send-crypto-asset.layout';
-import { BitcoinCryptoCurrencySendForm } from './forms/btc/btc-crypto-currency-send-form';
-import { StacksFungibleTokenSendForm } from './forms/stx-sip10/stacks-fungible-token-send-form';
-import { StacksCryptoCurrencySendForm } from './forms/stx/stx-crypto-currency-send-form';
-=======
-import { ChooseCryptoAssetLayout } from './components/send-crypto-asset.layout';
-import { BtcCryptoCurrencySendForm } from './forms/btc/btc-crypto-currency-send-form';
-import { StacksFungibleTokenSendForm } from './forms/stx-sip10/stacks-fungible-token-send-form';
-import { StxCryptoCurrencySendForm } from './forms/stx/stx-crypto-currency-send-form';
->>>>>>> origin/main
 
 export function SendCryptoAssetForm() {
   const { symbol } = useParams();
@@ -38,7 +25,7 @@ export function SendCryptoAssetForm() {
   const content = (() => {
     switch (symbol) {
       case 'btc':
-        return <BtcCryptoCurrencySendForm />;
+        return <StxCryptoCurrencySendForm />;
 
       case 'stx':
         return <StxCryptoCurrencySendForm />;
@@ -50,11 +37,5 @@ export function SendCryptoAssetForm() {
     }
   })();
 
-<<<<<<< HEAD
   return <SendCryptoAssetFormLayout>{content}</SendCryptoAssetFormLayout>;
-||||||| d62440114
-  return <SendCryptoAssetLayout>{content}</SendCryptoAssetLayout>;
-=======
-  return <ChooseCryptoAssetLayout>{content}</ChooseCryptoAssetLayout>;
->>>>>>> origin/main
 }
