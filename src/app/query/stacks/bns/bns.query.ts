48001d207ad3bdc04b073dafe667d9749ba5c85f
Hank Stoever <hstove@gmail.com>
2023-02-02 15:42:17 -0800

    feat: fetch BNSx names on mainnet

--- src/app/query/stacks/bns/bns.query.ts
+++ src/app/query/stacks/bns/bns.query.ts
@@ -1,8 +1,11 @@
+import { BnsNamesOwnByAddressResponse } from '@stacks/stacks-blockchain-api-types';
 import { useQuery } from '@tanstack/react-query';
 
+import { fetcher as fetchApi } from '@app/common/api/wrapped-fetch';
 import { AppUseQueryConfig } from '@app/query/query-config';
 import { StacksClient } from '@app/query/stacks/stacks-client';
 import { useStacksClientUnanchored } from '@app/store/common/api-clients.hooks';
+import { useCurrentNetworkState } from '@app/store/networks/networks.hooks';
 
 import { RateLimiter, useHiroApiRateLimiter } from '../rate-limiter';
 
@@ -17,10 +20,34 @@ const bnsQueryOptions = {
   refetchOnReconnect: false,
 } as const;
 
-function getBnsNameFetcherFactory(client: StacksClient, limiter: RateLimiter) {
-  return async (address: string) => {
+// Fetch names owned by address via BNSx API
+// https://docs.bns.xyz/integrate-bnsx/bnsx-api
+async function getBnsxName(address: string): Promise<BnsNamesOwnByAddressResponse> {
+  const url = `https://api.bns.xyz/v1/addresses/stacks/${address}`;
+  const response = await fetchApi(url);
+  if (!response.ok) {
+    throw new Error('Unable to fetch BNSx names');
+  }
+  return response.json();
+}
+
+// Fetch names owned by an address.
+//
+// If `isTestnet` is `false` (aka mainnet), names are fetched from the
+// BNSx API.
+function getBnsNameFetcherFactory(client: StacksClient, limiter: RateLimiter, isTestnet: boolean) {
+  const baseFetcher = async (address: string) => {
     await limiter.removeTokens(1);
     return client.namesApi.getNamesOwnedByAddress({ address, blockchain: 'stacks' });
+  };
+  if (isTestnet) {
+    return baseFetcher;
+  }
+  // Fetch from BNSx API, and fallback to vanilla API in case
+  // of error.
+  return async (address: string) => {
+    const bnsxFetch = getBnsxName(address);
+    return bnsxFetch.catch(() => baseFetcher(address));
   };
 }
 
@@ -31,11 +58,12 @@ export function useGetBnsNamesOwnedByAddress<T extends unknown = BnsNameFetcherR
   options?: AppUseQueryConfig<BnsNameFetcherResp, T>
 ) {
   const client = useStacksClientUnanchored();
+  const { isTestnet } = useCurrentNetworkState();
   const limiter = useHiroApiRateLimiter();
   return useQuery({
     enabled: address !== '',
     queryKey: ['bns-names-by-address', address],
-    queryFn: () => getBnsNameFetcherFactory(client, limiter)(address),
+    queryFn: () => getBnsNameFetcherFactory(client, limiter, isTestnet)(address),
     ...bnsQueryOptions,
     ...options,
   });
