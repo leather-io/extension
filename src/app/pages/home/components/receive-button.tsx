549732d671f37fbfbef23f13220b2c134a1cea81
kyranjamie <kyran.burraston@gmail.com>
2023-02-04 08:58:16 +0100

    refactor: receive modal to use layout component
    fix: wip changes

--- src/app/pages/home/components/receive-button.tsx
+++ src/app/pages/home/components/receive-button.tsx
@@ -6,16 +6,18 @@ import { RouteUrls } from '@shared/route-urls';
 
 import { PrimaryButton } from '@app/components/primary-button';
 import { QrCodeIcon } from '@app/components/qr-code-icon';
+import { useBitcoinFeature } from '@app/store/feature-flags/feature-flags.slice';
 
 import { HomeActionButton } from './tx-button';
 
 export function ReceiveButton(props: ButtonProps) {
   const navigate = useNavigate();
+  const bitcoinFeature = useBitcoinFeature();
 
   return (
     <HomeActionButton
       icon={QrCodeIcon}
-      onClick={() => navigate(RouteUrls.Receive)}
+      onClick={() => navigate(bitcoinFeature ? RouteUrls.Receive : RouteUrls.ReceiveStx)}
       label="Receive"
       buttonComponent={PrimaryButton}
       {...props}
