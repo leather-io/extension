aa391d063cba50eff85154c6be18e405a0695b72
fbwoolf <fwoolf@blockstack.com>
2023-02-15 15:07:32 -0600

    Revert "fix: branch name env variable"
    This reverts commit afad56246612ff4bc16074b8e75d27668b115126.

--- src/shared/environment.ts
+++ src/shared/environment.ts
@@ -1,6 +1,6 @@
 export const BITCOIN_ENABLED = process.env.BITCOIN_ENABLED;
 export const BRANCH = process.env.GITHUB_REF;
-export const BRANCH_NAME = process.env.BRANCH_NAME;
+export const BRANCH_NAME = process.env.GITHUB_HEAD_REF;
 export const COINBASE_APP_ID = process.env.COINBASE_APP_ID ?? '';
 export const COMMIT_SHA = process.env.GITHUB_SHA;
 export const IS_DEV_ENV = process.env.WALLET_ENVIRONMENT === 'development';
