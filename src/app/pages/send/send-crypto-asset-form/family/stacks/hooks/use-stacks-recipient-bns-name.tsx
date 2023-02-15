900c93a18ea11e548c1ae94d50d5b8aebe7de372
Hank Stoever <hstove@gmail.com>
2023-02-15 08:24:23 -0800

    feat: fetch owner of a name

--- src/app/pages/send/send-crypto-asset-form/family/stacks/hooks/use-stacks-recipient-bns-name.tsx
+++ src/app/pages/send/send-crypto-asset-form/family/stacks/hooks/use-stacks-recipient-bns-name.tsx
@@ -2,23 +2,28 @@ import { useCallback, useState } from 'react';
 
 import { useField } from 'formik';
 
+import { fetchNameOwner } from '@app/query/stacks/bns/bns.utils';
 import { useStacksClientUnanchored } from '@app/store/common/api-clients.hooks';
+import { useCurrentNetworkState } from '@app/store/networks/networks.hooks';
 
 export function useStacksRecipientBnsName() {
   const [, _, recipientFieldHelpers] = useField('recipient');
   const [recipientAddressOrBnsNameField] = useField('recipientAddressOrBnsName');
   const [bnsAddress, setBnsAddress] = useState('');
   const client = useStacksClientUnanchored();
+  const { isTestnet } = useCurrentNetworkState();
 
   const getBnsAddress = useCallback(async () => {
     setBnsAddress('');
     try {
-      const res = await client.namesApi.getNameInfo({
-        name: recipientAddressOrBnsNameField.value ?? '',
-      });
-      if (res.address) {
-        recipientFieldHelpers.setValue(res.address);
-        setBnsAddress(res.address);
+      const owner = await fetchNameOwner(
+        client,
+        recipientAddressOrBnsNameField.value ?? '',
+        isTestnet
+      );
+      if (owner) {
+        recipientFieldHelpers.setValue(owner);
+        setBnsAddress(owner);
       } else {
         recipientFieldHelpers.setValue(recipientAddressOrBnsNameField.value);
       }
@@ -26,7 +31,7 @@ export function useStacksRecipientBnsName() {
     } catch {
       recipientFieldHelpers.setValue(recipientAddressOrBnsNameField.value);
     }
-  }, [client.namesApi, recipientAddressOrBnsNameField.value, recipientFieldHelpers]);
+  }, [recipientAddressOrBnsNameField.value, recipientFieldHelpers, isTestnet, client]);
 
   return { getBnsAddress, bnsAddress };
 }
