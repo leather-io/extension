7febe0ba71a0dd3c972050f9e3d0972622b972ad
Eduard Bardají Puig <edu@trustmachines.co>
2023-02-19 20:44:55 +0000

    feat: support multiple ordinal types

--- src/app/features/collectibles/ordinals.tsx
+++ src/app/features/collectibles/ordinals.tsx
@@ -1,9 +1,10 @@
+import OrdinalLogo from '@assets/images/ordinal-logo.svg';
 import { Box, Text, color } from '@stacks/ui';
 
 // import { useQuery } from '@tanstack/react-query';
 import { openInNewTab } from '@app/common/utils/open-in-new-tab';
 import { Caption } from '@app/components/typography';
-import { useGetOrdinalsQuery } from '@app/query/bitcoin/ordinals/ordinals.query';
+import { OrdinalType, useGetOrdinalsQuery } from '@app/query/bitcoin/ordinals/ordinals.query';
 
 interface OrdinalsProps {
   query: ReturnType<typeof useGetOrdinalsQuery>;
@@ -32,32 +33,83 @@ export function Ordinals({ query }: OrdinalsProps) {
     <>
       {/* {qMockImages.data && */}
       {/*   qMockImages.data.map(ordinal => ( */}
-      {data.map(ordinal => (
-        <Box>
-          <Box pb="base">
-            <Box
-              onClick={() => openInNewTab(ordinal.preview)}
-              borderRadius="12px"
-              sx={{ overflow: 'hidden' }}
-              _hover={{ cursor: 'pointer' }}
-            >
-              <img
-                key={ordinal.content}
-                src={ordinal.content}
-                style={{ aspectRatio: '1 / 1', objectFit: 'cover' }}
-              />
-            </Box>
-          </Box>
-          <Box pb="base-tight">
-            <Text pb="extra-tight" fontWeight="500" color={color('text-body')}>
-              {ordinal.title}
-            </Text>
+      {data.map(ordinal => {
+        switch (ordinal.type) {
+          case OrdinalType.Image: {
+            return (
+              <Box>
+                <Box pb="base">
+                  <Box
+                    onClick={() => openInNewTab(ordinal.infoUrl)}
+                    borderRadius="12px"
+                    sx={{ overflow: 'hidden' }}
+                    _hover={{ cursor: 'pointer' }}
+                  >
+                    <img
+                      key={ordinal.content}
+                      src={ordinal.content.replace('content', 'inscription')}
+                      style={{ aspectRatio: '1 / 1', objectFit: 'cover' }}
+                    />
+                  </Box>
+                </Box>
+                <Box pb="base-tight">
+                  <Text pb="extra-tight" fontWeight="500" color={color('text-body')}>
+                    {ordinal.title}
+                  </Text>
 
-            {/* NOTE: using `style` since font size gets overriden when using `fontSize` or `sx`. */}
-            <Caption style={{ fontSize: '12px' }}>Ordinal inscription</Caption>
-          </Box>
-        </Box>
-      ))}
+                  {/* NOTE: using `style` since font size gets overriden when using `fontSize` or `sx`. */}
+                  <Caption style={{ fontSize: '12px' }}>Ordinal inscription</Caption>
+                </Box>
+              </Box>
+            );
+          }
+          case OrdinalType.Other: {
+            return (
+              <Box>
+                <Box pb="base">
+                  <Box
+                    sx={{
+                      position: 'relative',
+                      paddingBottom: '100%',
+                      height: '0',
+                    }}
+                  >
+                    <Box
+                      onClick={() => openInNewTab('#test')}
+                      borderRadius="12px"
+                      backgroundColor="black" // NOTE: color not yet available from `@stacks/ui`.
+                      sx={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                      }}
+                      _hover={{ cursor: 'pointer' }}
+                    >
+                      <img src={OrdinalLogo} />
+                    </Box>
+                  </Box>
+                </Box>
+                <Box pb="base-tight">
+                  <Text pb="extra-tight" fontWeight="500" color={color('text-body')}>
+                    {ordinal.title}
+                  </Text>
+
+                  {/* NOTE: using `style` since font size gets overriden when using `fontSize` or `sx`. */}
+                  <Caption style={{ fontSize: '12px' }}>Ordinal inscription</Caption>
+                </Box>
+              </Box>
+            );
+          }
+          default: {
+            return null;
+          }
+        }
+      })}
     </>
   );
 }
--- src/app/query/bitcoin/ordinals/ordinals.query.ts
+++ src/app/query/bitcoin/ordinals/ordinals.query.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 
+import { Ordinals } from '@app/features/collectibles/ordinals';
 import { useCurrentTaprootAccountKeychain } from '@app/store/accounts/blockchain/bitcoin/taproot-account.hooks';
 import { useBitcoinClient } from '@app/store/common/api-clients.hooks';
 import { useCurrentNetwork } from '@app/store/networks/networks.selectors';
@@ -13,6 +14,49 @@ import {
 
 const stopSearchAfterNumberAddressesWithoutOrdinals = 20;
 
+/**
+ * Ordinals contain arbitrary data. When retrieving an ordinal, we should
+ * classify it into one of the types below, indicating that it is handled
+ * appropriately. Ordinals of types not ready to be handled by the app should be
+ * classified as `Other`.
+ */
+export enum OrdinalType {
+  Image = 'IMAGE',
+  Other = 'OTHER',
+}
+interface BaseOrdinalInfo {
+  /**
+   * The kind of ordinal as classified by this app. Different kinds of ordinals
+   * require different treatment (e.g., images vs documents).
+   */
+  type: OrdinalType;
+
+  /**
+   * Title which can be rendered in the UI alongside the ordinal.
+   */
+  title: string;
+
+  /**
+   * A link to a detailed techincal description about the ordinal.
+   */
+  infoUrl: string;
+}
+interface ImageOrdinalInfo extends BaseOrdinalInfo {
+  /**
+   * A URL of where the image can be downloaded from.
+   */
+  type: OrdinalType.Image;
+  content: string;
+}
+interface OtherOrdinalInfo extends BaseOrdinalInfo {
+  /**
+   * A URL of where the image can be downloaded from.
+   */
+  type: OrdinalType.Other;
+}
+
+type OrdinalInfo = ImageOrdinalInfo | OtherOrdinalInfo;
+
 export function useGetOrdinalsQuery() {
   const network = useCurrentNetwork();
   const keychain = useCurrentTaprootAccountKeychain();
@@ -24,8 +68,7 @@ export function useGetOrdinalsQuery() {
     async () => {
       let currentNumberOfAddressesWithoutOrdinals = 0;
       let index = 0;
-      // TODO: extract to type
-      const foundOrdinals: { title: string; content: string; preview: string }[] = [];
+      const foundOrdinals: OrdinalInfo[] = [];
 
       // What is this loop doing?
       //
@@ -78,11 +121,28 @@ export function useGetOrdinalsQuery() {
         const validatedResDataOrdApiInscriptions =
           ordApiXyzGetInscriptionByInscriptionSchema.validateSync(parsedResponseInscriptions);
 
+        // TODO: this if/else contains some repetition and works due to the
+        // limited amount of ordinal types currently supported. Perhaps worth
+        // converting to switch when new ordinal types are supported and
+        // extracting reusable code from each branch.
         if (validatedResDataOrdApiInscriptions['content type'].startsWith('image/')) {
           foundOrdinals.push({
+            type: OrdinalType.Image,
             title: validatedResDataOrdApiInscriptions.title,
+            infoUrl: `https://ordinals.com${validatedResDataOrdApiInscriptions.content}`.replace(
+              'content',
+              'inscription'
+            ),
             content: `https://ordinals.com${validatedResDataOrdApiInscriptions.content}`,
-            preview: `https://ordinals.com${validatedResDataOrdApiInscriptions.preview}`,
+          });
+        } else {
+          foundOrdinals.push({
+            type: OrdinalType.Other,
+            title: validatedResDataOrdApiInscriptions.title,
+            infoUrl: `https://ordinals.com${validatedResDataOrdApiInscriptions.content}`.replace(
+              'content',
+              'inscription'
+            ),
           });
         }
         index += 1;
