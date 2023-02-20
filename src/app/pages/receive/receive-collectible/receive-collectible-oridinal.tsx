a1e937a058da988cf35ef0653b681a6e8ecf9cda
fbwoolf <fwoolf@blockstack.com>
2023-02-20 11:56:00 -0600

    refactor: ordinal modal

--- src/app/pages/receive/receive-collectible/receive-collectible-oridinal.tsx
+++ src/app/pages/receive/receive-collectible/receive-collectible-oridinal.tsx
@@ -1,14 +1,15 @@
 import toast from 'react-hot-toast';
+import { FiArrowUpRight } from 'react-icons/fi';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { Box, Button, Flex, Stack, color, useClipboard } from '@stacks/ui';
-
-import { RouteUrls } from '@shared/route-urls';
+import { Box, Stack, color, useClipboard } from '@stacks/ui';
 
 import { useAnalytics } from '@app/common/hooks/analytics/use-analytics';
+import { openInNewTab } from '@app/common/utils/open-in-new-tab';
 import { BaseDrawer } from '@app/components/drawer/base-drawer';
 import { ErrorLabel } from '@app/components/error-label';
 import { OrdinalIcon } from '@app/components/icons/ordinal-icon';
+import { Divider } from '@app/components/layout/divider';
 import { PrimaryButton } from '@app/components/primary-button';
 import { Text, Title } from '@app/components/typography';
 
@@ -32,27 +33,51 @@ export function ReceiveCollectibleOrdinal() {
           <Title fontSize="20px" lineHeight="28px" px="extra-loose">
             Deposit Ordinal inscription
           </Title>
-          <ErrorLabel>Important to keep in mind</ErrorLabel>
-          <Text color={color('text-title')} fontSize={1} px={['base', '40px']}>
-            This Taproot address is for Ordinals only. Do not use this address more than once for
-            depositing an inscription
+          <Text color={color('text-title')} fontSize={1} px={['50px', '70px']}>
+            Use your wallet to create inscriptions within minutes on
+          </Text>
+          <Stack alignItems="center" pb={['unset', 'base']}>
+            <Stack
+              _hover={{ cursor: 'pointer' }}
+              alignItems="center"
+              as="button"
+              color={color('accent')}
+              fontSize={1}
+              isInline
+              onClick={() => openInNewTab('https://gamma.io/')}
+              spacing="extra-tight"
+              textDecoration="underline"
+              type="button"
+            >
+              <Text color="inherit">Gamma.io</Text>
+              <FiArrowUpRight />
+            </Stack>
+            <Stack
+              _hover={{ cursor: 'pointer' }}
+              alignItems="center"
+              as="button"
+              color={color('accent')}
+              fontSize={1}
+              isInline
+              onClick={() => openInNewTab('https://ordinalsbot.com/')}
+              spacing="extra-tight"
+              textDecoration="underline"
+              type="button"
+            >
+              <Text color="inherit">ordinalsbot.com</Text>
+              <FiArrowUpRight />
+            </Stack>
+          </Stack>
+          <Divider />
+          <ErrorLabel mt={['unset', 'base']}>Important to keep in mind</ErrorLabel>
+          <Text color={color('text-title')} fontSize={1} px={['unset', 'loose']}>
+            This Taproot address is for Ordinals only. Do not deposit other BTC here or you may have
+            difficulty retrieving it
           </Text>
         </Stack>
-        <Flex alignItems="center" justifyContent="center" my="extra-loose" width="100%">
-          <Button
-            borderRadius="10px"
-            height="48px"
-            mode="tertiary"
-            mr="base"
-            onClick={() => navigate(RouteUrls.ReceiveCollectible)}
-            type="button"
-          >
-            Go back
-          </Button>
-          <PrimaryButton flexGrow={1} onClick={copyToClipboard}>
-            Proceed to copy address
-          </PrimaryButton>
-        </Flex>
+        <PrimaryButton flexGrow={1} my="extra-loose" onClick={copyToClipboard} width="100%">
+          Copy address
+        </PrimaryButton>
       </Box>
     </BaseDrawer>
   );
