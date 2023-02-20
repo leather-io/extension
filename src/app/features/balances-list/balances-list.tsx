921a25e6ba61eb13d4f81aed23de019193d3550b
kyranjamie <kyran.burraston@gmail.com>
2023-02-20 13:31:12 +0100

    fix: remove empty state, closes #3206

--- src/app/features/balances-list/balances-list.tsx
+++ src/app/features/balances-list/balances-list.tsx
@@ -1,10 +1,5 @@
-import { useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
-
 import { Box, Stack, StackProps } from '@stacks/ui';
 import { HomePageSelectorsLegacy } from '@tests-legacy/page-objects/home.selectors';
-
-import { RouteUrls } from '@shared/route-urls';
 
 import { CryptoCurrencyAssetItem } from '@app/components/crypto-assets/crypto-currency-asset/crypto-currency-asset-item';
 import { StxAvatar } from '@app/components/crypto-assets/stacks/components/stx-avatar';
@@ -21,7 +16,6 @@ import {
 import { useCurrentBtcNativeSegwitAccountAddressIndexZero } from '@app/store/accounts/blockchain/bitcoin/native-segwit-account.hooks';
 
 import { Collectibles } from '../collectibles/collectibles';
-import { FundAccount } from './components/fund-account';
 import { StacksFungibleTokenAssetList } from './components/stacks-fungible-token-asset-list';
 import { StacksNonFungibleTokenAssetList } from './components/stacks-non-fungible-token-asset-list';
 
@@ -35,22 +29,10 @@ export function BalancesList({ address, ...props }: BalancesListProps) {
   const btcAssetBalance = useBitcoinAssetBalance(bitcoinAddress);
   const stacksFtAssetBalances = useStacksFungibleTokenAssetBalancesAnchoredWithMetadata(address);
   const { data: stacksNftAssetBalances = [] } = useStacksNonFungibleTokenAssetsUnanchored();
-  const navigate = useNavigate();
   const isBitcoinEnabled = useConfigBitcoinEnabled();
-
-  const handleFundAccount = useCallback(() => navigate(RouteUrls.Fund), [navigate]);
 
   // Better handle loading state
   if (!stxAssetBalance || !stxUnachoredAssetBalance) return <LoadingSpinner />;
-
-  const noAssets =
-    btcAssetBalance.balance.amount.isEqualTo(0) &&
-    stxAssetBalance.balance.amount.isEqualTo(0) &&
-    stxUnachoredAssetBalance.balance.amount.isEqualTo(0) &&
-    stacksFtAssetBalances.length === 0 &&
-    stacksNftAssetBalances.length === 0;
-
-  if (noAssets) return <FundAccount onFundAccount={handleFundAccount} {...props} />;
 
   return (
     <Stack
@@ -59,17 +41,16 @@ export function BalancesList({ address, ...props }: BalancesListProps) {
       data-testid={HomePageSelectorsLegacy.BalancesList}
       {...props}
     >
-      {btcAssetBalance.balance.amount.isGreaterThan(0) && isBitcoinEnabled && (
+      {isBitcoinEnabled && (
         <CryptoCurrencyAssetItem assetBalance={btcAssetBalance} icon={<Box as={BtcIcon} />} />
       )}
-      {(stxAssetBalance.balance.amount.isGreaterThan(0) ||
-        stxUnachoredAssetBalance.balance.amount.isGreaterThan(0)) && (
-        <CryptoCurrencyAssetItem
-          assetBalance={stxAssetBalance}
-          assetSubBalance={stxUnachoredAssetBalance}
-          icon={<StxAvatar {...props} />}
-        />
-      )}
+
+      <CryptoCurrencyAssetItem
+        assetBalance={stxAssetBalance}
+        assetSubBalance={stxUnachoredAssetBalance}
+        icon={<StxAvatar {...props} />}
+      />
+
       <StacksFungibleTokenAssetList assetBalances={stacksFtAssetBalances} />
       <StacksNonFungibleTokenAssetList assetBalances={stacksNftAssetBalances} />
       <Collectibles />
