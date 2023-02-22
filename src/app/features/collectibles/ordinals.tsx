543d173755afcaa50f40ee96d4b4d037c7240f2f
Fara Woolf <fwoolf@blockstack.com>
2023-02-22 11:16:32 -0600

    Revert "refactor: clean up ordinals branching"
    This reverts commit 6300c76f796a14f6b8f773a5fcd22df4dd59079b.

--- src/app/features/collectibles/ordinals.tsx
+++ src/app/features/collectibles/ordinals.tsx
@@ -1,5 +1,6 @@
+// import { useQuery } from '@tanstack/react-query';
 import { openInNewTab } from '@app/common/utils/open-in-new-tab';
-import { useGetOrdinalsQuery } from '@app/query/bitcoin/ordinals/ordinals.query';
+import { OrdinalType, useGetOrdinalsQuery } from '@app/query/bitcoin/ordinals/ordinals.query';
 
 import { ImageCollectible } from './components/image-collectible';
 import { OtherCollectible } from './components/other-collectible';
@@ -8,23 +9,60 @@ interface OrdinalsProps {
   query: ReturnType<typeof useGetOrdinalsQuery>;
 }
 export function Ordinals({ query }: OrdinalsProps) {
-  const { isLoading, isError, data: ordinals } = query;
+  const { isLoading, isError, data } = query;
+
+  // const qMockImages = useQuery(['mock'], async () => {
+  //   const res = await fetch('https://picsum.photos/v2/list');
+  //   const resParsed = await res.json();
+  //   return resParsed.map(i => ({
+  //     title: 'Foo title',
+  //     content: i.download_url,
+  //     preview: `http://localhost:1234?dl=${i.download_url}`,
+  //   }));
+  // });
+  // if (Math.random() < 1) {
+  //   return (
+  //     <>
+  //       <p>working?</p>
+  //       {qMockImages.data &&
+  //         qMockImages.data
+  //           .slice(12)
+  //           .map(ordinal => (
+  //             <ImageCollectible
+  //               onClick={() => alert('clicked')}
+  //               src={ordinal.content}
+  //               subtitle="Ordinal inscription"
+  //               title={ordinal.title}
+  //             />
+  //           ))}
+  //     </>
+  //   );
+  // }
+
+  // if (Math.random() < 1) {
+  //   return (
+  //     <OtherCollectible
+  //       onClick={() => alert('clicked')}
+  //       subtitle="Ordinal inscription"
+  //       title="Foo"
+  //     />
+  //   );
+  // }
 
   if (isLoading) return null;
 
   // TODO: user facing error handling.
   if (isError) return null;
 
-  if (ordinals.length === 0) return null;
+  if (data.length === 0) return null;
 
   return (
     <>
-      {ordinals.map(ordinal => {
+      {data.map(ordinal => {
         switch (ordinal.type) {
-          case 'image': {
+          case OrdinalType.Image: {
             return (
               <ImageCollectible
-                key={ordinal.infoUrl}
                 onClick={() => openInNewTab(ordinal.infoUrl)}
                 src={ordinal.content}
                 subtitle="Ordinal inscription"
@@ -32,10 +70,9 @@ export function Ordinals({ query }: OrdinalsProps) {
               />
             );
           }
-          case 'other': {
+          case OrdinalType.Other: {
             return (
               <OtherCollectible
-                key={ordinal.infoUrl}
                 onClick={() => openInNewTab(ordinal.infoUrl)}
                 subtitle="Ordinal inscription"
                 title={ordinal.title}
