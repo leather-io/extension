ddea6f2b274d5c675a0667e697f1953f99c64044
kyranjamie <kyran.burraston@gmail.com>
2023-02-23 15:48:48 +0100

    fix: asset page routing

--- src/app/pages/send/send-container.tsx
+++ src/app/pages/send/send-container.tsx
@@ -2,8 +2,6 @@ import { Outlet, useNavigate } from 'react-router-dom';
 
 import { Flex } from '@stacks/ui';
 
-import { RouteUrls } from '@shared/route-urls';
-
 import { useRouteHeader } from '@app/common/hooks/use-route-header';
 import { whenPageMode } from '@app/common/utils';
 import { CENTERED_FULL_PAGE_MAX_WIDTH } from '@app/components/global-styles/full-page-styles';
@@ -11,9 +9,11 @@ import { Header } from '@app/components/header';
 
 export function SendContainer() {
   const navigate = useNavigate();
+
   useRouteHeader(
-    <Header hideActions onClose={() => navigate(RouteUrls.SendCryptoAsset)} title="Send" />
+    <Header hideActions onClose={() => navigate('..', { relative: 'path' })} title="Send" />
   );
+
   return whenPageMode({
     full: (
       <Flex
