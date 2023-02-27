0a76fa650c1546d68e9ec9ad0465d3fb41e310e8
Eduard Bardají Puig <edu@trustmachines.co>
2023-02-27 18:03:52 +0000

    feat: collectibles refresh button

--- src/app/features/collectibles/collectibles.tsx
+++ src/app/features/collectibles/collectibles.tsx
@@ -1,7 +1,8 @@
 import { Flex, Grid, Spinner, color } from '@stacks/ui';
-import { useIsFetching } from '@tanstack/react-query';
+import { useIsFetching, useQueryClient } from '@tanstack/react-query';
 
 import { useWalletType } from '@app/common/use-wallet-type';
+import { Refresh } from '@app/components/icons/refresh';
 import { Caption } from '@app/components/typography';
 import { useConfigNftMetadataEnabled } from '@app/query/common/hiro-config/hiro-config.query';
 import { QueryPrefixes } from '@app/query/query-prefixes';
@@ -10,13 +11,14 @@ import { AddCollectible } from './components/add-collectible';
 import { Ordinals } from './components/ordinals';
 import { StacksCryptoAssets } from './components/stacks-crypto-assets';
 
-function isFetchingAnOrdinalsRelatedQuery(...args: number[]) {
+function isFetchingCollectiblesRelatedQuery(...args: number[]) {
   return args.reduce((acc, curr) => acc + curr, 0) > 0;
 }
 
 export function Collectibles() {
   const { whenWallet } = useWalletType();
   const isNftMetadataEnabled = useConfigNftMetadataEnabled();
+  const queryClient = useQueryClient();
 
   // Ordinal inscriptions
   const n1 = useIsFetching([QueryPrefixes.TaprootAddressUtxosMetadata]);
@@ -34,8 +36,15 @@ export function Collectibles() {
     <>
       <Flex flexDirection="row" justifyContent="left" columnGap="8px">
         <Caption>Collectibles</Caption>
-        {isFetchingAnOrdinalsRelatedQuery(n1, n2, n3, n4, n5, n6) && (
-          <Spinner color={color('text-caption')} opacity={0.5} size="xs" />
+        {isFetchingCollectiblesRelatedQuery(n1, n2, n3, n4, n5, n6) ? (
+          <Spinner color={color('text-caption')} opacity={0.5} size="16px" />
+        ) : (
+          <Refresh
+            cursor="pointer"
+            onClick={() => {
+              queryClient.refetchQueries({ type: 'active' });
+            }}
+          />
         )}
       </Flex>
       <Grid
