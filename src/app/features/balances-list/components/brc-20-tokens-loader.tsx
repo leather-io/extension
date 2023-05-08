a47c4ef84509a53c3b387ee71bece4685bc2a049
fbwoolf <fwoolf@blockstack.com>
2023-05-08 11:44:08 -0500

    fix: balances list tr breaking ledger

---
+++ src/app/features/balances-list/components/brc-20-tokens-loader.tsx
@@ -0,0 +1,15 @@
+import {
+  Brc20Token,
+  useBrc20TokensByAddressQuery,
+} from '@app/query/bitcoin/ordinals/brc20-tokens.query';
+import { useCurrentBtcTaprootAccountAddressIndexZeroPayment } from '@app/store/accounts/blockchain/bitcoin/taproot-account.hooks';
+
+interface Brc20TokensLoaderProps {
+  children(brc20Tokens: Brc20Token[]): JSX.Element;
+}
+export function Brc20TokensLoader({ children }: Brc20TokensLoaderProps) {
+  const { address: bitcoinAddressTaproot } = useCurrentBtcTaprootAccountAddressIndexZeroPayment();
+  const { data: brc20Tokens } = useBrc20TokensByAddressQuery(bitcoinAddressTaproot);
+  if (!bitcoinAddressTaproot || !brc20Tokens) return null;
+  return children(brc20Tokens.result.list);
+}
