e873225f2d6eef6736e119ee56fd8002ffcbafb8
fbwoolf <fwoolf@blockstack.com>
2023-05-10 08:28:51 -0500

    refactor: remove symbol from error msgs

--- src/app/common/error-formatters.ts
+++ src/app/common/error-formatters.ts
@@ -3,14 +3,10 @@ import { isFunction } from '@shared/utils';
 
 import { FormErrorMessages } from '@app/common/error-messages';
 
-export function formatErrorWithSymbol(symbol: string, error: string) {
-  return error.replace('{symbol}', symbol);
-}
-
 export function formatPrecisionError(num?: Money) {
   if (!num) return FormErrorMessages.CannotDeterminePrecision;
   const error = FormErrorMessages.TooMuchPrecision;
-  return formatErrorWithSymbol(num.symbol, error).replace('{decimals}', String(num.decimals));
+  return error.replace('{decimals}', String(num.decimals));
 }
 
 export function formatInsufficientBalanceError(
