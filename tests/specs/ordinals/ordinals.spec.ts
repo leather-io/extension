d5b3795416b95bf11a6945592ecb185239c1febc
alter-eggo <anastas3993@gmail.com>
2023-05-11 15:52:26 +0400

    feat: add hiro api test

--- tests/specs/ordinals/ordinals.spec.ts
+++ tests/specs/ordinals/ordinals.spec.ts
@@ -1,6 +1,8 @@
 import { test } from '@playwright/test';
 
+import { fetchInscription } from '@app/query/bitcoin/ordinals/inscription.query';
 import { getNumberOfInscriptionOnUtxo } from '@app/query/bitcoin/ordinals/ordinals-aware-utxo.query';
+import { fetchData } from '@app/query/utils';
 
 test.describe(getNumberOfInscriptionOnUtxo.name, () => {
   test('should return 3 in case of 3 inscriptions', async () => {
@@ -17,5 +19,22 @@ test.describe(getNumberOfInscriptionOnUtxo.name, () => {
       0
     );
     test.expect(resp).toBe(0);
+  });
+});
+
+test.describe('Check Hiro API', () => {
+  test('should get same inscription info as in ordapi', async () => {
+    const id = '43af4721f64f6db20ab2c0b41d084c5e3733d346925bd613abd6fd64ad2b6645i0';
+    const [respHiroApi, respOrdApi] = await Promise.all([
+      // fetch inscription from hiro api
+      fetchInscription()(id),
+      // fetch inscription from ordapi
+      fetchData({
+        errorMsg: 'Failed to fetch inscription',
+        url: `https://ordapi.xyz/inscription/${id}`,
+      }),
+    ]);
+    test.expect(respHiroApi.id).toBe(respOrdApi.id);
+    test.expect(respHiroApi.number).toBe(respOrdApi.inscription_number);
   });
 });
