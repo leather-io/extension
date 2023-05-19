affc42b2e132017cb14a6619e9059630052a119d
alter-eggo <anastas3993@gmail.com>
2023-05-19 16:52:41 +0400

    feat: show available btc balance in balances list and total balance, closes #3685

--- src/app/features/balances-list/balances-list.tsx
+++ src/app/features/balances-list/balances-list.tsx
@@ -11,6 +11,7 @@ import { LoadingSpinner } from '@app/components/loading-spinner';
 import { Brc20TokensLoader } from '@app/features/balances-list/components/brc-20-tokens-loader';
 import { useConfigBitcoinEnabled } from '@app/query/common/hiro-config/hiro-config.query';
 import { useStacksFungibleTokenAssetBalancesAnchoredWithMetadata } from '@app/query/stacks/balance/stacks-ft-balances.hooks';
+import { useCurrentBtcNativeSegwitAccountAddressIndexZero } from '@app/store/accounts/blockchain/bitcoin/native-segwit-account.hooks';
 
 import { Collectibles } from '../collectibles/collectibles';
 import { BitcoinFungibleTokenAssetList } from './components/bitcoin-fungible-tokens-asset-list';
@@ -23,8 +24,9 @@ interface BalancesListProps extends StackProps {
 export function BalancesList({ address, ...props }: BalancesListProps) {
   const stacksFtAssetBalances = useStacksFungibleTokenAssetBalancesAnchoredWithMetadata(address);
   const isBitcoinEnabled = useConfigBitcoinEnabled();
+  const btcAddress = useCurrentBtcNativeSegwitAccountAddressIndexZero();
   const { stxEffectiveBalance, stxEffectiveUsdBalance, isLoading } = useStxBalance();
-  const { btcAddress, btcAssetBalance, btcUsdBalance } = useBtcAssetBalance();
+  const { btcAvailableAssetBalance, btcAvailableUsdBalance } = useBtcAssetBalance(btcAddress);
   const { whenWallet } = useWalletType();
 
   // Better handle loading state
@@ -39,8 +41,8 @@ export function BalancesList({ address, ...props }: BalancesListProps) {
     >
       {isBitcoinEnabled && (
         <CryptoCurrencyAssetItem
-          assetBalance={btcAssetBalance}
-          usdBalance={btcUsdBalance}
+          assetBalance={btcAvailableAssetBalance}
+          usdBalance={btcAvailableUsdBalance}
           icon={<Box as={BtcIcon} />}
           address={btcAddress}
         />
