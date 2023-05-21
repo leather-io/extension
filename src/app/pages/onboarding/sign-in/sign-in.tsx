4bcabd6f6da512c31acf0b1e8dc9115fa66d9283
Laksh Dhamija <laksh.dhamija@gmail.com>
2023-05-22 00:14:09 +0530

    feat: Mask secret key during sign in

--- src/app/pages/onboarding/sign-in/sign-in.tsx
+++ src/app/pages/onboarding/sign-in/sign-in.tsx
@@ -19,10 +19,14 @@ import { PageTitle } from '@app/components/page-title';
 import { PrimaryButton } from '@app/components/primary-button';
 import { Title } from '@app/components/typography';
 import { useSignIn } from '@app/pages/onboarding/sign-in/hooks/use-sign-in';
+import { useState } from 'react';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
+import { Link } from '@app/components/link';
 
 export function SignIn() {
   const { onPaste, submitMnemonicForm, error, isLoading, ref } = useSignIn();
   const navigate = useNavigate();
+  const [isKeyMasked, setIsKeyMasked] = useState(true);
 
   const [desktopViewport] = useMediaQuery(`(min-width: ${DESKTOP_VIEWPORT_MIN_WIDTH})`);
 
@@ -72,7 +76,7 @@ export function SignIn() {
                   ref={ref as any}
                   spellCheck={false}
                   style={{ resize: 'none' }}
-                  value={form.values.secretKey}
+                  value={isKeyMasked? form.values.secretKey.replace(/[^ ]/g, '*') : form.values.secretKey}
                   width="100%"
                 />
                 {error && (
@@ -89,6 +93,18 @@ export function SignIn() {
                     </Text>
                   </ErrorLabel>
                 )}
+                <Stack alignItems="center">
+                  <Link
+                    fontSize="14px"
+                    _hover={{ textDecoration: 'none' }}
+                    onClick={() => setIsKeyMasked(!isKeyMasked)}
+                  >
+                    <Stack alignItems="center" isInline spacing="tight">
+                      {isKeyMasked ? <FiEye /> : <FiEyeOff />}
+                      <Text>{isKeyMasked ? 'Show' : 'Hide'} Secret Key</Text>
+                    </Stack>
+                  </Link>
+                </Stack>
               </Stack>
               <PrimaryButton
                 data-testid={OnboardingSelectors.SignInBtn}
