63e3c486fe5a5f2d2e7e51475a95b0d7b654896e
Laksh Dhamija <laksh.dhamija@gmail.com>
2023-06-14 21:44:34 +0530

    feat: masking secret key functionality while typing and copying key

--- src/app/pages/onboarding/sign-in/hooks/use-sign-in.ts
+++ src/app/pages/onboarding/sign-in/hooks/use-sign-in.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
@@ -24,6 +24,8 @@ async function simulateShortDelayToAvoidImmediateNavigation() {
 
 export function useSignIn() {
   const [error, setError] = useSeedInputErrorState();
+  const [isKeyMasked, setIsKeyMasked] = useState(true);
+  const [sanitizedSecretKey, setSanitizedSecretKey] = useState('');
 
   const { isLoading, setIsLoading, setIsIdle } = useLoading('useSignIn');
   const navigate = useNavigate();
@@ -97,6 +99,33 @@ export function useSignIn() {
     [submitMnemonicForm]
   );
 
+  const onChange = useCallback(
+    (
+      event: ChangeEvent<HTMLInputElement>,
+      handleFormChange: (e: ChangeEvent<HTMLInputElement>) => void
+    ) => {
+      const { value } = event.target;
+      setSanitizedSecretKey(previousSanitizedKey => {
+        // if value is shorter than sanitized secret key, remove characters
+        // from sanitized secret key
+        if (value.length < previousSanitizedKey.length) {
+          const removedChars = previousSanitizedKey.slice(value.length);
+          return previousSanitizedKey.slice(0, -removedChars.length);
+        } else {
+          // append new characters if value is longer than sanitized secret key
+          const newChars = value.slice(previousSanitizedKey.length);
+          return previousSanitizedKey + newChars;
+        }
+      });
+      handleFormChange(event);
+    },
+    []
+  );
+
+  const toggleKeyMask = useCallback(() => {
+    setIsKeyMasked(prev => !prev);
+  }, []);
+
   useEffect(
     () => () => {
       setError(undefined);
@@ -107,5 +136,15 @@ export function useSignIn() {
     [setError]
   );
 
-  return { onPaste, submitMnemonicForm, ref: textAreaRef, error, isLoading };
+  return {
+    onPaste,
+    submitMnemonicForm,
+    ref: textAreaRef,
+    error,
+    isLoading,
+    onChange,
+    toggleKeyMask,
+    isKeyMasked,
+    sanitizedSecretKey,
+  };
 }
