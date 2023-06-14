6d9d8929fccf391d4d1c1e330542437421af4451
fbwoolf <fwoolf@blockstack.com>
2023-06-14 10:03:12 -0500

    fix: dup placeholder node

--- src/app/features/psbt-signer/components/psbt-decoded-request/psbt-decoded-request-views/psbt-decoded-request-simple.tsx
+++ src/app/features/psbt-signer/components/psbt-decoded-request/psbt-decoded-request-views/psbt-decoded-request-simple.tsx
@@ -1,5 +1,6 @@
 import * as btc from '@scure/btc-signer';
 
+import { PsbtPlaceholderNode } from '../psbt-decoded-request-node/psbt-placeholder-node';
 import { PsbtUnsignedInputList } from '../psbt-unsigned-input-list/psbt-unsigned-input-list';
 import { PsbtUnsignedOutputList } from '../psbt-unsigned-output-list/psbt-unsigned-output-list';
 
@@ -17,19 +18,19 @@ export function PsbtDecodedRequestSimple({
   outputs,
   showPlaceholder,
 }: PsbtDecodedRequestSimpleProps) {
+  if (showPlaceholder) return <PsbtPlaceholderNode />;
+
   return (
     <>
       <PsbtUnsignedInputList
         addressNativeSegwit={bitcoinAddressNativeSegwit}
         addressTaproot={bitcoinAddressTaproot}
         inputs={inputs}
-        showPlaceholder={showPlaceholder}
       />
       <PsbtUnsignedOutputList
         addressNativeSegwit={bitcoinAddressNativeSegwit}
         addressTaproot={bitcoinAddressTaproot}
         outputs={outputs}
-        showPlaceholder={showPlaceholder}
       />
     </>
   );
--- src/app/features/psbt-signer/components/psbt-decoded-request/psbt-unsigned-output-list/psbt-unsigned-output-list.tsx
+++ src/app/features/psbt-signer/components/psbt-decoded-request/psbt-unsigned-output-list/psbt-unsigned-output-list.tsx
@@ -1,38 +1,31 @@
 import * as btc from '@scure/btc-signer';
 import { Box, Text } from '@stacks/ui';
 
-import { PsbtPlaceholderNode } from '../psbt-decoded-request-node/psbt-placeholder-node';
 import { PsbtUnsignedOutputItem } from './components/psbt-unsigned-output-item';
 
 interface PsbtUnsignedOutputListProps {
   addressNativeSegwit: string;
   addressTaproot: string;
   outputs: btc.TransactionOutputRequired[];
-  showPlaceholder: boolean;
 }
 export function PsbtUnsignedOutputList({
   addressNativeSegwit,
   addressTaproot,
   outputs,
-  showPlaceholder,
 }: PsbtUnsignedOutputListProps) {
   return (
     <Box background="white" borderBottomLeftRadius="16px" borderBottomRightRadius="16px" p="loose">
       <Text fontWeight={500}>Outputs</Text>
-      {showPlaceholder ? (
-        <PsbtPlaceholderNode />
-      ) : (
-        outputs.map((output, i) => {
-          return (
-            <PsbtUnsignedOutputItem
-              addressNativeSegwit={addressNativeSegwit}
-              addressTaproot={addressTaproot}
-              key={i}
-              output={output}
-            />
-          );
-        })
-      )}
+      {outputs.map((output, i) => {
+        return (
+          <PsbtUnsignedOutputItem
+            addressNativeSegwit={addressNativeSegwit}
+            addressTaproot={addressTaproot}
+            key={i}
+            output={output}
+          />
+        );
+      })}
     </Box>
   );
 }
