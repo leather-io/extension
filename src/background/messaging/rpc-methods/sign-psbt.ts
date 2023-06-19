950a28a955e49850a0a346336b3d890a52a94fc9
kyranjamie <kyran.burraston@gmail.com>
2023-06-19 14:31:02 +0200

    fix(psbt): parse param schema, return error, closes #3804

--- src/background/messaging/rpc-methods/sign-psbt.ts
+++ src/background/messaging/rpc-methods/sign-psbt.ts
@@ -3,7 +3,11 @@ import * as btc from '@scure/btc-signer';
 import { hexToBytes } from '@stacks/common';
 
 import { RouteUrls } from '@shared/route-urls';
-import { SignPsbtRequest } from '@shared/rpc/methods/sign-psbt';
+import {
+  SignPsbtRequest,
+  getRpcSignPsbtParamErrors,
+  validateRpcSignPsbtParams,
+} from '@shared/rpc/methods/sign-psbt';
 import { makeRpcErrorResponse } from '@shared/rpc/rpc-methods';
 import { ensureArray, isDefined } from '@shared/utils';
 
@@ -25,23 +29,22 @@ function validatePsbt(hex: string) {
 }
 
 export async function rpcSignPsbt(message: SignPsbtRequest, port: chrome.runtime.Port) {
-  if (!message.params || !message.params.hex) {
+  if (!validateRpcSignPsbtParams(message.params)) {
+    const errors = getRpcSignPsbtParamErrors(message.params);
     chrome.tabs.sendMessage(
       getTabIdFromPort(port),
       makeRpcErrorResponse('signPsbt', {
         id: message.id,
-        error: { code: RpcErrorCode.INVALID_PARAMS, message: 'Invalid parameters' },
+        error: {
+          code: RpcErrorCode.INVALID_PARAMS,
+          message:
+            'Invalid parameters: ' +
+            errors.map(e => `Error in path ${e.path}, ${e.message}.`).join(' '),
+        },
       })
     );
     return;
   }
-
-  const params: RequestParams = [
-    ['requestId', message.id],
-    ['hex', message.params.hex],
-    ['publicKey', message.params.publicKey],
-    ['network', message.params.network ?? 'mainnet'],
-  ];
 
   if (!validatePsbt(message.params.hex)) {
     chrome.tabs.sendMessage(
@@ -54,21 +57,28 @@ export async function rpcSignPsbt(message: SignPsbtRequest, port: chrome.runtime
     return;
   }
 
+  const requestParams: RequestParams = [
+    ['requestId', message.id],
+    ['hex', message.params.hex],
+    ['publicKey', message.params.publicKey],
+    ['network', message.params.network ?? 'mainnet'],
+  ];
+
   if (isDefined(message.params.account)) {
-    params.push(['accountIndex', message.params.account.toString()]);
+    requestParams.push(['accountIndex', message.params.account.toString()]);
   }
 
   if (isDefined(message.params.allowedSighash))
     ensureArray(message.params.allowedSighash).forEach(hash =>
-      params.push(['allowedSighash', hash.toString()])
+      requestParams.push(['allowedSighash', hash.toString()])
     );
 
   if (isDefined(message.params.signAtIndex))
     ensureArray(message.params.signAtIndex).forEach(index =>
-      params.push(['signAtIndex', index.toString()])
+      requestParams.push(['signAtIndex', index.toString()])
     );
 
-  const { urlParams, tabId } = makeSearchParamsWithDefaults(port, params);
+  const { urlParams, tabId } = makeSearchParamsWithDefaults(port, requestParams);
 
   const { id } = await triggerRequestWindowOpen(RouteUrls.RpcSignPsbt, urlParams);
   listenForPopupClose({
