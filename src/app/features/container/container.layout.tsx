0a27c0a0cbcb0e9c5c67ea117b875eeebc97aef2
kyranjamie <kyran.burraston@gmail.com>
2023-06-19 11:42:40 +0200

    chore: not serious suggestion

--- src/app/features/container/container.layout.tsx
+++ src/app/features/container/container.layout.tsx
@@ -1,4 +1,4 @@
-import { Flex, color } from '@stacks/ui';
+import { Flex } from '@stacks/ui';
 
 interface ContainerLayoutProps {
   children: JSX.Element | JSX.Element[];
@@ -8,7 +8,7 @@ export function ContainerLayout(props: ContainerLayoutProps) {
   const { children, header } = props;
 
   return (
-    <Flex flexDirection="column" flexGrow={1} width="100%" background={color('bg')}>
+    <Flex flexDirection="column" flexGrow={1} width="100%" position="relative" zIndex={1}>
       {header || null}
       <Flex className="main-content" flexGrow={1} position="relative" width="100%">
         {children}
--- test-app/src/components/global-styles.tsx
+++ test-app/src/components/global-styles.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react';
-import { CSSReset } from '@stacks/ui';
+
 import { Global, css } from '@emotion/react';
+import { CSSReset } from '@stacks/ui';
 
 const SizeStyles = css`
   body {
@@ -11,6 +12,17 @@ const SizeStyles = css`
     flex-grow: 1;
     display: flex;
     min-height: 100vh;
+  }
+  video {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    object-fit: cover;
+    z-index: 0;
   }
 `;
 
