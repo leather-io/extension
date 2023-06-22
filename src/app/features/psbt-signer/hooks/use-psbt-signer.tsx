848e0fcb4625514fdd8c5a68b81a7fb578e6e493
fbwoolf <fwoolf@blockstack.com>
2023-06-22 14:11:32 -0500

    refactor: psbt error handling, closes #3804

--- src/app/features/psbt-signer/hooks/use-psbt-signer.tsx
+++ src/app/features/psbt-signer/hooks/use-psbt-signer.tsx
@@ -9,9 +9,12 @@ import { isString } from '@shared/utils';
 import { useCurrentAccountNativeSegwitSigner } from '@app/store/accounts/blockchain/bitcoin/native-segwit-account.hooks';
 import { useCurrentAccountTaprootSigner } from '@app/store/accounts/blockchain/bitcoin/taproot-account.hooks';
 
+import { useSignPsbtError } from './use-sign-psbt-error';
+
 export type DecodedPsbt = ReturnType<typeof btc.RawPSBTV0.decode>;
 
 export function usePsbtSigner() {
+  const signPsbtError = useSignPsbtError();
   const createNativeSegwitSigner = useCurrentAccountNativeSegwitSigner();
   const createTaprootSigner = useCurrentAccountTaprootSigner();
 
@@ -27,7 +30,7 @@ export function usePsbtSigner() {
           try {
             taprootSigner?.signIndex(tx, idx, allowedSighash);
           } catch (e2) {
-            logger.error('Error signing tx at provided index', e1, e2);
+            signPsbtError(`Error signing PSBT at provided index, ${e1}, ${e2}`);
           }
         }
       },
@@ -38,7 +41,7 @@ export function usePsbtSigner() {
           try {
             taprootSigner?.sign(tx);
           } catch (e2) {
-            logger.error('Error signing PSBT', e1, e2);
+            signPsbtError(`Error signing PSBT, ${e1}, ${e2}`);
           }
         }
       },
@@ -51,16 +54,16 @@ export function usePsbtSigner() {
         try {
           return btc.RawPSBTV0.decode(bytes);
         } catch (e0) {
-          logger.info('Failed to decode as PSBT v0, trying V2', e0);
-        }
-        try {
-          return btc.RawPSBTV2.decode(bytes);
-        } catch (e2) {
-          logger.error('Error parsing psbt version', e2);
+          logger.error(`Failed to decode as PSBT v0, trying v2, ${e0}`);
+          try {
+            return btc.RawPSBTV2.decode(bytes);
+          } catch (e2) {
+            signPsbtError(`Failed to decode PSBT as v0 and v2, ${e0}, ${e2}`);
+          }
         }
         return;
       },
     }),
-    [nativeSegwitSigner, taprootSigner]
+    [nativeSegwitSigner, signPsbtError, taprootSigner]
   );
 }
