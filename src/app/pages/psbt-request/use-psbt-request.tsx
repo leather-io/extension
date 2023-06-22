e2dae7e2ccf09ca27a02e9d816bd1766343ac383
fbwoolf <fwoolf@blockstack.com>
2023-06-22 14:11:32 -0500

    refactor: psbt error handling, closes #3804

--- src/app/pages/psbt-request/use-psbt-request.tsx
+++ src/app/pages/psbt-request/use-psbt-request.tsx
@@ -3,13 +3,12 @@ import { useMemo, useState } from 'react';
 import { bytesToHex, hexToBytes } from '@noble/hashes/utils';
 
 import { finalizePsbt } from '@shared/actions/finalize-psbt';
-import { logger } from '@shared/logger';
 import { ensureArray, isUndefined } from '@shared/utils';
 
 import { useAnalytics } from '@app/common/hooks/analytics/use-analytics';
 import { getPsbtPayloadFromToken } from '@app/common/psbt/requests';
+import { usePsbtRequestSearchParams } from '@app/common/psbt/use-psbt-request-params';
 import { usePsbtSigner } from '@app/features/psbt-signer/hooks/use-psbt-signer';
-import { usePsbtRequestSearchParams } from '@app/pages/psbt-request/psbt-request.hooks';
 
 export function usePsbtRequest() {
   const { requestToken, tabId } = usePsbtRequestSearchParams();
@@ -18,6 +17,7 @@ export function usePsbtRequest() {
   const analytics = useAnalytics();
   return useMemo(() => {
     if (!requestToken) throw new Error('Cannot decode psbt without request token');
+
     const payload = getPsbtPayloadFromToken(requestToken);
     const payloadTxBytes = hexToBytes(payload.hex);
     const appName = payload?.appDetails?.name;
@@ -34,15 +34,13 @@ export function usePsbtRequest() {
       payloadTxBytes,
       onDenyPsbtSigning() {
         void analytics.track('request_psbt_cancel');
-        finalizePsbt({ requestPayload: requestToken ?? '', tabId, data: 'cancel' });
+        finalizePsbt({ requestPayload: requestToken, tabId, data: 'PSBT request was canceled' });
       },
       onSignPsbt() {
         setIsLoading(true);
         void analytics.track('request_sign_psbt_submit');
 
         const tx = getPsbtAsTransaction(payload.hex);
-
-        if (!tx) return logger.error('No psbt to sign');
 
         const indexOrIndexes = payload?.signAtIndex;
         const allowedSighash = payload?.allowedSighash;
