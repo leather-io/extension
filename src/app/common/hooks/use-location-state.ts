b9f772ba854e8d6226172a0d9592e30b7138e527
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-07-28 09:33:45 +0100

    fix: reinstate overloads and update types

--- src/app/common/hooks/use-location-state.ts
+++ src/app/common/hooks/use-location-state.ts
@@ -5,11 +5,18 @@ import get from 'lodash.get';
 
 import { isUndefined } from '@shared/utils';
 
+
+export function useLocationState(propName: string): string | undefined;
+export function useLocationState(propName: string, defaultValue: string): string;
+export function useLocationState(propName: 'accountIndex'): number;
+export function useLocationState(propName: 'backgroundLocation'): Location;
 export function useLocationState(propName: string, defaultValue?: string) {
   const location = useLocation();
   return get(location, `state.${propName}`, defaultValue);
 }
 
+export function useLocationStateWithCache<T = string>(propName: string): T | undefined;
+export function useLocationStateWithCache<T = string>(propName: string, defaultValue: T): T;
 export function useLocationStateWithCache<T = string>(propName: string, defaultValue?: T) {
   const location = useLocation();
   const [value, setValue] = useState(defaultValue);
