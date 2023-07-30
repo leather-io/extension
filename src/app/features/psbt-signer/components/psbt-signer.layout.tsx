4864ec27bd1a4584d04dca201aab21cf8dc4a7d4
Prasad Nilkanth Kulkarni <kulkarniprasad57@gmail.com>
2023-07-30 12:57:52 +0530

    fix : Use fixed overlay only when content needs to be fixed #3570

--- src/app/features/psbt-signer/components/psbt-signer.layout.tsx
+++ src/app/features/psbt-signer/components/psbt-signer.layout.tsx
@@ -8,7 +8,7 @@ export function PsbtSignerLayout({ children }: PsbtSignerLayoutProps) {
     <Stack
       alignItems="center"
       maxHeight="calc(100vh - 72px)"
-      overflowY="scroll"
+      overflowY="auto"
       pb="120px"
       px="loose"
       spacing="base-loose"
