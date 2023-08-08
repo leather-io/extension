5da155a328510571ddfd6e701c3a5319411cd8d0
alter-eggo <anastas3993@gmail.com>
2023-08-08 16:12:18 +0400

    feat: radix poc

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 
+import { Theme as RadixTheme } from '@radix-ui/themes';
 import { ColorModeProvider as ColorModeProviderLegacy, ThemeProvider } from '@stacks/ui';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -15,6 +16,8 @@ import { AppRoutes } from '@app/routes/app-routes';
 import { persistor, store } from '@app/store';
 
 import { ThemeSwitcherProvider } from './common/theme-provider';
+import './styles.css';
+import './themes-config.css';
 
 const reactQueryDevToolsEnabled = process.env.REACT_QUERY_DEVTOOLS_ENABLED === 'true';
 
@@ -26,17 +29,19 @@ export function App() {
       <PersistGate loading={<FullPageLoadingSpinner />} persistor={persistor}>
         <ThemeProvider theme={theme}>
           <ThemeSwitcherProvider>
-            <GlobalStyles />
-            <QueryClientProvider client={queryClient}>
-              <ColorModeProvider defaultMode="light">
-                <Suspense fallback={<NewAccountLoadingSpinner />}>
-                  <AppErrorBoundary>
-                    <AppRoutes />
-                  </AppErrorBoundary>
-                  {reactQueryDevToolsEnabled && <Devtools />}
-                </Suspense>
-              </ColorModeProvider>
-            </QueryClientProvider>
+            <RadixTheme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">
+              <GlobalStyles />
+              <QueryClientProvider client={queryClient}>
+                <ColorModeProvider defaultMode="light">
+                  <Suspense fallback={<NewAccountLoadingSpinner />}>
+                    <AppErrorBoundary>
+                      <AppRoutes />
+                    </AppErrorBoundary>
+                    {reactQueryDevToolsEnabled && <Devtools />}
+                  </Suspense>
+                </ColorModeProvider>
+              </QueryClientProvider>
+            </RadixTheme>
           </ThemeSwitcherProvider>
         </ThemeProvider>
       </PersistGate>
--- src/app/pages/home/components/tx-button.tsx
+++ src/app/pages/home/components/tx-button.tsx
@@ -1,19 +1,26 @@
 import { memo } from 'react';
 
-import { Box, ButtonProps, Text } from '@stacks/ui';
+import { Button as RadixButton, Text } from '@radix-ui/themes';
 
-interface HomeActionButtonProps extends ButtonProps {
+// import { ButtonProps } from '@stacks/ui';
+
+interface HomeActionButtonProps extends any {
   icon: any;
   label: string;
-  buttonComponent(props: ButtonProps): React.JSX.Element;
+  buttonComponent(props: any): React.JSX.Element;
 }
 export const HomeActionButton = memo((props: HomeActionButtonProps) => {
   const { icon, label, buttonComponent: Button, ...rest } = props;
 
   return (
-    <Button height="36px" px="base-tight" py="tight" position="relative" {...rest}>
-      <Box as={icon} mr="tight" size="14px" />
-      <Text fontSize="14px">{label}</Text>
-    </Button>
+    // <Button height="36px" px="base-tight" py="tight" position="relative" {...rest}>
+    //   <Box as={icon} mr="tight" size="14px" />
+    //   <Text fontSize="14px">{label}</Text>
+    // </Button>
+    <RadixButton color="brown" size="3">
+      {/* <IconButton>{icon}</IconButton> */}
+      {/* <Box as={icon} mr="tight" size="14px" /> */}
+      <Text color="red">{label}</Text>
+    </RadixButton>
   );
 });
