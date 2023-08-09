fe1e7b8ddcd4dcf9ecf7f6b5f66e3ce4df405b25
kyranjamie <kyran.burraston@gmail.com>
2023-08-09 10:51:43 +0200

    refactor: inscription sends, closes #4111

--- src/app/pages/send/ordinal-inscription/components/send-inscription-loader.tsx
+++ src/app/pages/send/ordinal-inscription/components/send-inscription-loader.tsx
@@ -1,25 +1,12 @@
-import { Navigate } from 'react-router-dom';
-
-import { RouteUrls } from '@shared/route-urls';
+import { AverageBitcoinFeeRates } from '@shared/models/fees/bitcoin-fees.model';
 
 import { useAverageBitcoinFeeRates } from '@app/query/bitcoin/fees/fee-estimates.hooks';
 
-import { useSendInscriptionRouteState } from '../hooks/use-send-inscription-route-state';
-import { createUtxoFromInscription } from './create-utxo-from-inscription';
-import { SendInscriptionContextState } from './send-inscription-container';
-
 interface SendInscriptionLoaderProps {
-  children(data: Partial<SendInscriptionContextState>): React.JSX.Element;
+  children(data: { feeRates: AverageBitcoinFeeRates }): React.JSX.Element;
 }
 export function SendInscriptionLoader({ children }: SendInscriptionLoaderProps) {
-  const { inscription } = useSendInscriptionRouteState();
   const { data: feeRates } = useAverageBitcoinFeeRates();
-
   if (!feeRates) return null;
-
-  if (!inscription) return <Navigate to={RouteUrls.Home} />;
-
-  const utxo = createUtxoFromInscription(inscription);
-
-  return children({ inscription, feeRates, utxo });
+  return children({ feeRates });
 }
