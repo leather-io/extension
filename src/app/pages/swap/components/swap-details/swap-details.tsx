0787f47d246be74887932474f0bc85d97e53a495
Kyle Fang <zhigang1992@gmail.com>
2023-08-23 17:27:09 +0800

    feat: add alex-sdk and alex integreation

--- src/app/pages/swap/components/swap-details/swap-details.tsx
+++ src/app/pages/swap/components/swap-details/swap-details.tsx
@@ -1,14 +1,35 @@
+import { useSwapContext } from '@app/pages/swap/swap.context';
+
 import { SwapDetailLayout } from './swap-detail.layout';
 import { SwapDetailsLayout } from './swap-details.layout';
+import {isDefined} from "@shared/utils";
 
-// TODO: Replace with live data
 export function SwapDetails() {
+  const { swapSubmissionData } = useSwapContext();
+  if (!isDefined(swapSubmissionData)) {
+    return null;
+  }
   return (
     <SwapDetailsLayout>
-      <SwapDetailLayout title="Placeholder" tooltipLabel="Tooltip info" value="0" />
-      <SwapDetailLayout title="Placeholder" value="0" />
-      <SwapDetailLayout title="Placeholder" value="0" />
-      <SwapDetailLayout title="Placeholder" value="0" />
+      <SwapDetailLayout
+        title="Route"
+        value={swapSubmissionData.router.map(x => x.name).join(' > ')}
+      />
+      <SwapDetailLayout
+        title="Liquidity Provider Fee"
+        tooltipLabel="To receive a share of these fees, become a Liquidity Provider through 'Pool'"
+        value={`${swapSubmissionData.liquidityFee} ${swapSubmissionData.swapAssetFrom.name}`}
+      />
+      <SwapDetailLayout
+        title="Slippage Tolerance"
+        value={`${swapSubmissionData.slippage * 100}%`}
+      />
+      <SwapDetailLayout
+        title="Minimum Received"
+        value={`${Number(swapSubmissionData.swapAmountTo) * (1 - swapSubmissionData.slippage)} ${
+          swapSubmissionData.swapAssetTo.name
+        }`}
+      />
     </SwapDetailsLayout>
   );
 }
