f14e3f1a017170e235e73848cca1658f1c64fd33
alter-eggo <anastas3993@gmail.com>
2023-09-12 17:08:31 +0400

    feat: create new test acc

--- tests/mocks/constants.ts
+++ tests/mocks/constants.ts
@@ -1,10 +1,10 @@
-export const TEST_ACCOUNT_1_NATIVE_SEGWIT_ADDRESS = 'bc1qhdykvr9eafepm9cf6aryk0stmmwpv4wws9raj5';
+export const TEST_ACCOUNT_1_NATIVE_SEGWIT_ADDRESS = 'bc1q530dz4h80kwlzywlhx2qn0k6vdtftd93c499yq';
 
 export const TEST_ACCOUNT_1_TAPROOT_ADDRESS =
-  'bc1plpvrq9f8mdh0ug4984kmdx4gv5nztypxktyemlc89uyqnlu3gwkq35e5pv';
+  'bc1putuzj9lyfcm8fef9jpy85nmh33cxuq9u6wyuk536t9kemdk37yjqmkc0pg';
 
 // export const TEST_TESTNET_ACCOUNT_1_BTC_ADDRESS = 'tb1q3c7zyg58dd9hy07m77dv8es9vnpk8xad0yaw8y'
-export const TEST_ACCOUNT_1_STX_ADDRESS = 'SP297VG59W96DPGBT13SGD542QE1XS954X78Z75G0';
+export const TEST_ACCOUNT_1_STX_ADDRESS = 'SPS8CKF63P16J28AYF7PXW9E5AACH0NZNTEFWSFE';
 // export const TEST_ACCOUNT_1_PUBKEY =
 //   '02b6b0afe5f620bc8e532b640b148dd9dea0ed19d11f8ab420fcce488fe3974893';
 export const TEST_ACCOUNT_2_STX_ADDRESS = 'SPRE7HABZGQ204G3VQAKMDMVBBD8A8CYG6BQKHQ';
@@ -14,6 +14,3 @@ export const TEST_TESTNET_ACCOUNT_2_BTC_ADDRESS = 'tb1qkzvk9hr7uvas23hspvsgqfvyc
 export const TEST_BNS_NAME = 'test-hiro-wallet.btc';
 export const TEST_BNS_RESOLVED_ADDRESS = 'SP12YQ0M2KFT7YMJKVGP71B874YF055F77PFPH9KM';
 export const TEST_PASSWORD = 'my_s3cret_p@ssw0r4';
-
-export const TEST_SECRET_KEY =
-  'derive plug aerobic cook until crucial school fine cushion panda ready crew photo typical nuclear ride steel indicate cupboard potato ignore bamboo script galaxy';
