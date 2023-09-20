4d105c7b43881ae503e4813c137ada8b1603bba1
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-09-20 16:59:29 +0100

    fix: test ci

--- src/app/pages/onboarding/allow-diagnostics/allow-diagnostics-layout.tsx
+++ src/app/pages/onboarding/allow-diagnostics/allow-diagnostics-layout.tsx
@@ -33,8 +33,8 @@ export function AllowDiagnosticsLayout(props: AllowDiagnosticsLayoutProps) {
       <Dialog.Content
         style={{
           width: '500px',
-          marginLeft: '12px',
-          marginRight: '12px',
+          marginLeft: token('spacing.space.03'),
+          marginRight: token('spacing.space.03'),
           backgroundColor: token('colors.accent.background-primary'),
         }}
       >
