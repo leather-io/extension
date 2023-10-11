1549eb9cc87c8b071efb88c8c98368d601bbee92
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-10-11 12:26:43 +0100

    fix: stop create account button disappearing at smaller heights, #4165

--- src/app/features/switch-account-drawer/components/create-account-action.tsx
+++ src/app/features/switch-account-drawer/components/create-account-action.tsx
@@ -7,7 +7,7 @@ interface CreateAccountActionProps {
 }
 export function CreateAccountAction({ onCreateAccount }: CreateAccountActionProps) {
   return (
-    <Flex py="space.05" px="space.05" flexGrow="1">
+    <Flex py="space.05" px="space.05" flexGrow="1" position="fixed" bottom={0} width="100%">
       <LeatherButton fullWidth onClick={() => onCreateAccount()}>
         Create new account
       </LeatherButton>
