230fe46abafafd71e3434779bdb7c4b591736c0b
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-10-13 12:41:57 +0100

    fix: name wrapper more specifically

--- src/app/features/activity-list/components/tab-wrapper.tsx
+++ src/app/features/activity-list/components/tab-wrapper.tsx
@@ -1,11 +1,14 @@
 import { Box } from 'leather-styles/jsx';
 
-interface TabWrapperProps {
+interface ActivityListTabWrapperProps {
   children: React.ReactNode;
   padContent?: boolean;
 }
 
-export function TabWrapper({ children, padContent = false }: TabWrapperProps) {
+export function ActivityListTabWrapper({
+  children,
+  padContent = false,
+}: ActivityListTabWrapperProps) {
   return (
     // Height set based on the height of the empty assets screen
     <Box minHeight="477px" py={padContent ? 'space.11' : undefined}>
