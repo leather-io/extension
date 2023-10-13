3bd16ce48e9fad47bfe4cb9acfaa0ba7490a8e35
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-10-13 10:00:17 +0100

    fix: re-introduce bg location for hometabs so modals overlay on top of activity

--- src/app/pages/home/home.tsx
+++ src/app/pages/home/home.tsx
@@ -1,14 +1,18 @@
-import { useNavigate } from 'react-router-dom';
-import { Outlet } from 'react-router-dom';
+import { Route, useNavigate } from 'react-router-dom';
+import { Outlet, Routes, useLocation } from 'react-router-dom';
 
 import { RouteUrls } from '@shared/route-urls';
 
 import { useTrackFirstDeposit } from '@app/common/hooks/analytics/transactions-analytics.hooks';
 import { useOnboardingState } from '@app/common/hooks/auth/use-onboarding-state';
+import { useLocationState } from '@app/common/hooks/use-location-state';
 import { useOnMount } from '@app/common/hooks/use-on-mount';
 import { useRouteHeader } from '@app/common/hooks/use-route-header';
 import { Header } from '@app/components/header';
+import { ActivityList } from '@app/features/activity-list/activity-list';
+import { AssetsList } from '@app/features/asset-list/asset-list';
 import { InAppMessages } from '@app/features/hiro-messages/in-app-messages';
+import { homePageModalRoutes } from '@app/routes/app-routes';
 
 import { CurrentAccount } from './components/account-area';
 import { HomeTabs } from './components/home-tabs';
@@ -17,6 +21,8 @@ import { HomeLayout } from './components/home.layout';
 export function Home() {
   const { decodedAuthRequest } = useOnboardingState();
 
+  const location = useLocation();
+  const backgroundLocation = useLocationState<Location>('backgroundLocation');
   const navigate = useNavigate();
 
   useTrackFirstDeposit();
@@ -35,7 +41,22 @@ export function Home() {
   return (
     <HomeLayout currentAccount={<CurrentAccount />}>
       <HomeTabs>
-        <Outlet />
+        <>
+          {/* 
+          To overlay modal on nested routes backgroundLocation is used 
+          to trick the router into thinking its on the same page
+          */}
+          <Routes location={backgroundLocation || location}>
+            <Route index element={<AssetsList />} />
+            <Route path={RouteUrls.Activity} element={<ActivityList />}>
+              {homePageModalRoutes}
+            </Route>
+
+            {homePageModalRoutes}
+            <Route path="*" element={<Outlet />} />
+          </Routes>
+          {backgroundLocation && <Outlet />}
+        </>
       </HomeTabs>
     </HomeLayout>
   );
