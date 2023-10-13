82a558458ec68b5abd648194a8e075777b3e5d07
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-10-13 12:49:12 +0100

    fix: pass child routes as children to appease dep cruiser

--- src/app/pages/fund/fund.tsx
+++ src/app/pages/fund/fund.tsx
@@ -1,20 +1,21 @@
-import { Route, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { RouteUrls } from '@shared/route-urls';
 
 import { useRouteHeader } from '@app/common/hooks/use-route-header';
 import { Header } from '@app/components/header';
 import { FullPageLoadingSpinner } from '@app/components/loading-spinner';
-import { ReceiveStxModal } from '@app/pages/receive/receive-stx';
 import { useCurrentStacksAccountAnchoredBalances } from '@app/query/stacks/balance/stx-balance.hooks';
 import { ModalBackgroundWrapper } from '@app/routes/components/modal-background-wrapper';
 import { useBackgroundLocationRedirect } from '@app/routes/hooks/use-background-location-redirect';
-import { settingsRoutes } from '@app/routes/settings-routes';
 import { useCurrentStacksAccount } from '@app/store/accounts/blockchain/stacks/stacks-account.hooks';
 
 import { FundLayout } from './fund.layout';
 
-export function FundPage() {
+interface FundPageProps {
+  children: React.ReactNode;
+}
+export function FundPage({ children }: FundPageProps) {
   useBackgroundLocationRedirect(RouteUrls.Fund);
   const navigate = useNavigate();
   const currentAccount = useCurrentStacksAccount();
@@ -27,10 +28,7 @@ export function FundPage() {
     <>
       <FundLayout address={currentAccount.address} />
       <>
-        <ModalBackgroundWrapper>
-          <Route path={RouteUrls.ReceiveStx} element={<ReceiveStxModal />} />
-          {settingsRoutes}
-        </ModalBackgroundWrapper>
+        <ModalBackgroundWrapper>{children}</ModalBackgroundWrapper>
       </>
     </>
   );
