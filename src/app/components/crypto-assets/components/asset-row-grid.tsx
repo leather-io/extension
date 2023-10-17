3903ba17f208cde8eeb1f13dd30c66eb7b2e3b64
alter-eggo <anastas3993@gmail.com>
2023-10-17 16:24:48 +0400

    feat: add connect btn

--- src/app/components/crypto-assets/components/asset-row-grid.tsx
+++ src/app/components/crypto-assets/components/asset-row-grid.tsx
@@ -5,14 +5,28 @@ interface AssetRowGridProps {
   balance: React.ReactNode;
   caption: React.ReactNode;
   usdBalance?: React.ReactNode;
+  connectBtn?: React.ReactNode;
 }
-export function AssetRowGrid({ title, balance, caption, usdBalance }: AssetRowGridProps) {
+export function AssetRowGrid({
+  title,
+  balance,
+  caption,
+  usdBalance,
+  connectBtn,
+}: AssetRowGridProps) {
+  const balanceItem = connectBtn ? (
+    <GridItem rowSpan={2} display="flex" justifyContent="end">
+      {connectBtn}
+    </GridItem>
+  ) : (
+    <GridItem textAlign="right">{balance}</GridItem>
+  );
   return (
     <Grid columns={2} gridTemplateColumns="2fr 1fr" gridTemplateRows={2} gap={0}>
       <GridItem textAlign="left" whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis">
         {title}
       </GridItem>
-      <GridItem textAlign="right">{balance}</GridItem>
+      {balanceItem}
       <GridItem textAlign="left">{caption}</GridItem>
       {usdBalance && <GridItem>{usdBalance}</GridItem>}
     </Grid>
