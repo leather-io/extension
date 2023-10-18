ddc91e36669a44d60ea886acbf661063b09ef066
alter-eggo <anastas3993@gmail.com>
2023-10-18 14:54:13 +0400

    fix: wallet type definition

--- src/app/components/account/account-name.tsx
+++ src/app/components/account/account-name.tsx
@@ -4,7 +4,6 @@ import { BoxProps } from '@stacks/ui';
 import { styled } from 'leather-styles/jsx';
 
 import { useAccountDisplayName } from '@app/common/hooks/account/use-account-names';
-import { BtcAccount } from '@app/store/accounts/blockchain/bitcoin/bitcoin-accounts.hooks';
 import { StacksAccount } from '@app/store/accounts/blockchain/stacks/stacks-account.models';
 
 interface AccountNameLayoutProps {
@@ -15,7 +14,7 @@ export const AccountNameLayout = memo(({ children }: AccountNameLayoutProps) => 
 ));
 
 interface AccountNameProps extends BoxProps {
-  account: StacksAccount | BtcAccount;
+  account: StacksAccount;
 }
 export const AccountName = memo(({ account }: AccountNameProps) => {
   const name = useAccountDisplayName(account);
--- src/app/features/switch-account-drawer/components/switch-account-list.tsx
+++ src/app/features/switch-account-drawer/components/switch-account-list.tsx
@@ -4,24 +4,16 @@ import { Virtuoso } from 'react-virtuoso';
 import { Box } from 'leather-styles/jsx';
 
 import { useWalletType } from '@app/common/use-wallet-type';
-import { BtcAccount } from '@app/store/accounts/blockchain/bitcoin/bitcoin-accounts.hooks';
-import { StacksAccount } from '@app/store/accounts/blockchain/stacks/stacks-account.models';
 
 import { SwitchAccountListItem } from './switch-account-list-item';
 
 interface SwitchAccountListProps {
   handleClose: () => void;
-  stacksAccounts: StacksAccount[];
-  bitcoinAccounts: BtcAccount[];
   currentAccountIndex: number;
+  addressesNum: number;
 }
 export const SwitchAccountList = memo(
-  ({
-    stacksAccounts,
-    currentAccountIndex,
-    handleClose,
-    bitcoinAccounts,
-  }: SwitchAccountListProps) => {
+  ({ currentAccountIndex, handleClose, addressesNum }: SwitchAccountListProps) => {
     const { whenWallet } = useWalletType();
 
     return (
@@ -29,12 +21,9 @@ export const SwitchAccountList = memo(
         initialTopMostItemIndex={whenWallet({ ledger: 0, software: currentAccountIndex })}
         height="72px"
         style={{ paddingTop: '24px', height: '70vh' }}
-        totalCount={stacksAccounts.length || bitcoinAccounts.length}
+        totalCount={addressesNum}
         itemContent={index => (
-          <Box
-            mx={['space.05', 'space.06']}
-            key={stacksAccounts[index]?.address ?? bitcoinAccounts[index]?.address ?? index}
-          >
+          <Box mx={['space.05', 'space.06']} key={index}>
             <SwitchAccountListItem
               handleClose={handleClose}
               currentAccountIndex={currentAccountIndex}
