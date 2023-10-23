733e503944a9b51c5f3f8798f59022c60c51c649
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-10-23 09:26:16 +0100

    chore: import prettier config from monorepo, closes #4268

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,5 @@
-const defaultConfig = require('@stacks/prettier-config');
+const defaultConfig = require('@leather-wallet/prettier-config');
 
 module.exports = {
   ...defaultConfig,
-  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
-  importOrder: [
-    '^react',
-    '<THIRD_PARTY_MODULES>',
-    '^@shared/(.*)$',
-    '^@(app|content-script|inpage|background)/(.*)$',
-    '^[./]',
-  ],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
 };
