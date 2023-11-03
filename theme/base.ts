e75507989c6379fa1f53ed48e7e331e39aa6b4eb
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-11-03 11:34:58 +0000

    chore: wip

---
+++ theme/base.ts
@@ -0,0 +1,13 @@
+import { definePreset } from '@pandacss/dev';
+
+export default definePreset({
+  theme: {
+    tokens: {
+      colors: {
+        darkModeBrown: {
+          2: { value: '#2C2A24' },
+        },
+      },
+    },
+  },
+});
