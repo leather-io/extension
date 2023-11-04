cb2ac94b1f5d4b0d138ec192b1e2e78c0117df98
kyranjamie <kyran.burraston@gmail.com>
2023-11-04 19:45:31 +0100

    fix(ledger): stacks contract call signing, closes #4478

--- src/app/pages/transaction-request/transaction-request.tsx
+++ src/app/pages/transaction-request/transaction-request.tsx
@@ -7,6 +7,7 @@ import get from 'lodash.get';
 import * as yup from 'yup';
 
 import { HIGH_FEE_WARNING_LEARN_MORE_URL_STX } from '@shared/constants';
+import { logger } from '@shared/logger';
 import { FeeTypes } from '@shared/models/fees/fees.model';
 import { StacksTransactionFormValues } from '@shared/models/form.model';
 import { RouteUrls } from '@shared/route-urls';
@@ -15,7 +16,6 @@ import { useAnalytics } from '@app/common/hooks/analytics/use-analytics';
 import { LoadingKeys, useLoading } from '@app/common/hooks/use-loading';
 import { useOnMount } from '@app/common/hooks/use-on-mount';
 import { useRouteHeader } from '@app/common/hooks/use-route-header';
-import { useWalletType } from '@app/common/use-wallet-type';
 import { stxFeeValidator } from '@app/common/validation/forms/fee-validators';
 import { nonceValidator } from '@app/common/validation/nonce-validators';
 import { EditNonceButton } from '@app/components/edit-nonce-button';
@@ -23,7 +23,6 @@ import { NonceSetter } from '@app/components/nonce-setter';
 import { PopupHeader } from '@app/features/current-account/popup-header';
 import { RequestingTabClosedWarningMessage } from '@app/features/errors/requesting-tab-closed-error-msg';
 import { HighFeeDrawer } from '@app/features/high-fee-drawer/high-fee-drawer';
-import { useLedgerNavigate } from '@app/features/ledger/hooks/use-ledger-navigate';
 import { ContractCallDetails } from '@app/features/stacks-transaction-request/contract-call-details/contract-call-details';
 import { ContractDeployDetails } from '@app/features/stacks-transaction-request/contract-deploy-details/contract-deploy-details';
 import { FeeForm } from '@app/features/stacks-transaction-request/fee-form';
@@ -40,41 +39,43 @@ import { useNextNonce } from '@app/query/stacks/nonce/account-nonces.hooks';
 import { useTransactionRequestState } from '@app/store/transactions/requests.hooks';
 import {
   useGenerateUnsignedStacksTransaction,
-  useSoftwareWalletTransactionRequestBroadcast,
+  useSignStacksTransaction,
+  useStacksTransactionBroadcast,
   useUnsignedStacksTransactionBaseState,
 } from '@app/store/transactions/transaction.hooks';
 
 function TransactionRequestBase() {
   const transactionRequest = useTransactionRequestState();
   const { setIsLoading, setIsIdle } = useLoading(LoadingKeys.SUBMIT_TRANSACTION_REQUEST);
-  const handleBroadcastTransaction = useSoftwareWalletTransactionRequestBroadcast();
+  // const handleBroadcastTransaction = useSoftwareWalletTransactionRequestBroadcast();
   const unsignedTx = useUnsignedStacksTransactionBaseState();
   const { data: stxFees } = useCalculateStacksTxFees(unsignedTx.transaction);
   const analytics = useAnalytics();
-  const { walletType } = useWalletType();
   const generateUnsignedTx = useGenerateUnsignedStacksTransaction();
   const { data: stacksBalances } = useCurrentStacksAccountAnchoredBalances();
-  const ledgerNavigate = useLedgerNavigate();
   const { data: nextNonce } = useNextNonce();
   const navigate = useNavigate();
+  const signStacksTransaction = useSignStacksTransaction();
+  const txBroadcast = useStacksTransactionBroadcast();
 
   useRouteHeader(<PopupHeader />);
 
-  useOnMount(() => {
-    void analytics.track('view_transaction_signing'), [analytics];
-  });
+  useOnMount(() => void analytics.track('view_transaction_signing'));
 
   const onSubmit = async (values: StacksTransactionFormValues) => {
-    if (walletType === 'ledger') {
-      const tx = await generateUnsignedTx(values);
-      if (!tx) return;
-      ledgerNavigate.toConnectAndSignTransactionStep(tx);
-      return;
-    }
+    const unsignedTx = await generateUnsignedTx(values);
+
+    if (!unsignedTx)
+      return logger.error('Failed to generate unsigned transaction in transaction-request');
+
+    const signedTx = await signStacksTransaction(unsignedTx);
+
+    if (!signedTx) return logger.error('Failed to sign transaction in transaction-request');
+
     setIsLoading();
 
     try {
-      await handleBroadcastTransaction(values);
+      await txBroadcast({ signedTx });
       setIsIdle();
     } catch (e) {
       navigate(RouteUrls.TransactionBroadcastError, { state: { message: get(e, 'message') } });
