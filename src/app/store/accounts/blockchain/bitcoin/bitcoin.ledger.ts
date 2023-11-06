6dc4c51feddebe3e198f59273cf88fd74376f999
alter-eggo <anastas3993@gmail.com>
2023-11-06 18:37:20 +0400

    feat: support btc testnet handling

--- src/app/store/accounts/blockchain/bitcoin/bitcoin.ledger.ts
+++ src/app/store/accounts/blockchain/bitcoin/bitcoin.ledger.ts
@@ -1,8 +1,41 @@
 import { useSelector } from 'react-redux';
 
+import { inferNetworkFromPath } from '@shared/crypto/bitcoin/bitcoin.utils';
+
 import { selectDefaultWalletBitcoinKeyEntities } from '@app/store/ledger/bitcoin/bitcoin-key.slice';
+import { useCurrentNetwork } from '@app/store/networks/networks.selectors';
 
 export function useHasBitcoinLedgerKeychain() {
+  const network = useCurrentNetwork();
+
   const bitcoinEntities = useSelector(selectDefaultWalletBitcoinKeyEntities);
-  return Object.keys(bitcoinEntities).length > 0;
+
+  const values = Object.values(bitcoinEntities);
+
+  if (values.length === 0) {
+    return false;
+  }
+
+  const hasNetworkKeys = values.some(v => {
+    const path = v?.path;
+    if (!path) {
+      return false;
+    }
+    return inferNetworkFromPath(path) === network.chain.bitcoin.network;
+  });
+
+  return hasNetworkKeys;
+}
+
+export function useFilteredBitcoinEntities() {
+  const bitcoinEntities = useSelector(selectDefaultWalletBitcoinKeyEntities);
+  const network = useCurrentNetwork();
+
+  return Object.values(bitcoinEntities).filter(v => {
+    const path = v?.path;
+    if (!path) {
+      return false;
+    }
+    return inferNetworkFromPath(path) === network.chain.bitcoin.network;
+  });
 }
