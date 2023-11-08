5ab4de18c545df42e3157d7e2c1988a32c087628
alter-eggo <anastas3993@gmail.com>
2023-11-08 17:18:20 +0400

    fix: btc account ledger selection

--- src/app/pages/send/send-crypto-asset-form/components/recipient-accounts-drawer/recipient-accounts-drawer.tsx
+++ src/app/pages/send/send-crypto-asset-form/components/recipient-accounts-drawer/recipient-accounts-drawer.tsx
@@ -2,43 +2,43 @@ import { memo, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Virtuoso } from 'react-virtuoso';
 
-import { Box } from '@stacks/ui';
+import { Box } from 'leather-styles/jsx';
 
-import { useWalletType } from '@app/common/use-wallet-type';
-import { BaseDrawer } from '@app/components/drawer/base-drawer';
+import { ControlledDrawer } from '@app/components/drawer/controlled-drawer';
+import { useFilteredBitcoinAccounts } from '@app/store/accounts/blockchain/bitcoin/bitcoin.ledger';
 import { useStacksAccounts } from '@app/store/accounts/blockchain/stacks/stacks-account.hooks';
 
 import { AccountListItem } from './account-list-item';
 
-const smallNumberOfAccountsToRenderWholeList = 10;
-
 export const RecipientAccountsDrawer = memo(() => {
-  const { whenWallet } = useWalletType();
-  const accounts = useStacksAccounts();
+  const stacksAccounts = useStacksAccounts();
   const navigate = useNavigate();
 
   const onGoBack = useCallback(() => navigate('..', { replace: true }), [navigate]);
+  const bitcoinAccounts = useFilteredBitcoinAccounts();
+  const btcAddressesNum = bitcoinAccounts.length / 2;
+  const stacksAddressesNum = stacksAccounts.length;
 
-  if (!accounts) return null;
+  if (stacksAddressesNum === 0 && btcAddressesNum === 0) return null;
 
   return (
-    <BaseDrawer title="My accounts" isShowing onClose={onGoBack}>
-      <Box mb="loose" mx={['base-loose', 'extra-loose']}>
-        {accounts.length <= smallNumberOfAccountsToRenderWholeList ? (
-          <Box marginBottom={8} mb={whenWallet({ ledger: 'base', software: '' })}>
-            {accounts.map(item => (
-              <AccountListItem account={item} key={item.address} onClose={onGoBack} />
-            ))}
-          </Box>
-        ) : (
-          <Virtuoso
-            height="72px"
-            itemContent={index => <AccountListItem account={accounts[index]} onClose={onGoBack} />}
-            style={{ paddingTop: '24px', height: '70vh' }}
-            totalCount={accounts.length}
-          />
-        )}
+    <ControlledDrawer title="My accounts" isShowing onClose={onGoBack}>
+      <Box>
+        <Virtuoso
+          height="72px"
+          itemContent={index => (
+            <Box mx={['space.05', 'space.06']} key={index}>
+              <AccountListItem
+                stacksAccount={stacksAccounts[index]}
+                onClose={onGoBack}
+                index={index}
+              />
+            </Box>
+          )}
+          style={{ paddingTop: '24px', height: '70vh' }}
+          totalCount={stacksAddressesNum || btcAddressesNum}
+        />
       </Box>
-    </BaseDrawer>
+    </ControlledDrawer>
   );
 });
