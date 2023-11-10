37e573beb75c38aec0be93dd447cc3caebdee039
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-11-10 09:31:29 +0000

    chore: simplify account loading code

--- src/app/pages/choose-account/components/accounts.tsx
+++ src/app/pages/choose-account/components/accounts.tsx
@@ -1,5 +1,4 @@
 import { Suspense, memo, useMemo, useState } from 'react';
-import { FiPlusCircle } from 'react-icons/fi';
 import { useNavigate } from 'react-router-dom';
 import { Virtuoso } from 'react-virtuoso';
 
@@ -10,7 +9,6 @@ import { RouteUrls } from '@shared/route-urls';
 import { useFinishAuthRequest } from '@app/common/authentication/use-finish-auth-request';
 import { useAccountDisplayName } from '@app/common/hooks/account/use-account-names';
 import { useCreateAccount } from '@app/common/hooks/account/use-create-account';
-import { useOnboardingState } from '@app/common/hooks/auth/use-onboarding-state';
 import { useWalletType } from '@app/common/use-wallet-type';
 import { slugify } from '@app/common/utils';
 import { AccountTotalBalance } from '@app/components/account-total-balance';
@@ -20,34 +18,14 @@ import { usePressable } from '@app/components/item-hover';
 import { useNativeSegwitAccountIndexAddressIndexZero } from '@app/store/accounts/blockchain/bitcoin/native-segwit-account.hooks';
 import { useStacksAccounts } from '@app/store/accounts/blockchain/stacks/stacks-account.hooks';
 import { StacksAccount } from '@app/store/accounts/blockchain/stacks/stacks-account.models';
-
-// #4476 TODO - test / refactor this - I'm not sure what it does
-// seems like it makes the account title a different colour when loading
-// I re-wrote this as it was way over complicated but not sure if its needed
-// - changes color based on whether its loading or not
-// const loadingProps = { color: '#A1A7B3' };
-// const getLoadingProps = (loading: boolean) => (loading ? loadingProps : {});
+import { PlusIcon } from '@app/ui/components/icons/plus-icon';
 
 interface AccountTitlePlaceholderProps {
   account: StacksAccount;
-  loading?: boolean;
 }
-function AccountTitlePlaceholder({ account, loading }: AccountTitlePlaceholderProps) {
+function AccountTitlePlaceholder({ account }: AccountTitlePlaceholderProps) {
   const name = `Account ${account?.index + 1}`;
-  return <styled.span color={loading ? 'red' : undefined}>{name}</styled.span>;
-}
-
-interface AccountTitleProps {
-  account: StacksAccount;
-  name: string;
-  loading?: boolean;
-}
-function AccountTitle({ name, loading }: AccountTitleProps) {
-  return (
-    <styled.span textStyle="label.01" color={loading ? 'red' : undefined}>
-      {name}
-    </styled.span>
-  );
+  return <styled.span>{name}</styled.span>;
 }
 
 interface ChooseAccountItemProps extends FlexProps {
@@ -57,13 +35,11 @@ interface ChooseAccountItemProps extends FlexProps {
   onSelectAccount(index: number): void;
 }
 const ChooseAccountItem = memo(
-  ({ selectedAddress, account, isLoading, onSelectAccount }: ChooseAccountItemProps) => {
+  ({ account, isLoading, onSelectAccount }: ChooseAccountItemProps) => {
     const [component, bind] = usePressable(true);
-    const { decodedAuthRequest } = useOnboardingState();
     const name = useAccountDisplayName(account);
     const btcAddress = useNativeSegwitAccountIndexAddressIndexZero(account.index);
 
-    const showLoadingProps = !!selectedAddress || !decodedAuthRequest;
     const accountSlug = useMemo(() => slugify(`Account ${account?.index + 1}`), [account?.index]);
 
     return (
@@ -74,10 +50,8 @@ const ChooseAccountItem = memo(
           index={account.index}
           isActive={false}
           accountName={
-            <Suspense
-              fallback={<AccountTitlePlaceholder account={account} loading={showLoadingProps} />}
-            >
-              <AccountTitle name={name} account={account} loading={showLoadingProps} />
+            <Suspense fallback={<AccountTitlePlaceholder account={account} />}>
+              <styled.span textStyle="label.01">{name}</styled.span>
             </Suspense>
           }
           avatar={
@@ -114,7 +88,7 @@ const AddAccountAction = memo(() => {
   return (
     <Box mb="loose" px="base-tight" py="tight" onClick={onCreateAccount} {...bind}>
       <HStack alignItems="center">
-        <FiPlusCircle size="16px" />
+        <PlusIcon size="16px" />
         {/* 
         #4476 TODO - test / refactor this - I'm not sure where it is or what it does 
         We have a button instead for `Create new account'
