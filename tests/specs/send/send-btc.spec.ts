58c4027c24b24d1ce2ef8701518608a3a288ad68
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-11-15 14:08:00 +0000

    chore: force click hit and hope

--- tests/specs/send/send-btc.spec.ts
+++ tests/specs/send/send-btc.spec.ts
@@ -12,7 +12,7 @@ test.describe('send btc', () => {
     await globalPage.setupAndUseApiCalls(extensionId);
     await onboardingPage.signInWithTestAccount(extensionId);
     await homePage.enableTestMode();
-    await homePage.sendButton.click();
+    await homePage.sendButton.click({ force: true });
     await sendPage.selectBtcAndGoToSendForm();
     await sendPage.waitForSendPageReady();
   });
@@ -22,8 +22,8 @@ test.describe('send btc', () => {
       await sendPage.amountInput.fill('0.00006');
       await sendPage.recipientInput.fill(TEST_TESTNET_ACCOUNT_2_BTC_ADDRESS);
 
-      await sendPage.previewSendTxButton.click();
-      await sendPage.feesListItem.filter({ hasText: BtcFeeType.High }).click();
+      await sendPage.previewSendTxButton.click({ force: true });
+      await sendPage.feesListItem.filter({ hasText: BtcFeeType.High }).click({ force: true });
 
       const details = await sendPage.confirmationDetails.allInnerTexts();
       test.expect(details).toBeTruthy();
@@ -34,7 +34,7 @@ test.describe('send btc', () => {
       await sendPage.recipientInput.blur();
       await sendPage.page.waitForTimeout(1000);
       await sendPage.previewSendTxButton.click();
-      await sendPage.feesListItem.filter({ hasText: BtcFeeType.High }).click();
+      await sendPage.feesListItem.filter({ hasText: BtcFeeType.High }).click({ force: true });
 
       const displayerAddress = await getDisplayerAddress(sendPage.confirmationDetailsRecipient);
       test.expect(displayerAddress).toEqual(TEST_TESTNET_ACCOUNT_2_BTC_ADDRESS);
@@ -49,8 +49,8 @@ test.describe('send btc', () => {
       await sendPage.recipientInput.blur();
       await sendPage.page.waitForTimeout(1000);
 
-      await sendPage.previewSendTxButton.click();
-      await sendPage.feesListItem.filter({ hasText: BtcFeeType.High }).click();
+      await sendPage.previewSendTxButton.click({ force: true });
+      await sendPage.feesListItem.filter({ hasText: BtcFeeType.High }).click({ force: true });
 
       const displayerAddress = await getDisplayerAddress(sendPage.confirmationDetailsRecipient);
       test.expect(displayerAddress).toEqual(TEST_TESTNET_ACCOUNT_2_BTC_ADDRESS);
@@ -65,7 +65,7 @@ test.describe('send btc', () => {
       await sendPage.amountInput.fill('0.00006');
       await sendPage.recipientInput.fill(TEST_TESTNET_ACCOUNT_2_BTC_ADDRESS);
 
-      await sendPage.previewSendTxButton.click();
+      await sendPage.previewSendTxButton.click({ force: true });
 
       const feeType = BtcFeeType.Standard;
       const fee = await sendPage.feesListItem
@@ -73,7 +73,7 @@ test.describe('send btc', () => {
         .getByTestId(SharedComponentsSelectors.FeesListItemFeeValue)
         .innerText();
 
-      await sendPage.feesListItem.filter({ hasText: feeType }).click();
+      await sendPage.feesListItem.filter({ hasText: feeType }).click({ force: true });
 
       const confirmationFee = await sendPage.confirmationDetails
         .getByTestId(SendCryptoAssetSelectors.ConfirmationDetailsFee)
@@ -84,3 +84,27 @@ test.describe('send btc', () => {
     });
   });
 });
+
+//https://github.com/microsoft/playwright/issues/12298
+    //   <div class="leather-d_flex leather-flex_column leathe…>…</div> from <div class="leather-d_flex leather-flex_column leathe…>…</div> subtree intercepts pointer events
+    // retrying click action, attempt #130
+    //   waiting 500ms
+    //   waiting for element to be visible, enabled and stable
+    //   element is visible, enabled and stable
+    //   scrolling into view if needed
+    //   done scrolling
+    //   <div class="leather-d_flex leather-flex_column leathe…>…</div> from <div class="leather-d_flex leather-flex_column leathe…>…</div> subtree intercepts pointer events
+    // retrying click action, attempt #131
+    //   waiting 500ms
+    //   waiting for element to be visible, enabled and stable
+    //   element is visible, enabled and stable
+    //   scrolling into view if needed
+    //   done scrolling
+    //   <div class="leather-d_flex leather-flex_column leathe…>…</div> from <div class="leather-d_flex leather-flex_column leathe…>…</div> subtree intercepts pointer events
+    // retrying click action, attempt #132
+    //   waiting 500ms
+    //   waiting for element to be visible, enabled and stable
+    //   element is visible, enabled and stable
+    //   scrolling into view if needed
+    //   done scrolling
+    //   <div class="leather-d_flex leather-flex_column leathe…>…
