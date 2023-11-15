2050ce97db8c1889d15d6395012ddc50505957ed
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-11-15 13:56:21 +0000

    chore: test dataTestId to fix tests

--- src/app/ui/components/typography/caption.tsx
+++ src/app/ui/components/typography/caption.tsx
@@ -2,12 +2,17 @@ import { forwardRef } from 'react';
 
 import { BoxProps, styled } from 'leather-styles/jsx';
 
-export const Caption = forwardRef<HTMLSpanElement, BoxProps>(({ children, ...props }, ref) => (
+interface CaptionProps extends BoxProps {
+  dataTestId?: string;
+}
+
+export const Caption = forwardRef<HTMLSpanElement, CaptionProps>(({ children, ...props }, ref) => (
   <styled.span
     color="accent.text-subdued"
     display="block"
     ref={ref}
     textStyle="caption.02"
+    data-testid={props.dataTestId}
     {...props}
   >
     {children}
