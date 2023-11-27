ee988fc06313b8fe92062b75b04242ecef8b24a4
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2023-11-27 20:30:19 +0400

    fix(popup): avatar circle on stacks transactions

--- src/app/features/current-account/current-account-avatar.tsx
+++ src/app/features/current-account/current-account-avatar.tsx
@@ -5,10 +5,14 @@ import { CircleProps } from 'leather-styles/jsx';
 import { useCurrentAccountDisplayName } from '@app/common/hooks/account/use-account-names';
 import { useDrawers } from '@app/common/hooks/use-drawers';
 import { AccountAvatar } from '@app/components/account/account-avatar';
-import { useCurrentStacksAccount } from '@app/store/accounts/blockchain/stacks/stacks-account.hooks';
+import { useCurrentAccountIndex } from '@app/store/accounts/account';
+import { useStacksAccounts } from '@app/store/accounts/blockchain/stacks/stacks-account.hooks';
+import { StacksAccount } from '@app/store/accounts/blockchain/stacks/stacks-account.models';
 
 export const CurrentAccountAvatar = memo((props: CircleProps) => {
-  const currentAccount = useCurrentStacksAccount();
+  const accountIndex = useCurrentAccountIndex();
+  const accounts = useStacksAccounts();
+  const currentAccount = accounts[accountIndex] as StacksAccount | undefined;
   const name = useCurrentAccountDisplayName();
   const { setIsShowingSwitchAccountsState } = useDrawers();
   if (!currentAccount) return null;
