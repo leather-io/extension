d476fa31cd7061613b43d157c6b724edda9efd74
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-11-29 16:26:29 +0000

    chore: consume design tokens from extension

--- theme/breakpoints.ts
+++ theme/breakpoints.ts
@@ -1,6 +1,5 @@
 import { breakpoints as leatherBreakpoints } from '@leather-wallet/tokens';
 
-// TODO consider removing this from the tokens package / doing this better
 // ts-unused-exports:disable-next-line
 export const breakpoints = {
   ...leatherBreakpoints,
