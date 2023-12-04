480e696d252e8c422a6dfe03efb2ba11e2465bfe
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-12-04 06:26:40 +0000

    fix: remove PSBT learn more link, closes #4607

--- src/app/features/psbt-signer/components/psbt-request-sighash-warning-label.tsx
+++ src/app/features/psbt-signer/components/psbt-request-sighash-warning-label.tsx
@@ -1,5 +1,4 @@
 import { WarningLabel } from '@app/components/warning-label';
-import { LeatherButton } from '@app/ui/components/button';
 
 export function PsbtRequestSighashWarningLabel() {
   return (
@@ -7,10 +6,6 @@ export function PsbtRequestSighashWarningLabel() {
       The details you see here are not guaranteed. Be sure to fully trust your counterparty, who can
       later modify this transaction to send or receive other assets from your account, and possibly
       even drain it.
-      {/* TODO: Link for this? */}
-      <LeatherButton display="inline" ml="space.01" textStyle="caption.02" variant="link">
-        Learn more ↗
-      </LeatherButton>
     </WarningLabel>
   );
 }
