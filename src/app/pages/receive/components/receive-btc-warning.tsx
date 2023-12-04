cdcc59787fc74ebd98449a2314d2dfb72ef5f518
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-12-04 11:15:43 +0000

    fix: add color to warning background, closes #4600

--- src/app/pages/receive/components/receive-btc-warning.tsx
+++ src/app/pages/receive/components/receive-btc-warning.tsx
@@ -2,7 +2,13 @@ import { Flex, styled } from 'leather-styles/jsx';
 
 export function ReceiveBtcModalWarning({ message }: { message: string }) {
   return (
-    <Flex justifyContent="center" bg="yellow.100" minHeight="48px" p="space.05" width="100%">
+    <Flex
+      justifyContent="center"
+      bg="warning.background"
+      minHeight="48px"
+      p="space.05"
+      width="100%"
+    >
       <styled.span textStyle="label.02">{message}</styled.span>
     </Flex>
   );
