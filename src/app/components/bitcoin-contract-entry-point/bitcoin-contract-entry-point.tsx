bc909cfa3102b18459984b83b6b62871402f43c3
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2023-12-12 16:59:21 +0100

    feat: modified bitcoin contracts balance fetching

--- src/app/components/bitcoin-contract-entry-point/bitcoin-contract-entry-point.tsx
+++ src/app/components/bitcoin-contract-entry-point/bitcoin-contract-entry-point.tsx
@@ -2,31 +2,29 @@ import { useNavigate } from 'react-router-dom';
 
 import { RouteUrls } from '@shared/route-urls';
 
-import { useBitcoinContractsBalanceQuery } from '@app/query/stacks/balance/bitcoin-contracts-balance.query';
+import { useGetBitcoinContractsBalance } from '@app/query/bitcoin/balance/bitcoin-contracts-balance.hooks';
 import { BitcoinContractIcon } from '@app/ui/components/icons/bitcoin-contract-icon';
 
-import { useCryptoCurrencyMarketData } from '@app/query/common/market-data/market-data.hooks';
-import { createMoney } from '@shared/models/money.model';
 import { BitcoinContractEntryPointLayout } from './bitcoin-contract-entry-point-layout';
 
 export function BitcoinContractEntryPoint() {
   const navigate = useNavigate();
 
-  const { data: bitcoinContactsBalance, isLoading, refetch } = useBitcoinContractsBalanceQuery();
-
+  const { bitcoinContractsBalance, bitcoinContractsBalanceInUSD, isLoading } =
+    useGetBitcoinContractsBalance();
 
   function onClick() {
     navigate(RouteUrls.BitcoinContractList);
   }
 
   return (
-      <BitcoinContractEntryPointLayout
-        isLoading={isLoading}
-        balance={bitcoinContactsBalance ? bitcoinContactsBalance : createMoney(0, 'BTC')}
-        caption={bitcoinContactsBalance ? bitcoinContactsBalance.symbol : 'BTC'}
-        icon={<BitcoinContractIcon />}
-        // usdBalance={btcUsdBalance}
-        onClick={onClick}
-      />
-    )
+    <BitcoinContractEntryPointLayout
+      isLoading={isLoading}
+      balance={bitcoinContractsBalance}
+      caption={bitcoinContractsBalance.symbol}
+      icon={<BitcoinContractIcon />}
+      usdBalance={bitcoinContractsBalanceInUSD}
+      onClick={onClick}
+    />
+  );
 }
--- src/app/query/bitcoin/balance/bitcoin-contracts-balance.query.ts
+++ src/app/query/bitcoin/balance/bitcoin-contracts-balance.query.ts
@@ -3,6 +3,7 @@ import { UseQueryResult, useQuery } from '@tanstack/react-query';
 import { Money } from '@shared/models/money.model';
 
 import { useBitcoinContracts } from '@app/common/hooks/use-bitcoin-contracts';
+import { useCurrentAccountIndex } from '@app/store/accounts/account';
 import { useCurrentNetwork } from '@app/store/networks/networks.selectors';
 
 const staleTime = 1 * 60 * 1000;
@@ -15,12 +16,12 @@ const balanceQueryOptions = {
 
 export function useBitcoinContractsBalanceQuery(): UseQueryResult<Money> {
   const { sumBitcoinContractCollateralAmounts } = useBitcoinContracts();
+  const currentAccountIndex = useCurrentAccountIndex();
   const currentNetwork = useCurrentNetwork();
 
   return useQuery({
-    queryKey: ['sum-bitcoin-contract-collateral-amounts', currentNetwork.id],
+    queryKey: ['sum-bitcoin-contract-collateral-amounts', currentAccountIndex, currentNetwork?.id],
     queryFn: sumBitcoinContractCollateralAmounts,
-    enabled: !!currentNetwork,
     ...balanceQueryOptions,
   });
 }
