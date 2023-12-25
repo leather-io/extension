fc23e9f6bb20b7b31832d94cd580f6427b3f032a
alter-eggo <anastas3993@gmail.com>
2023-12-25 13:44:10 +0400

    fix: nan total amount bug

--- src/app/common/math/calculate-averages.spec.ts
+++ src/app/common/math/calculate-averages.spec.ts
@@ -3,6 +3,9 @@ import BigNumber from 'bignumber.js';
 import { calculateMeanAverage } from './calculate-averages';
 
 describe(calculateMeanAverage.name, () => {
+  test('it calculates average of 0', () =>
+    expect(calculateMeanAverage([]).toString()).toEqual('0'));
+
   test('it calculates averages of 1', () =>
     expect(calculateMeanAverage([new BigNumber(1)]).toString()).toEqual('1'));
 
