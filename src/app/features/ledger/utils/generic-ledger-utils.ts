4ea53e201dc661e833e2b7d152dbef3db1e94725
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2024-01-08 17:10:31 +0400

    feat: prompt user to open the app

--- src/app/features/ledger/utils/generic-ledger-utils.ts
+++ src/app/features/ledger/utils/generic-ledger-utils.ts
@@ -1,10 +1,19 @@
 import { useState } from 'react';
 
+import Transport from '@ledgerhq/hw-transport';
+import TransportWebUSB from '@ledgerhq/hw-transport-webusb';
+
 import { delay } from '@app/common/utils';
 import { safeAwait } from '@app/common/utils/safe-await';
 
 import { LedgerTxSigningContext } from '../generic-flows/tx-signing/ledger-sign-tx.context';
 import { getStacksAppVersion } from './stacks-ledger-utils';
+
+export const LEDGER_APPS_MAP = {
+  STACKS: 'Stacks',
+  BITCOIN_MAINNET: 'Bitcoin',
+  BITCOIN_TESTNET: 'Bitcoin Test',
+} as const;
 
 export interface BaseLedgerOperationContext {
   latestDeviceResponse: null | Awaited<ReturnType<typeof getStacksAppVersion>>;
@@ -47,4 +56,41 @@ export function prepareLedgerDeviceForAppFn<T extends () => Promise<unknown>>(co
 
     return app;
   };
+}
+
+async function getAppAndVersion(transport: Transport): Promise<{
+  name: string;
+  version: string;
+  flags: number | Buffer;
+}> {
+  const r = await transport.send(0xb0, 0x01, 0x00, 0x00);
+  let i = 0;
+  const format = r[i++];
+
+  if (format !== 1) {
+    throw new Error('getAppAndVersion: format not supported');
+  }
+
+  const nameLength = r[i++];
+  const name = r.subarray(i, (i += nameLength)).toString('ascii');
+  const versionLength = r[i++];
+  const version = r.subarray(i, (i += versionLength)).toString('ascii');
+  const flagLength = r[i++];
+  const flags = r.subarray(i, (i += flagLength));
+  return {
+    name,
+    version,
+    flags,
+  };
+}
+
+export async function promptOpenAppOnDevice(appName: string) {
+  const tmpTransport = await TransportWebUSB.create();
+  const r = await getAppAndVersion(tmpTransport);
+  if (r.name !== appName) {
+    await tmpTransport.send(0xe0, 0xd8, 0x00, 0x00, Buffer.from(appName, 'ascii'));
+  }
+  // for some reason sending open app buffer to ledger will close the connection afterwards.
+  // we need to add a delay for this transport to properly finish for another one to open.
+  await delay(500);
 }
