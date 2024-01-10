77dc018286402c3f2ff7de369d92840d91bba602
alter-eggo <anastas3993@gmail.com>
2024-01-10 16:11:56 +0400

    fix: duplicate keys error

--- src/app/components/crypto-assets/choose-crypto-asset/crypto-asset-list.tsx
+++ src/app/components/crypto-assets/choose-crypto-asset/crypto-asset-list.tsx
@@ -1,4 +1,5 @@
 import type { AllTransferableCryptoAssetBalances } from '@shared/models/crypto-asset-balance.model';
+import { StacksFungibleTokenAsset } from '@shared/models/crypto-asset.model';
 
 import { CryptoAssetListItem } from './crypto-asset-list-item';
 import { CryptoAssetListLayout } from './crypto-asset-list.layout';
@@ -14,7 +15,10 @@ export function CryptoAssetList({ cryptoAssetBalances, onItemClick }: CryptoAsse
         <CryptoAssetListItem
           onClick={() => onItemClick(cryptoAssetBalance)}
           assetBalance={cryptoAssetBalance}
-          key={cryptoAssetBalance.asset.name}
+          key={
+            cryptoAssetBalance.asset.name ??
+            (cryptoAssetBalance.asset as StacksFungibleTokenAsset).contractAssetName
+          }
         />
       ))}
     </CryptoAssetListLayout>
