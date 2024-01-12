10e66d690391db79531655baac668ec3d460466e
Fara Woolf <fara@trustmachines.co>
2024-01-11 18:25:56 -0600

    refactor: select and dropdown, ref #4798

--- src/app/ui/components/dowpdown-menu/dropdown-menu.stories.tsx
+++ src/app/ui/components/dowpdown-menu/dropdown-menu.stories.tsx
@@ -1,38 +1,53 @@
-import * as RadixDropdownMenu from '@radix-ui/react-dropdown-menu';
 import type { Meta, StoryObj } from '@storybook/react';
+import { HStack, styled } from 'leather-styles/jsx';
 
-import { ListItemType } from '../list/list-item';
-import { DropdownMenu as Component } from './dropdown-menu';
-import { DropdownMenuItem } from './dropdown-menu-item';
-import { DropdownMenuSectionLabel } from './dropdown-menu-section-label';
+import { ChevronDownIcon } from '../icons/chevron-down-icon';
+import { PlaceholderIcon } from '../icons/placeholder-icon';
+import { DropdownMenu as Component, DropdownMenuItem } from './dropdown-menu';
+import { DropdownMenuItemLayout } from './dropdown-menu-item.layout';
 
-const items: ListItemType[] = [{ label: 'Label 1' }, { label: 'Label 2' }];
+const items: DropdownMenuItem[] = [{ label: 'Label 1' }, { label: 'Label 2' }];
 
-const meta: Meta<typeof Component> = {
-  component: Component,
+const meta: Meta<typeof Component.Root> = {
+  component: Component.Root,
   tags: ['autodocs'],
   title: 'Dropdown Menu',
 };
 
 export default meta;
-type Story = StoryObj<typeof Component>;
+type Story = StoryObj<typeof Component.Root>;
 
 export const DropdownMenu: Story = {
   render: () => (
-    <Component>
-      <RadixDropdownMenu.Group>
-        <DropdownMenuSectionLabel label="Items" />
-        {items.map(item => {
-          return (
-            <DropdownMenuItem
-              key={item.label}
-              iconLeft={item.iconLeft}
-              iconRight={item.iconRight}
-              label={item.label}
-            />
-          );
-        })}
-      </RadixDropdownMenu.Group>
-    </Component>
+    <Component.Root>
+      <Component.Trigger>
+        <styled.button>
+          <HStack gap="space.02" width="100%">
+            <styled.span textStyle="label.02">Options</styled.span>
+            <ChevronDownIcon />
+          </HStack>
+        </styled.button>
+      </Component.Trigger>
+      <Component.Portal>
+        <Component.Content align="start" sideOffset={8}>
+          <Component.Group>
+            <Component.Label>Label</Component.Label>
+            {items.map(item => (
+              <Component.Item key={item.label}>
+                <DropdownMenuItemLayout
+                  contentLeft={
+                    <HStack gap="space.02" minHeight="24px">
+                      <PlaceholderIcon />
+                      <styled.span textStyle="label.02">{item.label}</styled.span>
+                    </HStack>
+                  }
+                  contentRight={<PlaceholderIcon />}
+                />
+              </Component.Item>
+            ))}
+          </Component.Group>
+        </Component.Content>
+      </Component.Portal>
+    </Component.Root>
   ),
 };
