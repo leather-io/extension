a82457d8f04e94b696fe4b54edbd8981f311c353
Fara Woolf <fara@trustmachines.co>
2024-01-11 18:25:56 -0600

    refactor: select and dropdown, ref #4798

--- src/app/ui/components/dowpdown-menu/dropdown-menu.tsx
+++ src/app/ui/components/dowpdown-menu/dropdown-menu.tsx
@@ -1,14 +1,105 @@
-import { ReactNode } from 'react';
+import { ReactNode, forwardRef } from 'react';
 
-import { DropdownMenuTrigger } from './dropdown-menu-trigger';
-import { DropdownMenuLayout } from './dropdown-menu.layout';
+import * as RadixDropdownMenu from '@radix-ui/react-dropdown-menu';
+import { css } from 'leather-styles/css';
 
-interface DropdownMenuProps {
-  children: ReactNode;
-  trigger?: ReactNode;
+export interface DropdownMenuItem {
+  iconLeft?: ReactNode;
+  iconRight?: ReactNode;
+  label: string;
 }
-export function DropdownMenu({ children, trigger }: DropdownMenuProps) {
-  return (
-    <DropdownMenuLayout trigger={trigger ?? <DropdownMenuTrigger />}>{children}</DropdownMenuLayout>
-  );
-}
+
+const Root = RadixDropdownMenu.Root;
+
+const dropdownTriggerStyles = css({
+  bg: 'accent.background-primary',
+  borderRadius: '2px',
+  fontWeight: 500,
+  maxWidth: 'fit-content',
+  maxHeight: 'fit-content',
+  px: 'space.04',
+  py: 'space.03',
+  textStyle: 'label.02',
+
+  '&[data-state=open]': {
+    bg: 'accent.component-background-pressed',
+  },
+});
+const Trigger: typeof RadixDropdownMenu.Trigger = forwardRef((props, ref) => (
+  <RadixDropdownMenu.Trigger className={dropdownTriggerStyles} ref={ref} {...props} />
+));
+
+const Portal = RadixDropdownMenu.Portal;
+
+const dropdownContentStyles = css({
+  alignItems: 'center',
+  animationDuration: '400ms',
+  animationTimingFunction: 'cubic-bezier(0.16, 1, 0.3, 1)',
+  '--base-menu-padding': '0px',
+  bg: 'accent.background-primary',
+  borderRadius: '2px',
+  boxShadow:
+    '0px 12px 24px 0px rgba(18, 16, 15, 0.08), 0px 4px 8px 0px rgba(18, 16, 15, 0.08), 0px 0px 2px 0px rgba(18, 16, 15, 0.08)',
+  minWidth: '256px',
+  p: 'space.02',
+  willChange: 'transform, opacity',
+  zIndex: 999,
+
+  '&[data-side=bottom]': {
+    animationName: 'slideUpAndFade',
+  },
+});
+const Content: typeof RadixDropdownMenu.Content = forwardRef((props, ref) => (
+  <RadixDropdownMenu.Content className={dropdownContentStyles} ref={ref} {...props} />
+));
+
+const Group = RadixDropdownMenu.Group;
+
+const dropdownMenuLabelStyles = css({
+  color: 'accent.text-subdued',
+  height: 'auto',
+  px: 'space.03',
+  py: 'space.02',
+  textStyle: 'body.02',
+  width: '100%',
+});
+const Label: typeof RadixDropdownMenu.Label = forwardRef((props, ref) => (
+  <RadixDropdownMenu.Label className={dropdownMenuLabelStyles} ref={ref} {...props} />
+));
+
+const dropdownMenuItemStyles = css({
+  bg: 'accent.background-primary',
+  color: 'accent.text-primary',
+  height: 'auto',
+  outline: 'none',
+  userSelect: 'none',
+  p: 'space.03',
+
+  '&[data-highlighted]': {
+    bg: 'accent.component-background-hover',
+  },
+});
+const Item: typeof RadixDropdownMenu.Item = forwardRef((props, ref) => (
+  <RadixDropdownMenu.Item className={dropdownMenuItemStyles} ref={ref} {...props} />
+));
+
+const dropdownMenuSeparatorStyles = css({
+  bg: 'accent.background-primary',
+  color: 'accent.border-default',
+  mx: '0px',
+  my: 'space.03',
+});
+const Separator: typeof RadixDropdownMenu.Separator = forwardRef((props, ref) => (
+  <RadixDropdownMenu.Separator className={dropdownMenuSeparatorStyles} ref={ref} {...props} />
+));
+
+export const DropdownMenu = {
+  Root,
+  Trigger,
+  Portal,
+  Content,
+  Group,
+  Label,
+  Item,
+  Separator,
+};
--- src/app/ui/components/select/select.stories.tsx
+++ src/app/ui/components/select/select.stories.tsx
@@ -1,38 +1,59 @@
-import * as RadixSelect from '@radix-ui/react-select';
 import type { Meta, StoryObj } from '@storybook/react';
+import { HStack, styled } from 'leather-styles/jsx';
 
-import { ListItemType } from '../list/list-item';
-import { Select as Component } from './select';
-import { SelectItem } from './select-item';
-import { SelectSectionLabel } from './select-section-label';
+import { CheckmarkIcon } from '../icons/checkmark-icon';
+import { ChevronDownIcon } from '../icons/chevron-down-icon';
+import { PlaceholderIcon } from '../icons/placeholder-icon';
+import { Select as Component, SelectItem } from './select';
+import { SelectItemLayout } from './select-item.layout';
 
-const items: ListItemType[] = [{ label: 'Label 1' }, { label: 'Label 2' }];
+const items: SelectItem[] = [{ label: 'Label 1' }, { label: 'Label 2' }];
 
-const meta: Meta<typeof Component> = {
-  component: Component,
+const meta: Meta<typeof Component.Root> = {
+  component: Component.Root,
   tags: ['autodocs'],
   title: 'Select',
 };
 
 export default meta;
-type Story = StoryObj<typeof Component>;
+type Story = StoryObj<typeof Component.Root>;
 
 export const Select: Story = {
   render: () => (
-    <Component>
-      <RadixSelect.Group>
-        <SelectSectionLabel label="Items" />
-        {items.map(item => {
-          return (
-            <SelectItem
-              key={item.label}
-              iconLeft={item.iconLeft}
-              iconRight={item.iconRight}
-              label={item.label}
-            />
-          );
-        })}
-      </RadixSelect.Group>
-    </Component>
+    <Component.Root>
+      <Component.Trigger>
+        <Component.Value placeholder="Options" />
+        <Component.Icon>
+          <ChevronDownIcon />
+        </Component.Icon>
+      </Component.Trigger>
+      <Component.Portal>
+        <Component.Content align="start" position="popper" sideOffset={8}>
+          <Component.Viewport>
+            <Component.Group>
+              <Component.Label>Label</Component.Label>
+              {items.map(item => (
+                <Component.Item key={item.label} value={item.label}>
+                  <SelectItemLayout
+                    contentLeft={
+                      <HStack display="flex" gap="space.02" width="100%">
+                        <PlaceholderIcon />
+                        <Component.ItemText>
+                          <styled.span textStyle="label.02">{item.label}</styled.span>
+                        </Component.ItemText>
+                        <Component.ItemIndicator>
+                          <CheckmarkIcon />
+                        </Component.ItemIndicator>
+                      </HStack>
+                    }
+                    contentRight={<PlaceholderIcon />}
+                  />
+                </Component.Item>
+              ))}
+            </Component.Group>
+          </Component.Viewport>
+        </Component.Content>
+      </Component.Portal>
+    </Component.Root>
   ),
 };
