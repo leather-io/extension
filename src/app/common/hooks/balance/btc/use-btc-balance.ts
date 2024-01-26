67bdf11a2f2853cc8d5ba161dfdcf53ec6d11eb5
alter-eggo <anastas3993@gmail.com>
2024-01-26 14:58:39 +0400

    refactor: remove use spendable utxos hook

--- src/app/common/hooks/balance/btc/use-btc-balance.ts
+++ src/app/common/hooks/balance/btc/use-btc-balance.ts
@@ -1,24 +1,14 @@
 import { useMemo } from 'react';
 
-import BigNumber from 'bignumber.js';
-
-import { createMoney } from '@shared/models/money.model';
-
-import { baseCurrencyAmountInQuote, subtractMoney } from '@app/common/money/calculate-money';
+import { baseCurrencyAmountInQuote } from '@app/common/money/calculate-money';
 import { i18nFormatCurrency } from '@app/common/money/format-money';
 import { createBitcoinCryptoCurrencyAssetTypeWrapper } from '@app/query/bitcoin/address/address.utils';
-import { useBitcoinPendingTransactionsBalance } from '@app/query/bitcoin/address/transactions-by-address.hooks';
 import { useNativeSegwitBalance } from '@app/query/bitcoin/balance/btc-native-segwit-balance.hooks';
 import { useCryptoCurrencyMarketData } from '@app/query/common/market-data/market-data.hooks';
 
 export function useBtcAssetBalance(btcAddress: string) {
   const btcMarketData = useCryptoCurrencyMarketData('BTC');
   const btcAssetBalance = useNativeSegwitBalance(btcAddress);
-  const { data: pendingBalance } = useBitcoinPendingTransactionsBalance(btcAddress);
-  const availableBalance = subtractMoney(
-    btcAssetBalance.balance,
-    pendingBalance ?? createMoney(new BigNumber(0), 'BTC')
-  );
 
   return useMemo(
     () => ({
@@ -27,11 +17,13 @@ export function useBtcAssetBalance(btcAddress: string) {
       btcUsdBalance: i18nFormatCurrency(
         baseCurrencyAmountInQuote(btcAssetBalance.balance, btcMarketData)
       ),
-      btcAvailableAssetBalance: createBitcoinCryptoCurrencyAssetTypeWrapper(availableBalance),
+      btcAvailableAssetBalance: createBitcoinCryptoCurrencyAssetTypeWrapper(
+        btcAssetBalance.balance
+      ),
       btcAvailableUsdBalance: i18nFormatCurrency(
-        baseCurrencyAmountInQuote(availableBalance, btcMarketData)
+        baseCurrencyAmountInQuote(btcAssetBalance.balance, btcMarketData)
       ),
     }),
-    [btcAddress, btcAssetBalance, btcMarketData, availableBalance]
+    [btcAddress, btcAssetBalance, btcMarketData]
   );
 }
--- src/app/query/bitcoin/address/transactions-by-address.hooks.ts
+++ src/app/query/bitcoin/address/transactions-by-address.hooks.ts
@@ -1,16 +1,13 @@
 import { useCallback } from 'react';
 
-import { createMoney } from '@shared/models/money.model';
 import { BitcoinTx } from '@shared/models/transactions/bitcoin-transaction.model';
 
 import { sumNumbers } from '@app/common/math/helpers';
 
-import { UtxoResponseItem } from '../bitcoin-client';
 import {
   useGetBitcoinTransactionsByAddressQuery,
   useGetBitcoinTransactionsByAddressesQuery,
 } from './transactions-by-address.query';
-import { useAllSpendableUtxosByAddress } from './utxos-by-address.hooks';
 
 function useFilterAddressPendingTransactions() {
   return useCallback((txs: BitcoinTx[]) => {
@@ -52,33 +49,4 @@ export function calculateOutboundPendingTxsValue(pendingTxs: BitcoinTx[], addres
   const sumOutputs = sumNumbers(returnedOutputChangeValues);
 
   return sumInputs.minus(sumOutputs).toNumber();
-}
-
-// filter out pending txs that have inputs that are not in the utxos list to prevent double extraction
-function filterMissingUtxosPendingTxs(
-  pendingTxs: BitcoinTx[],
-  utxos: UtxoResponseItem[],
-  address: string
-) {
-  return pendingTxs.filter(tx => {
-    return tx.vin.every(input => {
-      return (
-        utxos.some(utxo => utxo.txid === input.txid) &&
-        address === input.prevout.scriptpubkey_address
-      );
-    });
-  });
-}
-
-export function useBitcoinPendingTransactionsBalance(address: string) {
-  const filterPendingTransactions = useFilterAddressPendingTransactions();
-  const { data: utxos } = useAllSpendableUtxosByAddress(address);
-
-  return useGetBitcoinTransactionsByAddressQuery(address, {
-    select(txs) {
-      const pendingTxs = filterPendingTransactions(txs);
-      const filteredTxs = filterMissingUtxosPendingTxs(pendingTxs, utxos || [], address);
-      return createMoney(calculateOutboundPendingTxsValue(filteredTxs, address), 'BTC');
-    },
-  });
 }
