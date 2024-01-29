8117eb539a9228524f9b8ca951211e68d526369c
alter-eggo <anastas3993@gmail.com>
2024-01-29 17:03:18 +0400

    fix: no btc signer in  ledger mode, ref #4873

--- src/app/common/hooks/use-transferable-asset-balances.hooks.ts
+++ src/app/common/hooks/use-transferable-asset-balances.hooks.ts
@@ -2,18 +2,14 @@ import { useMemo } from 'react';
 
 import type { AllTransferableCryptoAssetBalances } from '@shared/models/crypto-asset-balance.model';
 
-import { useNativeSegwitBalance } from '@app/query/bitcoin/balance/btc-native-segwit-balance.hooks';
 import { useTransferableStacksFungibleTokenAssetBalances } from '@app/query/stacks/balance/stacks-ft-balances.hooks';
 import { createStacksCryptoCurrencyAssetTypeWrapper } from '@app/query/stacks/balance/stacks-ft-balances.utils';
-import { useCurrentAccountNativeSegwitAddressIndexZero } from '@app/store/accounts/blockchain/bitcoin/native-segwit-account.hooks';
 import { useCurrentStacksAccount } from '@app/store/accounts/blockchain/stacks/stacks-account.hooks';
 
 import { useStxBalance } from './balance/stx/use-stx-balance';
 
 export function useAllTransferableCryptoAssetBalances(): AllTransferableCryptoAssetBalances[] {
   const account = useCurrentStacksAccount();
-  const currentBtcAddress = useCurrentAccountNativeSegwitAddressIndexZero();
-  const btcCryptoCurrencyAssetBalance = useNativeSegwitBalance(currentBtcAddress);
 
   const { availableBalance: availableStxBalance } = useStxBalance();
   const stxCryptoCurrencyAssetBalance = createStacksCryptoCurrencyAssetTypeWrapper(
@@ -24,6 +20,6 @@ export function useAllTransferableCryptoAssetBalances(): AllTransferableCryptoAs
   );
 
   return useMemo(() => {
-    return [btcCryptoCurrencyAssetBalance, stxCryptoCurrencyAssetBalance, ...stacksFtAssetBalances];
-  }, [btcCryptoCurrencyAssetBalance, stacksFtAssetBalances, stxCryptoCurrencyAssetBalance]);
+    return [stxCryptoCurrencyAssetBalance, ...stacksFtAssetBalances];
+  }, [stacksFtAssetBalances, stxCryptoCurrencyAssetBalance]);
 }
--- src/app/components/crypto-assets/choose-crypto-asset/crypto-asset-list.tsx
+++ src/app/components/crypto-assets/choose-crypto-asset/crypto-asset-list.tsx
@@ -2,26 +2,39 @@ import type { AllTransferableCryptoAssetBalances } from '@shared/models/crypto-a
 import { StacksFungibleTokenAsset } from '@shared/models/crypto-asset.model';
 
 import { useWalletType } from '@app/common/use-wallet-type';
+import { BitcoinNativeSegwitAccountLoader } from '@app/components/account/bitcoin-account-loader';
+import { BitcoinBalanceLoader } from '@app/components/balance/bitcoin-balance-loader';
+import { Brc20TokensLoader } from '@app/components/brc20-tokens-loader';
 import { Brc20TokenAssetList } from '@app/components/crypto-assets/bitcoin/brc20-token-asset-list/brc20-token-asset-list';
-import { Brc20Token } from '@app/query/bitcoin/ordinals/brc20/brc20-tokens.query';
+import { BtcIcon } from '@app/ui/components/icons/btc-icon';
 
+import { CryptoCurrencyAssetItem } from '../crypto-currency-asset/crypto-currency-asset-item';
 import { CryptoAssetListItem } from './crypto-asset-list-item';
 import { CryptoAssetListLayout } from './crypto-asset-list.layout';
 
 interface CryptoAssetListProps {
   cryptoAssetBalances: AllTransferableCryptoAssetBalances[];
   onItemClick(cryptoAssetBalance: AllTransferableCryptoAssetBalances): void;
-  brc20Tokens?: Brc20Token[];
 }
-export function CryptoAssetList({
-  cryptoAssetBalances,
-  onItemClick,
-  brc20Tokens,
-}: CryptoAssetListProps) {
+export function CryptoAssetList({ cryptoAssetBalances, onItemClick }: CryptoAssetListProps) {
   const { whenWallet } = useWalletType();
 
   return (
     <CryptoAssetListLayout>
+      <BitcoinNativeSegwitAccountLoader current>
+        {signer => (
+          <BitcoinBalanceLoader address={signer.address}>
+            {balance => (
+              <CryptoCurrencyAssetItem
+                assetBalance={balance}
+                icon={<BtcIcon />}
+                onClick={() => onItemClick(balance)}
+                isPressable
+              />
+            )}
+          </BitcoinBalanceLoader>
+        )}
+      </BitcoinNativeSegwitAccountLoader>
       {cryptoAssetBalances.map(cryptoAssetBalance => (
         <CryptoAssetListItem
           onClick={() => onItemClick(cryptoAssetBalance)}
@@ -32,12 +45,18 @@ export function CryptoAssetList({
           }
         />
       ))}
-      {brc20Tokens
-        ? whenWallet({
-            software: <Brc20TokenAssetList brc20Tokens={brc20Tokens} />,
-            ledger: null,
-          })
-        : null}
+      {whenWallet({
+        software: (
+          <BitcoinNativeSegwitAccountLoader current>
+            {() => (
+              <Brc20TokensLoader>
+                {brc20Tokens => <Brc20TokenAssetList brc20Tokens={brc20Tokens} />}
+              </Brc20TokensLoader>
+            )}
+          </BitcoinNativeSegwitAccountLoader>
+        ),
+        ledger: null,
+      })}
     </CryptoAssetListLayout>
   );
 }
