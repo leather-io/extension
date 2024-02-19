6b4d2c8bde5f4b185bf60c2354e78e320ccbea77
Vin Lim <vin@greenroom.com.my>
2024-02-19 16:22:11 +0800

    Omit unrelated commit

--- src/app/pages/rpc-get-addresses/use-request-accounts.ts
+++ src/app/pages/rpc-get-addresses/use-request-accounts.ts
@@ -43,7 +43,7 @@ export function useGetAddresses() {
           publicKey: bytesToHex(nativeSegwitSigner.publicKey),
           derivationPath: nativeSegwitSigner.derivationPath,
         };
-        
+
         keysToIncludeInResponse.push(nativeSegwitAddressResponse);
       }
 
