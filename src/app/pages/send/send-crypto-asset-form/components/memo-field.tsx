8829a2a67af86fdf082532d055e87320365d00d7
kyranjamie <kyran.burraston@gmail.com>
2024-02-19 16:38:17 +0100

    fix: memo using old input

--- src/app/pages/send/send-crypto-asset-form/components/memo-field.tsx
+++ src/app/pages/send/send-crypto-asset-form/components/memo-field.tsx
@@ -1,9 +1,27 @@
 import { SendCryptoAssetSelectors } from '@tests/selectors/send.selectors';
+import { useField } from 'formik';
+import { Box } from 'leather-styles/jsx';
 
-import { TextInputField } from '@app/components/text-input-field';
+import { useShowFieldError } from '@app/common/form-utils';
+import { TextInputFieldError } from '@app/components/field-error';
+import { Input } from '@app/ui/components/input/input';
+
+const name = 'memo';
 
 export function MemoField() {
+  const [field] = useField(name);
+
+  const showError = useShowFieldError(name);
+
   return (
-    <TextInputField dataTestId={SendCryptoAssetSelectors.MemoFieldInput} label="Memo" name="memo" />
+    <Box width="100%">
+      <Input.Root hasError={!!showError}>
+        <Input.Label>Memo</Input.Label>
+        <Input.Field data-testid={SendCryptoAssetSelectors.MemoFieldInput} {...field} />
+      </Input.Root>
+      <Box mt="space.02">
+        <TextInputFieldError name={name} />
+      </Box>
+    </Box>
   );
 }
