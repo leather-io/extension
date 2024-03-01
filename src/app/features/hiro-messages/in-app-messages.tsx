e47e49e74972a5f28ba307d9563e5428cc8801ce
kyranjamie <kyran.burraston@gmail.com>
2024-03-01 10:50:00 +0100

    fix: warning banner, add fake app warning

--- src/app/features/hiro-messages/in-app-messages.tsx
+++ src/app/features/hiro-messages/in-app-messages.tsx
@@ -25,7 +25,7 @@ export function InAppMessages(props: FlexProps) {
   }
 
   return (
-    <Flex bg="ink.component-background-default" color="white" {...props}>
+    <Flex bg="ink.non-interactive" {...props}>
       <HiroMessageItem onDismiss={messageId => dismissMessage(messageId)} {...firstMessage} />
     </Flex>
   );
