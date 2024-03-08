bdeb1c18717ed487fedf5552f40e84f63032a5a3
kyranjamie <kyran.burraston@gmail.com>
2024-03-08 14:41:27 +0100

    fix: upgrade tokens package

--- theme/typography.ts
+++ theme/typography.ts
@@ -1,5 +1,5 @@
-import { textStyles as leatherTextStyles } from '@leather-wallet/tokens';
+import { getWebTextVariants } from '@leather-wallet/tokens';
 import { defineTextStyles } from '@pandacss/dev';
 
 // ts-unused-exports:disable-next-line
-export const textStyles = defineTextStyles({ ...leatherTextStyles });
+export const textStyles = defineTextStyles({ ...getWebTextVariants() });
