52adc39721ce9ba4e6d122fa70d5e22e3935d198
kyranjamie <kyran.burraston@gmail.com>
2024-03-14 11:01:39 +0100

    chore: migrate to pnpm

--- test-app/webpack/webpack.config.base.cjs
+++ test-app/webpack/webpack.config.base.cjs
@@ -102,6 +102,7 @@ const config = {
       crypto: require.resolve('crypto-browserify'),
       stream: require.resolve('stream-browserify'),
       vm: require.resolve('vm-browserify'),
+      buffer: require.resolve('buffer/'),
     },
   },
   optimization: {
