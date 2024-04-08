e06ee175052f0c1cff4608060b19f6830a10c75d
Vin Lim <vinlim@Vins-MacBook-Pro.local>
2024-04-08 10:12:35 +0800

    Add test

---
+++ tests/specs/rpc-get-xpub/get-xpub.spec.ts
@@ -0,0 +1,47 @@
+import {BrowserContext, Page} from '@playwright/test';
+import {test} from '../../fixtures/fixtures';
+import {TEST_ACCOUNT_1_XPUB} from "@tests/mocks/constants";
+
+test.describe('RPC get Xpub', () => {
+  test.beforeEach(async ({extensionId, globalPage, onboardingPage, page}) => {
+    await globalPage.setupAndUseApiCalls(extensionId);
+    await onboardingPage.signInWithTestAccount(extensionId);
+    await page.goto('localhost:3000', {waitUntil: 'networkidle'});
+  });
+
+  function checkVisibleContent(context: BrowserContext) {
+    return async (buttonToPress: 'Cancel' | 'Confirm') => {
+      const popup = await context.waitForEvent('page');
+      await popup.waitForTimeout(500);
+      const btn = popup.locator('text="Connect Leather"');
+
+      if (buttonToPress === 'Confirm') {
+        await btn.click();
+      } else {
+        await popup.close();
+      }
+    };
+  }
+
+  function initiateRPCGetXpub(page: Page) {
+    return async () =>
+      page.evaluate(
+        async () =>
+          (window as any).LeatherProvider.request('getXpub')
+            .catch((e: unknown) => e)
+      );
+  }
+
+  test('that xpub is correct', async ({
+    page,
+    context,
+  }) => {
+    const xpub = TEST_ACCOUNT_1_XPUB;
+    const [result] = await Promise.all([
+      initiateRPCGetXpub(page)(),
+      checkVisibleContent(context)('Confirm'),
+    ]);
+
+    test.expect(result.result.xpubs[0].xpub).toEqual(xpub);
+  });
+});
