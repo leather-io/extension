679b8004c0bdc3c649c5f0703bad8d75916d8ff2
alter-eggo <anastas3993@gmail.com>
2024-04-10 17:59:11 +0400

    fix: wrong bns name while loading

--- src/app/components/account/account-name.tsx
+++ src/app/components/account/account-name.tsx
@@ -1,21 +1,24 @@
 import { memo } from 'react';
 
-import { styled } from 'leather-styles/jsx';
+import { type HTMLStyledProps, styled } from 'leather-styles/jsx';
 
 import { shimmerStyles } from '@app/ui/shared/shimmer-styles';
 
-interface AccountNameLayoutProps {
+interface AccountNameLayoutProps extends HTMLStyledProps<'span'> {
   children: React.ReactNode;
   isLoading?: boolean;
 }
 
-export const AccountNameLayout = memo(({ children, isLoading }: AccountNameLayoutProps) => (
-  <styled.span
-    className={shimmerStyles}
-    textStyle="label.02"
-    aria-busy={isLoading}
-    data-state={isLoading ? 'loading' : undefined}
-  >
-    {children}
-  </styled.span>
-));
+export const AccountNameLayout = memo(
+  ({ children, isLoading, ...rest }: AccountNameLayoutProps) => (
+    <styled.span
+      className={shimmerStyles}
+      textStyle="label.02"
+      aria-busy={isLoading}
+      data-state={isLoading ? 'loading' : undefined}
+      {...rest}
+    >
+      {children}
+    </styled.span>
+  )
+);
