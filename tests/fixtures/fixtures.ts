6322afdb6c6dac405fe6d75ef9c198ad8156e274
kyranjamie <kyran.burraston@gmail.com>
2024-04-21 12:41:51 +0200

    test: rpc get addresses, closes #5279, closes #5066 closes #5042

--- tests/fixtures/fixtures.ts
+++ tests/fixtures/fixtures.ts
@@ -44,6 +44,7 @@ export const test = base.extend<TestFixtures>({
       ],
     });
     await use(context);
+    await context.close();
   },
   extensionId: async ({ context }, use) => {
     let [background] = context.serviceWorkers();
