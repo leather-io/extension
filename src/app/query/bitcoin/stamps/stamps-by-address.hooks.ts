a8278c82774fdc4c9e566a1c6bdde3500847510a
Fara Woolf <fara@trustmachines.co>
2024-05-06 16:31:28 -0500

    fix: src-20 tokens map

--- src/app/query/bitcoin/stamps/stamps-by-address.hooks.ts
+++ src/app/query/bitcoin/stamps/stamps-by-address.hooks.ts
@@ -1,17 +1,21 @@
+import { ensureArray, isUndefined } from '@shared/utils';
+
 import { useStampsByAddressQuery } from './stamps-by-address.query';
 
 export function useStampsByAddress(address: string) {
   return useStampsByAddressQuery(address, {
-    select(data) {
-      return data.data?.stamps;
+    select(resp) {
+      if (isUndefined(resp.data)) return [];
+      return ensureArray(resp.data.stamps);
     },
   });
 }
 
 export function useSrc20TokensByAddress(address: string) {
   return useStampsByAddressQuery(address, {
-    select(data) {
-      return data.data?.src20;
+    select(resp) {
+      if (isUndefined(resp.data)) return [];
+      return ensureArray(resp.data.src20);
     },
   });
 }
