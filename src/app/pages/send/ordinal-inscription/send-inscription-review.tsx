3a42fc9848c124e37035ce8d3a41b82eac998450
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-05-17 16:09:16 +0100

    fix: fix padding on back button, ref leather-wallet/issues#25

--- src/app/pages/send/ordinal-inscription/send-inscription-review.tsx
+++ src/app/pages/send/ordinal-inscription/send-inscription-review.tsx
@@ -64,7 +64,7 @@ export function SendInscriptionReview() {
         });
       },
       onError(e) {
-        void analytics.track('ordinalbroadcast__error', { error: e });
+        void analytics.track('broadcast_ordinal_error', { error: e });
         navigate(`/${RouteUrls.SendOrdinalInscription}/${RouteUrls.SendOrdinalInscriptionError}`, {
           state: {
             error: e,
