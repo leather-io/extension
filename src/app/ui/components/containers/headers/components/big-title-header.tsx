72fba1a86559c56ba82acfdf8f8d64264daf2e65
Jean Ribeiro <contact@jeanribeiro.dev>
2024-05-21 20:15:12 -0300

    fix: close button still showing in breakpoint

--- src/app/ui/components/containers/headers/components/big-title-header.tsx
+++ src/app/ui/components/containers/headers/components/big-title-header.tsx
@@ -16,13 +16,15 @@ export function BigTitleHeader({ onClose, title }: BigTitleHeaderProps) {
   return (
     <Flex justifyContent="space-between" mt={{ base: 'space.05', md: 'unset' }}>
       <styled.h3 textStyle="heading.03">{title}</styled.h3>
-      {onClose && (
-        <HeaderActionButton
-          icon={<CloseIcon hideBelow="md" />}
-          dataTestId={SharedComponentsSelectors.HeaderCloseBtn}
-          onAction={onClose}
-        />
-      )}
+        {onClose && (
+          <styled.div hideBelow="md">
+            <HeaderActionButton
+              icon={<CloseIcon />}
+              dataTestId={SharedComponentsSelectors.HeaderCloseBtn}
+              onAction={onClose}
+            />
+          </styled.div>
+        )}
     </Flex>
   );
 }
