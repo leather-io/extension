473a3938e500812a23a4b116a82cc5e97fab066c
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-05-23 10:12:57 +0100

    feat: panda preset package, ref leather-wallet/issues#62

--- theme/breakpoints.ts
+++
@@ -1,6 +0,0 @@
-import { breakpoints as leatherBreakpoints } from '@leather-wallet/tokens';
-
-// ts-unused-exports:disable-next-line
-export const breakpoints = {
-  ...leatherBreakpoints,
-};
--- theme/typography.ts
+++
@@ -1,5 +0,0 @@
-import { getWebTextVariants } from '@leather-wallet/tokens';
-import { defineTextStyles } from '@pandacss/dev';
-
-// ts-unused-exports:disable-next-line
-export const textStyles = defineTextStyles({ ...getWebTextVariants() });
