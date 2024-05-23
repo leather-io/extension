36e52388cbf4d765942d228e776d334fbf44984f
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-05-23 10:12:57 +0100

    feat: panda preset package, ref leather-wallet/issues#62

--- theme/colors.ts
+++
@@ -1,8 +0,0 @@
-import { defineTokens } from '@pandacss/dev';
-
-// ts-unused-exports:disable-next-line
-export const colors = defineTokens.colors({
-  // TODO: Move to mono repo
-  current: { value: 'currentColor' },
-  overlay: { value: 'rgba(0,0,0,0.4)' },
-});
