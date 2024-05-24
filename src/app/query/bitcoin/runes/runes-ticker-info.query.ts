aa50a08b0250e1cb1748da8143995369f332f21a
alter-eggo <anastas3993@gmail.com>
2024-05-24 09:57:33 +0400

    fix: display runes balance, closes #5434

--- src/app/query/bitcoin/runes/runes-ticker-info.query.ts
+++ src/app/query/bitcoin/runes/runes-ticker-info.query.ts
@@ -1,10 +1,12 @@
 import { useQueries } from '@tanstack/react-query';
 
-import { useConfigRunesEnabled } from '@app/query/common/remote-config/remote-config.query';
+import { isDefined } from '@shared/utils';
+
 import { useBitcoinClient } from '@app/store/common/api-clients.hooks';
 import { useCurrentNetwork } from '@app/store/networks/networks.selectors';
 
 import type { RuneBalance, RuneTickerInfo } from '../bitcoin-client';
+import { useRunesEnabled } from './runes.hooks';
 import { createRuneCryptoAssetDetails } from './runes.utils';
 
 const queryOptions = { staleTime: 5 * 60 * 1000 };
@@ -12,13 +14,12 @@ const queryOptions = { staleTime: 5 * 60 * 1000 };
 export function useGetRunesTickerInfoQuery(runesBalances: RuneBalance[]) {
   const client = useBitcoinClient();
   const network = useCurrentNetwork();
-  const runesEnabled = useConfigRunesEnabled();
+  const runesEnabled = useRunesEnabled();
 
   return useQueries({
     queries: runesBalances.map(runeBalance => {
       return {
-        enabled:
-          !runeBalance && (network.chain.bitcoin.bitcoinNetwork === 'testnet' || runesEnabled),
+        enabled: isDefined(runeBalance) && runesEnabled,
         queryKey: ['runes-ticker-info', runeBalance.rune_name],
         queryFn: () =>
           client.bestinSlotApi.getRunesTickerInfo(
