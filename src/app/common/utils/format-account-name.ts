1c26530022d667e85bc12738ab66cd1410dbb0fc
Zishan <amanzishan.az@gmail.com>
2024-06-03 16:48:15 +0530

    fix(long-bns-names): truncate BNS names
    - new format utility function to truncate BNS name if length is greater than 20 characters

---
+++ src/app/common/utils/format-account-name.ts
@@ -0,0 +1,9 @@
+import { truncateMiddle } from '@leather-wallet/utils';
+
+export function formatAccountName(input: string, maxLength = 20, offset = 4) {
+  if (input.length > maxLength) {
+    return truncateMiddle(input, offset);
+  } else {
+    return input;
+  }
+}
