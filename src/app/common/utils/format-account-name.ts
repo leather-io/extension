ddd3a291ffe231ce2437b9f487ebb6d5bdcefa21
Zishan <amanzishan.az@gmail.com>
2024-06-03 16:54:54 +0530

    refactor(format-account-name): remove unnecessary else case

--- src/app/common/utils/format-account-name.ts
+++ src/app/common/utils/format-account-name.ts
@@ -3,7 +3,6 @@ import { truncateMiddle } from '@leather-wallet/utils';
 export function formatAccountName(input: string, maxLength = 20, offset = 4) {
   if (input.length > maxLength) {
     return truncateMiddle(input, offset);
-  } else {
-    return input;
   }
+  return input;
 }
