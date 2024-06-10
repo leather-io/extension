3413148bef11642bae96069b971a07d1ead654d6
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-06-10 11:28:17 +0100

    chore: re-enable settings menu animations

--- src/app/ui/components/dropdown-menu/dropdown-menu.tsx
+++ src/app/ui/components/dropdown-menu/dropdown-menu.tsx
@@ -66,8 +66,8 @@ const dropdownContentStyles = css({
   p: '0',
   willChange: 'transform, opacity',
   zIndex: 999,
-  // _closed: { animation: 'slideDownAndOut 140ms ease-in-out' },
-  // _open: { animation: 'slideUpAndFade 140ms ease-in-out' },
+  _closed: { animation: 'slideDownAndOut 140ms ease-in-out' },
+  _open: { animation: 'slideUpAndFade 140ms ease-in-out' },
 });
 const Content: typeof RadixDropdownMenu.Content = forwardRef(({ className, ...props }, ref) => (
   <RadixDropdownMenu.Content
