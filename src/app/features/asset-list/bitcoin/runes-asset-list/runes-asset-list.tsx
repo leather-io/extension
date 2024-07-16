0a175f7edaf4d002cb26da62b25513b88f1fe683
Anastasios <anastas3993@gmail.com>
2024-07-16 19:28:10 +0400

    feat: add runes fiat balance, closes #5300

--- src/app/features/asset-list/bitcoin/runes-asset-list/runes-asset-list.tsx
+++ src/app/features/asset-list/bitcoin/runes-asset-list/runes-asset-list.tsx
@@ -1,17 +1,20 @@
-import type { CryptoAssetBalance, RuneCryptoAssetInfo } from '@leather.io/models';
+import type { CryptoAssetBalance, MarketData, RuneCryptoAssetInfo } from '@leather.io/models';
 import { RunesAvatarIcon } from '@leather.io/ui';
 import { convertAmountToBaseUnit, createMoneyFromDecimal } from '@leather.io/utils';
 
+import { convertAssetBalanceToFiat } from '@app/common/asset-utils';
 import { CryptoAssetItemLayout } from '@app/components/crypto-asset-item/crypto-asset-item.layout';
 
 interface RuneTokenAssetDetails {
   balance: CryptoAssetBalance;
   info: RuneCryptoAssetInfo;
+  marketData: MarketData;
 }
 
 interface RunesAssetListProps {
   runes: RuneTokenAssetDetails[];
 }
+
 export function RunesAssetList({ runes }: RunesAssetListProps) {
   return runes.map((rune, i) => (
     <CryptoAssetItemLayout
@@ -25,6 +28,10 @@ export function RunesAssetList({ runes }: RunesAssetListProps) {
       icon={<RunesAvatarIcon />}
       key={`${rune.info.symbol}${i}`}
       titleLeft={rune.info.spacedRuneName ?? rune.info.runeName}
+      fiatBalance={convertAssetBalanceToFiat({
+        balance: rune.balance.availableBalance,
+        marketData: rune.marketData,
+      })}
     />
   ));
 }
