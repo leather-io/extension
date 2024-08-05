da5cc05b1e2be631282ca3f29dc5527f26dc0214
kyranjamie <kyran.burraston@gmail.com>
2024-08-05 15:14:48 +0200

    chore: update org name

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -2,7 +2,7 @@ export const gaiaUrl = 'https://hub.blockstack.org';
 
 export const ZERO_INDEX = 0;
 
-export const GITHUB_ORG = 'leather-wallet';
+export const GITHUB_ORG = 'leather-io';
 export const GITHUB_REPO = 'extension';
 
 export const HIRO_EXPLORER_URL = 'https://explorer.hiro.so';
