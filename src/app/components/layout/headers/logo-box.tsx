e6983a426dd9135af0aeb2c2faedca825c113abf
Anastasios <anastas3993@gmail.com>
2024-08-07 16:29:23 +0400

    fix: logo box show

--- src/app/components/layout/headers/logo-box.tsx
+++ src/app/components/layout/headers/logo-box.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import { OnboardingSelectors } from '@tests/selectors/onboarding.selectors';
 import { Box } from 'leather-styles/jsx';
@@ -9,12 +9,21 @@ import { RouteUrls } from '@shared/route-urls';
 
 export function LogoBox({ isSessionLocked }: { isSessionLocked?: boolean | undefined }) {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const shouldShowLogo =
+    location.pathname === RouteUrls.Home || location.pathname === RouteUrls.Activity;
+
+  const hideBelow = shouldShowLogo ? undefined : isSessionLocked ? undefined : 'sm';
+  const hideFrom = shouldShowLogo ? undefined : isSessionLocked ? 'sm' : undefined;
+
   return (
     <Box
       height="headerContainerHeight"
       margin="auto"
       px="space.02"
-      hideFrom={isSessionLocked ? 'sm' : undefined}
+      hideBelow={hideBelow}
+      hideFrom={hideFrom}
     >
       <Logo
         data-testid={OnboardingSelectors.LogoRouteToHome}
