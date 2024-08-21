9bc13ab71d2e4360d073d66122799ff98ad78dbd
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-08-21 13:43:33 +0100

    chore: remove back button on unlock screen and remove fragment

--- src/app/features/container/headers/unlock.header.tsx
+++ src/app/features/container/headers/unlock.header.tsx
@@ -1,15 +1,13 @@
 import { useNavigate, useOutletContext } from 'react-router-dom';
 
 import { SettingsSelectors } from '@tests/selectors/settings.selectors';
-import { SharedComponentsSelectors } from '@tests/selectors/shared-component.selectors';
 
-import { ArrowLeftIcon, HamburgerIcon } from '@leather.io/ui';
+import { HamburgerIcon } from '@leather.io/ui';
 
 import { RouteUrls } from '@shared/route-urls';
 import { SwitchAccountOutletContext } from '@shared/switch-account';
 
 import { Header } from '@app/components/layout/headers/header';
-import { HeaderActionButton } from '@app/components/layout/headers/header-action-button';
 import { HeaderGrid, HeaderGridRightCol } from '@app/components/layout/headers/header-grid';
 import { LogoBox } from '@app/components/layout/headers/logo-box';
 import { Settings } from '@app/features/settings/settings';
@@ -20,34 +18,20 @@ export function UnlockHeader() {
   const navigate = useNavigate();
 
   return (
-    <>
-      <Header paddingLeft={{ base: undefined, sm: 0 }}>
-        <HeaderGrid
-          leftCol={
-            <>
-              <HeaderActionButton
-                icon={<ArrowLeftIcon />}
-                onAction={() => navigate(-1)}
-                dataTestId={SharedComponentsSelectors.HeaderBackBtn}
-              />
-
-              <LogoBox
-                onClick={() => navigate(RouteUrls.Home)}
-                hideBelow={undefined}
-                hideFrom="sm"
-              />
-            </>
-          }
-          rightCol={
-            <HeaderGridRightCol>
-              <Settings
-                triggerButton={<HamburgerIcon data-testid={SettingsSelectors.SettingsMenuBtn} />}
-                toggleSwitchAccount={() => setIsShowingSwitchAccount(!isShowingSwitchAccount)}
-              />
-            </HeaderGridRightCol>
-          }
-        />
-      </Header>
-    </>
+    <Header paddingLeft={{ base: undefined, sm: 0 }}>
+      <HeaderGrid
+        leftCol={
+          <LogoBox onClick={() => navigate(RouteUrls.Home)} hideBelow={undefined} hideFrom="sm" />
+        }
+        rightCol={
+          <HeaderGridRightCol>
+            <Settings
+              triggerButton={<HamburgerIcon data-testid={SettingsSelectors.SettingsMenuBtn} />}
+              toggleSwitchAccount={() => setIsShowingSwitchAccount(!isShowingSwitchAccount)}
+            />
+          </HeaderGridRightCol>
+        }
+      />
+    </Header>
   );
 }
