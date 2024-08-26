717737caa2d6d99d043f9d961af901a35a2be055
Anastasios <anastas3993@gmail.com>
2024-08-26 16:48:05 +0400

    feat: add mono link ui script

--- scripts/mono-link.js
+++ scripts/mono-link.js
@@ -1,28 +1,21 @@
 import fs from 'fs/promises';
 
-// Paths to your package.json files
 const filePath = './package.json';
 const backupFilePath = './package-original.json';
 
 async function modifyPackageJson() {
   try {
-    // Check if backup file exists
     try {
       await fs.access(backupFilePath);
       console.log(`Backup file already exists: ${backupFilePath}`);
     } catch {
-      // Backup the original package.json
       await fs.copyFile(filePath, backupFilePath);
       console.log(`Backup created: ${backupFilePath}`);
     }
 
-    // Read the package.json file
     const data = await fs.readFile(filePath, 'utf-8');
-
-    // Parse the JSON
     let packageJson = JSON.parse(data);
 
-    // Update dependencies to use relative paths
     const relativePaths = [
       '@leather.io/bitcoin',
       '@leather.io/constants',
@@ -30,15 +23,11 @@ async function modifyPackageJson() {
       '@leather.io/models',
       '@leather.io/query',
       '@leather.io/tokens',
-      '@leather.io/ui',
       '@leather.io/utils',
       '@leather.io/stacks',
     ];
-    
-    const devRelativePaths = [
-      '@leather.io/panda-preset',
-      '@leather.io/rpc'
-    ];
+
+    const devRelativePaths = ['@leather.io/panda-preset', '@leather.io/rpc'];
 
     relativePaths.forEach(dep => {
       if (packageJson.dependencies[dep]) {
@@ -52,7 +41,6 @@ async function modifyPackageJson() {
       }
     });
 
-    // Update pnpm.overrides to use relative paths
     if (!packageJson.pnpm) {
       packageJson.pnpm = {};
     }
@@ -60,16 +48,14 @@ async function modifyPackageJson() {
       packageJson.pnpm.overrides = {};
     }
 
-    relativePaths.concat(devRelativePaths).forEach(dep => {
+    [...relativePaths, ...devRelativePaths].forEach(dep => {
       if (packageJson.dependencies[dep] || packageJson.devDependencies[dep]) {
         packageJson.pnpm.overrides[dep] = `file:../mono/packages/${dep.split('/').pop()}`;
       }
     });
 
-    // Write the modified JSON back to the file with an empty line at the end
     await fs.writeFile(filePath, JSON.stringify(packageJson, null, 2) + '\n', 'utf-8');
     console.log(`Successfully updated ${filePath}`);
-
   } catch (err) {
     console.error(`Error: ${err}`);
   }
