68de602240f0fb83899babea56402f5dd28bd4b7
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-09-06 09:37:47 +0100

    fix: refactor add network to use card footer, closes leather-io/issues#313

--- src/app/components/error-label.tsx
+++ src/app/components/error-label.tsx
@@ -3,7 +3,7 @@ import { ErrorCircleIcon, Flag, type FlagProps } from '@leather.io/ui';
 export function ErrorLabel({ children, ...rest }: FlagProps) {
   return (
     <Flag
-      img={<ErrorCircleIcon variant="small" />}
+      img={<ErrorCircleIcon color="red.action-primary-default" variant="small" />}
       spacing="space.02"
       color="red.action-primary-default"
       textStyle="body.02"
