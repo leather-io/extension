2c02360d2c449bc0884cb2a0f68ee7173a8178fd
Will Cameron <camerow@users.noreply.github.com>
2024-12-16 12:05:29 -0800

    chore: fix unused exports

--- src/shared/rpc/methods/supported-methods.ts
+++ src/shared/rpc/methods/supported-methods.ts
@@ -1,10 +1,3 @@
-import { DefineRpcMethod, RpcRequest, RpcSuccessResponse } from '@leather.io/rpc';
+import { RpcRequest } from '@leather.io/rpc';
 
 export type SupportedMethodsRequest = RpcRequest<'supportedMethods'>;
-
-type SupportedMethodsResponse = RpcSuccessResponse<{
-  documentation: string;
-  methods: { name: string; docsUrl?: string | string[] }[];
-}>;
-
-export type SupportedMethods = DefineRpcMethod<SupportedMethodsRequest, SupportedMethodsResponse>;
