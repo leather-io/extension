258f0b00a02a946b4c3eec1fda6cdd5b1ab3bb1f
Fara Woolf <fara@trustmachines.co>
2024-12-17 19:27:47 -0500

    refactor: remove utxo filtering

--- src/app/query/bitcoin/address/utxos-by-address.hooks.ts
+++ src/app/query/bitcoin/address/utxos-by-address.hooks.ts
@@ -2,26 +2,14 @@ import { useNativeSegwitUtxosByAddress } from '@leather.io/query';
 
 import { useCurrentAccountNativeSegwitIndexZeroSigner } from '@app/store/accounts/blockchain/bitcoin/native-segwit-account.hooks';
 
-const defaultArgs = {
-  filterInscriptionUtxos: true,
-  filterPendingTxsUtxos: true,
-  filterRunesUtxos: true,
-};
-
-/**
- * Warning: ⚠️ To avoid spending inscriptions, when using UTXOs
- * we set `filterInscriptionUtxos` and `filterPendingTxsUtxos` to true
- */
-export function useCurrentNativeSegwitUtxos(args = defaultArgs) {
-  const { filterInscriptionUtxos, filterPendingTxsUtxos, filterRunesUtxos } = args;
-
+export function useCurrentNativeSegwitUtxos() {
   const nativeSegwitSigner = useCurrentAccountNativeSegwitIndexZeroSigner();
   const address = nativeSegwitSigner.address;
 
   return useNativeSegwitUtxosByAddress({
     address,
-    filterInscriptionUtxos,
-    filterPendingTxsUtxos,
-    filterRunesUtxos,
+    filterInscriptionUtxos: false,
+    filterPendingTxsUtxos: true,
+    filterRunesUtxos: false,
   });
 }
