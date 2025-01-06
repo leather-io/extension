3a1b17e8169fd13646403b609ca02d272502005d
Fara Woolf <fara@trustmachines.co>
2025-01-06 11:58:36 -0500

    fix: btc balance

--- tests/page-object-models/onboarding.page.ts
+++ tests/page-object-models/onboarding.page.ts
@@ -12,7 +12,7 @@ const TEST_ACCOUNT_SECRET_KEY = process.env.TEST_ACCOUNT_SECRET_KEY ?? '';
 
 // If default wallet state changes, we'll need to update this
 export const testSoftwareAccountDefaultWalletState = {
-  chains: { stx: { default: { highestAccountIndex: 1, currentAccountIndex: 0 } } },
+  chains: { stx: { default: { highestAccountIndex: 2, currentAccountIndex: 0 } } },
   appPermissions: {
     entities: {},
     ids: [],
