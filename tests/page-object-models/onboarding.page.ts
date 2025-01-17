1060864e502a2a463df4b2525c29939ce52e430f
kyranjamie <kyran.burraston@gmail.com>
2025-01-17 13:56:09 +0000

    test: use debug helper to set higher account index

--- tests/page-object-models/onboarding.page.ts
+++ tests/page-object-models/onboarding.page.ts
@@ -10,10 +10,6 @@ import { RouteUrls } from '@shared/route-urls';
 
 const TEST_ACCOUNT_SECRET_KEY = process.env.TEST_ACCOUNT_SECRET_KEY ?? '';
 
-async function pauseForStateToBeSet() {
-  return delay(2000);
-}
-
 // If default wallet state changes, we'll need to update this
 export const testSoftwareAccountDefaultWalletState = {
   chains: { stx: { default: { highestAccountIndex: 0, currentAccountIndex: 0 } } },
@@ -323,26 +319,7 @@ export class OnboardingPage {
       iterationCounter.increment();
     } while (!(await isSignedIn()));
 
-    await this.createNewAccount();
-    await this.createNewAccount();
-    await this.selectFirstAccount();
-    await pauseForStateToBeSet();
-  }
-
-  async createNewAccount() {
-    await this.page.getByTestId('switch-account-trigger').click();
-    await this.page.getByTestId('switch-account-item-0').isVisible();
-    const dialog = this.page.getByRole('dialog');
-    await this.page.getByTestId('create-account-btn').click();
-    await dialog.waitFor({ state: 'detached' });
-  }
-
-  async selectFirstAccount() {
-    await this.page.getByTestId('switch-account-trigger').click();
-    await this.page.getByTestId('switch-account-item-0').isVisible();
-    const dialog = this.page.getByRole('dialog');
-    await this.page.getByTestId('switch-account-item-0').click();
-    await dialog.waitFor({ state: 'detached' });
+    await this.page.evaluate(() => window.debug.setHighestAccountIndex(2));
   }
 
   /**
