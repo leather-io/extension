30a2d8ee5f7f16a909eafa7fd149939b373248d5
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2025-01-28 16:31:58 +0000

    fix: show available metadata for stacks nfts, ref leather-io/extension#5349

--- src/app/components/collectibles/collectible-image.tsx
+++ src/app/components/collectibles/collectible-image.tsx
@@ -1,5 +1,7 @@
 import { ReactNode, useState } from 'react';
 
+import { isValidUrl } from '@shared/utils/validate-url';
+
 import { CollectibleItemLayout, CollectibleItemLayoutProps } from './collectible-item.layout';
 import { ImageUnavailable } from './image-unavailable';
 
@@ -13,36 +15,34 @@ export function CollectibleImage(props: CollectibleImageProps) {
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [width, setWidth] = useState(0);
-
-  if (isError)
-    return (
-      <CollectibleItemLayout collectibleTypeIcon={icon} {...rest}>
-        <ImageUnavailable />
-      </CollectibleItemLayout>
-    );
+  const isImageAvailable = src && isValidUrl(src);
 
   return (
     <CollectibleItemLayout collectibleTypeIcon={icon} {...rest}>
-      <img
-        alt={alt}
-        onError={() => setIsError(true)}
-        loading="lazy"
-        onLoad={event => {
-          const target = event.target as HTMLImageElement;
-          setWidth(target.naturalWidth);
-          setIsLoading(false);
-        }}
-        src={src}
-        style={{
-          width: '100%',
-          height: '100%',
-          aspectRatio: '1 / 1',
-          objectFit: 'cover',
-          // display: 'none' breaks onLoad event firing
-          opacity: isLoading ? '0' : '1',
-          imageRendering: width <= 40 ? 'pixelated' : 'auto',
-        }}
-      />
+      {isError || !isImageAvailable ? (
+        <ImageUnavailable />
+      ) : (
+        <img
+          alt={alt}
+          onError={() => setIsError(true)}
+          loading="lazy"
+          onLoad={event => {
+            const target = event.target as HTMLImageElement;
+            setWidth(target.naturalWidth);
+            setIsLoading(false);
+          }}
+          src={src}
+          style={{
+            width: '100%',
+            height: '100%',
+            aspectRatio: '1 / 1',
+            objectFit: 'cover',
+            // display: 'none' breaks onLoad event firing
+            opacity: isLoading ? '0' : '1',
+            imageRendering: width <= 40 ? 'pixelated' : 'auto',
+          }}
+        />
+      )}
     </CollectibleItemLayout>
   );
 }
