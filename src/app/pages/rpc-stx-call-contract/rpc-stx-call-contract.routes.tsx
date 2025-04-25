985c3b83282393ed599adeeee00a0df763cd339d
kyranjamie <kyran.burraston@gmail.com>
2025-04-25 11:45:21 -0400

    fix(ledger): incorrect chain signer for stacks flow, closes #6223, LEA-63

--- src/app/pages/rpc-stx-call-contract/rpc-stx-call-contract.routes.tsx
+++ src/app/pages/rpc-stx-call-contract/rpc-stx-call-contract.routes.tsx
@@ -3,7 +3,7 @@ import { Route } from 'react-router-dom';
 import { RouteUrls } from '@shared/route-urls';
 
 import { BroadcastErrorSheet } from '@app/components/broadcast-error-dialog/broadcast-error-dialog';
-import { ledgerBitcoinTxSigningRoutes } from '@app/features/ledger/flows/bitcoin-tx-signing/ledger-bitcoin-sign-tx-container';
+import { ledgerStacksTxSigningRoutes } from '@app/features/ledger/flows/stacks-tx-signing/ledger-sign-stacks-tx-container';
 import { NonceEditor } from '@app/features/nonce-editor/nonce-editor';
 import { AccountGate } from '@app/routes/account-gate';
 
@@ -24,6 +24,6 @@ export const rpcStxCallContractRoutes = (
     <Route path={RouteUrls.FeeEditor} element={<FeeEditor />} />
     <Route path={RouteUrls.NonceEditor} element={<NonceEditor />} />
     <Route path={RouteUrls.BroadcastError} element={<BroadcastErrorSheet />} />
-    {ledgerBitcoinTxSigningRoutes}
+    {ledgerStacksTxSigningRoutes}
   </Route>
 );
