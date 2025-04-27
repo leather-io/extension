476c7ad3604d1f8385a2e621229ea1407bbdc223
fbwoolf <fara@trustmachines.co>
2025-04-27 12:24:13 -0400

    feat: ledger route gates

---
+++ src/app/features/ledger/generic-steps/connect-device/connect-ledger-bitcoin.tsx
@@ -0,0 +1,42 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import { Sheet, SheetHeader } from '@leather.io/ui';
+
+import { RouteUrls } from '@shared/route-urls';
+import { closeWindow } from '@shared/utils';
+
+import { whenPageMode } from '@app/common/utils';
+import { openIndexPageInNewTab } from '@app/common/utils/open-in-new-tab';
+
+import { immediatelyAttemptLedgerConnection } from '../../hooks/use-when-reattempt-ledger-connection';
+import { ConnectLedger } from './connect-ledger';
+
+export function ConnectLedgerBitcoin() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  function onConnectBitcoin() {
+    return whenPageMode({
+      full() {
+        navigate('bitcoin/connect-your-ledger', {
+          replace: true,
+          state: {
+            [immediatelyAttemptLedgerConnection]: true,
+            backgroundLocation: { pathname: RouteUrls.Home },
+            fromLocation: location,
+          },
+        });
+      },
+      popup() {
+        void openIndexPageInNewTab(RouteUrls.Home);
+        closeWindow();
+      },
+    });
+  }
+
+  return (
+    <Sheet isShowing header={<SheetHeader />} onClose={() => navigate('../')}>
+      <ConnectLedger connectBitcoin={onConnectBitcoin()} showInstructions />
+    </Sheet>
+  );
+}
--- src/app/pages/rpc-stx-call-contract/rpc-stx-call-contract.routes.tsx
+++ src/app/pages/rpc-stx-call-contract/rpc-stx-call-contract.routes.tsx
@@ -4,8 +4,10 @@ import { RouteUrls } from '@shared/route-urls';
 
 import { BroadcastErrorSheet } from '@app/components/broadcast-error-dialog/broadcast-error-dialog';
 import { ledgerStacksTxSigningRoutes } from '@app/features/ledger/flows/stacks-tx-signing/ledger-sign-stacks-tx-container';
+import { ConnectLedgerStacks } from '@app/features/ledger/generic-steps/connect-device/connect-ledger-stacks';
 import { NonceEditor } from '@app/features/nonce-editor/nonce-editor';
 import { AccountGate } from '@app/routes/account-gate';
+import { LedgerStacksGate } from '@app/routes/ledger-stacks-gate';
 
 import { FeeEditor } from '../../features/fee-editor/fee-editor';
 import { RpcStxCallContract } from './rpc-stx-call-contract';
@@ -16,7 +18,9 @@ export const rpcStxCallContractRoutes = (
     path={RouteUrls.RpcStxCallContract}
     element={
       <AccountGate>
-        <RpcStxCallContractContainer />
+        <LedgerStacksGate fallback={<ConnectLedgerStacks />}>
+          <RpcStxCallContractContainer />
+        </LedgerStacksGate>
       </AccountGate>
     }
   >
