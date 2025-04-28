5a9507542550b030c04d499340e82c2937b59d3a
fbwoolf <fara@trustmachines.co>
2025-04-28 13:01:05 -0400

    fix: return children if not ledger

--- src/app/routes/ledger-bitcoin-gate.tsx
+++ src/app/routes/ledger-bitcoin-gate.tsx
@@ -1,10 +1,13 @@
 import { BitcoinNativeSegwitAccountLoader } from '@app/components/loaders/bitcoin-account-loader';
+import { useHasLedgerKeys } from '@app/store/ledger/ledger.selectors';
 
 interface LedgerBitcoinGateProps {
   children: React.ReactNode;
   fallback?: React.ReactNode;
 }
 export function LedgerBitcoinGate({ children, fallback }: LedgerBitcoinGateProps) {
+  const isLedger = useHasLedgerKeys();
+  if (!isLedger) return children;
   return (
     <BitcoinNativeSegwitAccountLoader current fallback={fallback}>
       {() => children}
--- src/app/routes/ledger-stacks-gate.tsx
+++ src/app/routes/ledger-stacks-gate.tsx
@@ -1,10 +1,13 @@
 import { CurrentStacksAccountLoader } from '@app/components/loaders/stacks-account-loader';
+import { useHasLedgerKeys } from '@app/store/ledger/ledger.selectors';
 
 interface LedgerStacksGateProps {
   children: React.ReactNode;
   fallback?: React.ReactNode;
 }
 export function LedgerStacksGate({ children, fallback }: LedgerStacksGateProps) {
+  const isLedger = useHasLedgerKeys();
+  if (!isLedger) return children;
   return (
     <CurrentStacksAccountLoader fallback={fallback}>{() => children}</CurrentStacksAccountLoader>
   );
