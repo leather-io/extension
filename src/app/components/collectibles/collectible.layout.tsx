b5bc71ae4bff2a70922c82b7771ae1eb42738dd6
kyranjamie <kyran.burraston@gmail.com>
2025-05-23 10:06:48 +0200

    feat: additional inscription controls

--- src/app/components/collectibles/collectible.layout.tsx
+++ src/app/components/collectibles/collectible.layout.tsx
@@ -1,13 +1,22 @@
-import { Flex, Grid, HStack, styled } from 'leather-styles/jsx';
+import { Box, Flex, Grid, HStack, styled } from 'leather-styles/jsx';
 import { token } from 'leather-styles/tokens';
 
-import { ArrowRotateRightLeftIcon, Spinner } from '@leather.io/ui';
+import {
+  ArrowOutOfBoxIcon,
+  ArrowRotateRightLeftIcon,
+  DropdownMenu,
+  Flag,
+  Spinner,
+  TrashIcon,
+} from '@leather.io/ui';
 
 interface CollectiblesLayoutProps {
   title: string;
   isLoading: boolean;
   isLoadingMore?: boolean;
   onRefresh(): void;
+  onDiscardAllInscriptions(): void;
+  onRecoverAllInscriptions(): void;
   subHeader?: React.ReactNode;
   children: React.ReactNode;
 }
@@ -17,13 +26,35 @@ export function CollectiblesLayout({
   onRefresh,
   subHeader,
   children,
+  onDiscardAllInscriptions,
+  onRecoverAllInscriptions,
 }: CollectiblesLayoutProps) {
   return (
     <>
       <Flex flexDirection="row" justifyContent="space-between" alignItems="center" flex={1}>
         <HStack columnGap="space.02">
           <styled.span textStyle="label.01" paddingY="space.05">
-            {title}
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger>{title}</DropdownMenu.Trigger>
+              <DropdownMenu.Content>
+                <Box p="space.02" textStyle="label.03">
+                  <DropdownMenu.Item onClick={() => onRecoverAllInscriptions()}>
+                    <Flag spacing="space.02" img={<ArrowOutOfBoxIcon variant="small" />}>
+                      Recover all inscriptions
+                    </Flag>
+                  </DropdownMenu.Item>
+                  <DropdownMenu.Item onClick={() => onDiscardAllInscriptions()}>
+                    <Flag
+                      spacing="space.02"
+                      color="red.action-primary-default"
+                      img={<TrashIcon color="red.action-primary-default" variant="small" />}
+                    >
+                      Unprotect all inscriptions
+                    </Flag>
+                  </DropdownMenu.Item>
+                </Box>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
           </styled.span>
           {isLoading ? (
             <Spinner color={token('colors.ink.text-primary')} opacity={0.5} />
