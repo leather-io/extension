85062d5d1e69d249507de158bd1bb8305ded9765
kyranjamie <kyran.burraston@gmail.com>
2025-08-27 10:24:29 +0200

    feat: more obvious full page button
    In action popup mode only, we now show a header level button to access
    fullpage mode. We're tracking both events to see how often users opt to
    use this now-obvious feature.

--- src/app/features/container/headers/home.header.tsx
+++ src/app/features/container/headers/home.header.tsx
@@ -3,6 +3,8 @@ import { SettingsSelectors } from '@tests/selectors/settings.selectors';
 import { BarsTwoIcon } from '@leather.io/ui';
 
 import { useSwitchAccountSheet } from '@app/common/switch-account/use-switch-account-sheet-context';
+import { whenPageMode } from '@app/common/utils';
+import { FullScreenButton } from '@app/components/full-screen-button';
 import { Header } from '@app/components/layout/headers/header';
 import { HeaderGrid, HeaderGridRightCol } from '@app/components/layout/headers/header-grid';
 import { LogoBox } from '@app/components/layout/headers/logo-box';
@@ -17,6 +19,7 @@ export function HomeHeader() {
         leftCol={<LogoBox hideBelow={undefined} />}
         rightCol={
           <HeaderGridRightCol>
+            {whenPageMode({ full: null, popup: <FullScreenButton /> })}
             <Settings
               triggerButton={<BarsTwoIcon data-testid={SettingsSelectors.SettingsMenuBtn} />}
               toggleSwitchAccount={() => setIsShowingSwitchAccount(!isShowingSwitchAccount)}
