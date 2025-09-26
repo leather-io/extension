4c705365130c6007c3492cd51b82eafe847b88cd
Alex Perry <alexp3y@gmail.com>
2025-09-26 11:15:23 +0200

    feat: apply visibility services

--- src/app/services/extension-settings.service.ts
+++ src/app/services/extension-settings.service.ts
@@ -1,6 +1,8 @@
 import { SettingsService } from '@leather.io/services';
+import { serializeAssetId } from '@leather.io/utils';
 
 import { store } from '@app/store';
+import { selectTokenState } from '@app/store/manage-tokens/manage-tokens.slice';
 import { selectCurrentNetwork } from '@app/store/networks/networks.selectors';
 
 export class ExtensionSettingsService implements SettingsService {
@@ -8,6 +10,15 @@ export class ExtensionSettingsService implements SettingsService {
     return {
       quoteCurrency: 'USD',
       network: selectCurrentNetwork(store.getState()),
+      assetVisibility: Object.fromEntries(
+        Object.values(selectTokenState(store.getState()).entities).map(tokenSetting => [
+          serializeAssetId({
+            protocol: tokenSetting.id.includes('::') ? 'sip10' : 'rune',
+            id: tokenSetting.id,
+          }),
+          tokenSetting.enabled,
+        ])
+      ),
     };
   }
 }
